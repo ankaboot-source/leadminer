c9f54b499a39c2f399764bd0679e69ac5d2167fa
yossbg <youssri@flyweight.tech>
2022-02-15 12:12:58 +0100

    Update socket.js

--- frontend/src/boot/socket.js
+++ frontend/src/boot/socket.js
@@ -2,7 +2,7 @@ import { boot } from "quasar/wrappers";
 
 import { io } from "socket.io-client";
 
-const socket = io("http://46.101.205.87:8081");
+const socket = io("http://46.101.205.87:81");
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
