f2f562b98a156f676a3c71fa5dc01bcda97b59c7
you1996 <youssri@flyweight.tech>
2022-02-28 10:19:07 +0100

    feat:email type, fix:some bugs,feat:regex

--- backend/app/utils/regexp.js
+++ backend/app/utils/regexp.js
@@ -1,27 +1,59 @@
 const regex = new RegExp(
-  /("*(?<name>[,\p{L}\p{M}\d\s\(\)-]{1,})"*\s)*(<|\[)*(mailto:)*(?<address>[A-Za-z0-9!#$%&'+\/=?^_`\{|\}~-]+(?:\.[A-Za-z0-9!#$%&'*+\/=?^_`\{|\}~-]+)*@(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9,\s])?)(>|\])*/dgimu
+  /((?<name>[\p{L}\p{M}.\w\s\d\s\(\),-]{1,})"*\s)*(<|\[)*(?<address>[A-Za-z0-9!#$%&'+\/=?^_`\{|\}~-]+(?:\.[A-Za-z0-9!#$%&'*+\/=?^_`\{|\}~-]+)*@(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?)(>|\])*/gmu
 );
+//  /("*(?<name>[,\p{L}\p{M}\d\s\(\)-]{1,})"*\s)*(<|\[)*(mailto:)*(?<address>[A-Za-z0-9!#$%&'+\/=?^_`\{|\}~-]+(?:\.[A-Za-z0-9!#$%&'*+\/=?^_`\{|\}~-]+)*@(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9,\s])?)(>|\])*/dgimu
+let domainList = require("./domains.json");
+let domainsSet = new Set(domainList);
 
-matchRegexp = (ImapData) => {
-  let m;
-  let dataAfterRegEx;
+async function matchRegexp(ImapData) {
+  let promise = new Promise((resolve, reject) => {
+    let dataAfterRegEx;
+    ImapData = ImapData.filter(
+      (v, i, a) =>
+        a.findIndex((t) => JSON.stringify(t) === JSON.stringify(v)) === i
+    );
+    dataAfterRegEx = ImapData.map((data, index) => {
+      let ele = regex.exec(data);
+      if (ele == null || ele["groups"] == null) {
+        return null;
+      } else {
+        return ele["groups"];
+      }
+    });
+    // remove null values then return data
+    dataAfterRegEx = dataAfterRegEx.filter((item) => {
+      return Boolean(item);
+    });
+    // remove duplicates
+    dataAfterRegEx = dataAfterRegEx.filter(
+      (el, index, array) =>
+        array.findIndex((t) => t.address == el.address) == index
+    );
 
-  dataAfterRegEx = ImapData.map((data, index) => {
-    //console.log(data, index);
-    m = regex.exec(data);
-    if (m == null || m["groups"] == null) {
-      return null;
-    } else {
-      return m["groups"];
-    }
+    resolve(dataAfterRegEx);
   });
-  // remove duplicates
-  dataAfterRegEx = dataAfterRegEx.filter(
-    (v, i, a) =>
-      a.findIndex((t) => JSON.stringify(t) === JSON.stringify(v)) === i
-  );
-  // remove null values then return data
-  return dataAfterRegEx.filter((item) => item);
-};
+  let result = await promise;
+  return result;
+}
 
+async function checkDomainType(collectedEmails) {
+  let promise = new Promise((resolve, reject) => {
+    //
+    collectedEmails.map((email) => {
+      if (
+        domainsSet.has(
+          email.address.substring(email.address.lastIndexOf("@") + 1)
+        )
+      ) {
+        email["type"] = "Personal";
+      } else {
+        email["type"] = "Business";
+      }
+    });
+    resolve(collectedEmails);
+  });
+  let result = await promise;
+  return result;
+}
 exports.matchRegexp = matchRegexp;
+exports.checkDomainType = checkDomainType;
