5a544b9cc8cf14b189bb022dd568cde8001eb933
you1996 <youssri@flyweight.tech>
2022-06-10 14:53:31 +0100

    fix:remove content-type header

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,6 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "X-Requested-With,content-type"
   );
-  res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Connection", "keep-alive");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("X-Accel-Buffering", "no");
