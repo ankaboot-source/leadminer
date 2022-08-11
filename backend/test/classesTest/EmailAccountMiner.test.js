960c33016162e1c943ffa5dde5ce2b673b313fb7
you1996 <youssri@flyweight.tech>
2022-08-11 18:45:17 +0100

    fix(test): wait for test response on user login

--- backend/test/classesTest/EmailAccountMiner.test.js
+++ backend/test/classesTest/EmailAccountMiner.test.js
@@ -29,41 +29,41 @@ describe("Full maining flow", function () {
         })
         .expect((res) => {
           loggedInUser = JSON.parse(res.text).imap;
+          describe("Get Tree from imap server", function () {
+            describe("GET /boxes", function () {
+              it("should return 200 ", async function () {
+                const response = await request(app.server)
+                  .get(`/api/imap/${loggedInUser.id}/boxes`)
+                  .query({
+                    user: `{"id":${'"' + loggedInUser.id + '"'},"email":${
+                      '"' + loggedInUser.email + '"'
+                    },"password":${'"' + passwordTest + '"'},"host":${
+                      '"' + loggedInUser.host + '"'
+                    },"port":"993"}`,
+                  })
+                  .expect(200);
+              });
+            });
+          });
+          describe("mine folder for the logged in user", function () {
+            it("should mine and return 200", async function () {
+              // loggedInUser.email = '"' + loggedInUser.email + '"';
+              // loggedInUser.host = '"' + loggedInUser.host + '"';
+              let res = await request(app.server)
+                .get(`/api/imap/${loggedInUser.id}/collectEmails`)
+                .query({
+                  fields: ["HEADER", "1"],
+                  boxes: ["testFile", "none"],
+                  user: `{"id":${'"' + loggedInUser.id + '"'},"email":${
+                    '"' + loggedInUser.email + '"'
+                  },"password":${'"' + passwordTest + '"'},"host":${
+                    '"' + loggedInUser.host + '"'
+                  },"port":"993"}`,
+                })
+                .expect(200);
+            });
+          });
         });
-    });
-  });
-  describe("Get Tree from imap server", function () {
-    describe("GET /boxes", function () {
-      it("should return 200 ", async function () {
-        const response = await request(app.server)
-          .get(`/api/imap/${loggedInUser.id}/boxes`)
-          .query({
-            user: `{"id":${'"' + loggedInUser.id + '"'},"email":${
-              '"' + loggedInUser.email + '"'
-            },"password":${'"' + passwordTest + '"'},"host":${
-              '"' + loggedInUser.host + '"'
-            },"port":"993"}`,
-          })
-          .expect(200);
-      });
-    });
-  });
-  describe("mine folder for the logged in user", function () {
-    it("should mine and return 200", async function () {
-      // loggedInUser.email = '"' + loggedInUser.email + '"';
-      // loggedInUser.host = '"' + loggedInUser.host + '"';
-      let res = await request(app.server)
-        .get(`/api/imap/${loggedInUser.id}/collectEmails`)
-        .query({
-          fields: ["HEADER", "1"],
-          boxes: ["testFile", ""],
-          user: `{"id":${'"' + loggedInUser.id + '"'},"email":${
-            '"' + loggedInUser.email + '"'
-          },"password":${'"' + passwordTest + '"'},"host":${
-            '"' + loggedInUser.host + '"'
-          },"port":"993"}`,
-        })
-        .expect(200);
     });
   });
 });
