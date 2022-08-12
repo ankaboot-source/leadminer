9a29e0dfaf70fa2097e81fb969b719f46c2f9145
you1996 <youssri@flyweight.tech>
2022-08-12 12:38:31 +0100

    fix: enhance tests and workflow

--- backend/test/classesTest/EmailAccountMiner.test.js
+++ backend/test/classesTest/EmailAccountMiner.test.js
@@ -17,7 +17,7 @@ after(async () => {
   require("../../server").stop();
 });
 describe("Full mining flow", function () {
-  this.timeout(40000);
+  this.timeout(10000);
   let loggedInUser;
   describe("login", function () {
     it("create user (login request)", async function () {
@@ -31,23 +31,9 @@ describe("Full mining flow", function () {
         .expect((res) => {
           loggedInUser = JSON.parse(res.text).imap;
         });
-      logger.debug(loggedInUser);
     });
   });
-  describe("tree", function () {
-    it("Get Tree from imap server", async function () {
-      await request(app.server)
-        .get(`/api/imap/${loggedInUser.id}/boxes`)
-        .query({
-          user: `{"id":${'"' + loggedInUser.id + '"'},"email":${
-            '"' + loggedInUser.email + '"'
-          },"password":${'"' + passwordTest + '"'},"host":${
-            '"' + loggedInUser.host + '"'
-          },"port":"993"}`,
-        })
-        .expect(200);
-    });
-  });
+
   describe("mine", function () {
     it("mine folder for the logged in user", async function () {
       // loggedInUser.email = '"' + loggedInUser.email + '"';
@@ -57,6 +43,20 @@ describe("Full mining flow", function () {
         .query({
           fields: ["HEADER", "1"],
           boxes: ["testFile", "none"],
+          user: `{"id":${'"' + loggedInUser.id + '"'},"email":${
+            '"' + loggedInUser.email + '"'
+          },"password":${'"' + passwordTest + '"'},"host":${
+            '"' + loggedInUser.host + '"'
+          },"port":"993"}`,
+        })
+        .expect(200);
+    });
+  });
+  describe("tree", () => {
+    it("Get Tree from imap server", async function () {
+      await request(app.server)
+        .get(`/api/imap/${loggedInUser.id.trim()}/boxes`)
+        .query({
           user: `{"id":${'"' + loggedInUser.id + '"'},"email":${
             '"' + loggedInUser.email + '"'
           },"password":${'"' + passwordTest + '"'},"host":${
