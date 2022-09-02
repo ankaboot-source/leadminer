54c26973b5b73ba7daf9b7e2fc6c441f61e5d183
you1996 <youssri@flyweight.tech>
2022-09-02 15:41:37 +0100

    fix:remove redis from module

--- backend/app/models/emails.model.js
+++ backend/app/models/emails.model.js
@@ -1,10 +1,6 @@
-const redis = require('redis');
-// initialise sse (server sent events)
-const client = redis.createClient(6379);
-client.connect();
 /* A function that returns a sequelize model. */
 module.exports = (sequelize, Sequelize) => {
-  const Emails = sequelize.define('emails', {
+  const Emails = sequelize.define("emails", {
     email_Id: {
       allowNull: false,
       type: Sequelize.STRING,
