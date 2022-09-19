d0e5854c2f829d3b7956e9011a35432898ffeae7
you1996 <youssri@flyweight.tech>
2022-09-19 12:16:36 +0100

    feat:add worker for refining data using helpers

--- backend/app/workers/dataRefiningWorker.js
+++ backend/app/workers/dataRefiningWorker.js
@@ -11,7 +11,6 @@ parentPort.on("message", (userId) => {
       const minedEmails = sortedData[0];
       const transactional = sortedData[1];
       const noReply = await minedDataHelpers.getNoReplyEmails(userId.userId);
-      console.log(noReply);
       //const noReply = await redisClient.get('noReply');
       const invalidDomain = await redisClient.scard("invalidDomainEmails");
       let data = {
