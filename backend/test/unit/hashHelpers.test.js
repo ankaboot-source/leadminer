597ef529f884c9e4400304f1741c4555df91ef24
Chedly Zouche <chedly.zouche@pm.me>
2022-10-31 16:28:51 +0100

    Fix failing test

--- backend/test/unit/hashHelpers.test.js
+++ backend/test/unit/hashHelpers.test.js
@@ -1,10 +1,11 @@
 const { expect } = require('chai');
 const hashHelpers = require('../../app/utils/helpers/hashHelpers');
-const dataTest = require('../testData.json');
 
 describe('hashHelpers.hashEmail(emailAddress)', () => {
   it('should return valid hash', () => {
     const output = hashHelpers.hashEmail(dataTest.emailsHash[0]);
-    expect(output).eq(dataTest.emailsHash[1]);
+    const expected =
+      '9d18a49b21fc1eed4096d8c84eaebcddd03caa9eab536a885aac6a509a6b1edd';
+    expect(output).eq(expected);
   });
 });
