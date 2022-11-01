84d48dc8a8f44ff0fd7b4ec7dfcc7672775331c2
Chedly Zouche <chedly.zouche@pm.me>
2022-11-01 16:27:51 +0100

    Change postgres settings

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -2,13 +2,13 @@ const config = require('config');
 
 const POSTGRES_DB = config.has('server.postgres.name')
   ? config.get('server.postgres.name')
-  : process.env.POSTGRES_DB;
+  : '';
 const POSTGRES_PASSWORD = config.has('server.postgres.password')
   ? config.get('server.postgres.password')
   : process.env.POSTGRES_PASSWORD;
 const POSTGRES_USER = config.has('server.postgres.user')
   ? config.get('server.postgres.user')
-  : process.env.POSTGRES_USER;
+  : '';
 const POSTGRES_HOST = config.has('server.postgres.host')
   ? config.get('server.postgres.host')
   : 'localhost';
