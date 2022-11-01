50812ed5ca7ff752f260d44da6817737efa3c8bf
Chedly Zouche <chedly.zouche@pm.me>
2022-11-01 16:40:48 +0100

    Fix test:integration

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -2,16 +2,16 @@ const config = require('config');
 
 const POSTGRES_DB = config.has('server.postgres.name')
   ? config.get('server.postgres.name')
-  : '';
+  : process.env.POSTGRES_DB;
 const POSTGRES_PASSWORD = config.has('server.postgres.password')
   ? config.get('server.postgres.password')
   : process.env.POSTGRES_PASSWORD;
 const POSTGRES_USER = config.has('server.postgres.user')
   ? config.get('server.postgres.user')
-  : '';
+  : process.env.POSTGRES_USER;
 const POSTGRES_HOST = config.has('server.postgres.host')
   ? config.get('server.postgres.host')
-  : 'localhost';
+  : process.env.POSTGRES_HOST;
 
 /* Exporting the database information to be used in the server.js file. */
 module.exports = {
