53a146d08a4adaa16089e1aad5b68e5206811d44
Chedly Zouche <chedly.zouche@pm.me>
2022-11-01 09:13:08 +0100

    Update cors middleware

--- backend/app/middleware/cors.js
+++ backend/app/middleware/cors.js
@@ -1,9 +1,10 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: '*',
+  origin: ['http://localhost:8082', 'https://leadminer.io'],
   methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
   allowedHeaders: 'X-Requested-With,Content-type,X-imap-login',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
   credentials: true
 };
 const corsMiddleware = cors(corsOptions);
