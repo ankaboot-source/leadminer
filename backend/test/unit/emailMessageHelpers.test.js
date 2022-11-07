9d9075f45eea706d4e5c559479a519806c050821
malek salem <malek.salem.14@gmail.com>
2022-11-07 12:46:46 +0100

    add tests

--- backend/test/unit/emailMessageHelpers.test.js
+++ backend/test/unit/emailMessageHelpers.test.js
@@ -1,5 +1,43 @@
 const { expect } = require('chai');
+
 const emailMessageHelpers = require('../../app/utils/helpers/emailMessageHelpers');
+const EmailMessage = require("../../app/services/EmailMessage")
+
+const config = require('config'),
+  NEWSLETTER_HEADER_FIELDS = config.get('email_types.newsletter').split(',').filter(n => n),
+  TRANSACTIONAL_HEADER_FIELDS = config
+    .get('email_types.transactional')
+    .split(',').filter(n => n),
+  MAILING_LIST_HEADER_FIELDS = config.get('email_types.list').split(',').filter(n => n);
+
+const TEST_HEADERS = {
+    'delivered-to': [ '' ],
+    'received': ['',],
+    'x-google-smtp-source': [''],
+    'x-received': [''],
+    'arc-seal': [''],
+    'arc-message-signature': [''],
+    'arc-authentication-results': [''],
+    'return-path': ['',''],
+    'received-spf': [''],
+    'authentication-results': [''],
+    'dkim-signature': [''],
+    'x-hs-cid': [ '' ],
+    'list-unsubscribe': [''],
+    'date': [ '' ],
+    'from': [ '' ],
+    'reply-to': [ '' ],
+    'to': [ '' ],
+    'message-id': [''],
+    'subject': [ '' ],
+    'mime-version': [ '1.0' ],
+    'content-type': [''],
+    'precedence': [ 'bulk' ],
+    'x-report-abuse-to': [''],
+    'feedback-id': [ '' ]
+  }
+
+const Email = new EmailMessage('', TEST_HEADERS, '', '', '')
 
 describe('emailMessageHelpers.isNoReply(emailAddress)', () => {
   it('should return true for no-reply-leadminer@leadminer.io', () => {
@@ -14,3 +52,87 @@ describe('emailMessageHelpers.isNoReply(emailAddress)', () => {
     expect(output).to.be.false;
   });
 });
+
+describe('EmailMessage.isNewsletter', () => {
+    
+    it('Should return false when headers not present', () => {
+
+        NEWSLETTER_HEADER_FIELDS.forEach(
+            (el) => {
+                if (Email.header[el])
+                    delete Email.header[el];
+        })
+        expect(Email.isNewsletter()).to.be.false
+    })
+
+    NEWSLETTER_HEADER_FIELDS.forEach(
+        (el) => {
+            it('Should return true for header: ' + el, () => {
+                Email.header[el] = ['']
+                expect(Email.isNewsletter()).to.be.true
+                delete Email.header[el]
+            })
+        }
+    )
+})
+
+describe('EmailMessage.isTransactional', () => {
+
+    it('Should return false when headers not present', () => {
+
+        TRANSACTIONAL_HEADER_FIELDS.forEach(
+            (el) => {
+                if (Email.header[el])
+                    delete Email.header[el];
+        })
+        expect(Email.isTransactional()).to.be.false
+    })
+
+    TRANSACTIONAL_HEADER_FIELDS.forEach(
+        (el) => {
+            it('Should return true for header: ' + el, () => {
+                Email.header[el] = ['']
+                expect(Email.isTransactional()).to.be.true
+                delete Email.header[el]
+            })
+        }
+    )
+})
+
+describe('EmailMessage.isList', () => {
+
+    it('Should return false when headers not present', () => {
+
+        MAILING_LIST_HEADER_FIELDS.forEach(
+            (el) => {
+                if (Email.header[el])
+                    delete Email.header[el];
+        })
+        expect(Email.isList()).to.be.false
+    })
+
+    MAILING_LIST_HEADER_FIELDS.forEach(
+        (el) => {
+            it('Should return true for header: ' + el, () => {
+                Email.header[el] = ['']
+                expect(Email.isList()).to.be.true
+                delete Email.header[el]
+            })
+        }
+    )
+})
+
+describe('EmailMessage.isInConversation', () => {
+
+    it('Should return 0 when key "references" is not present', () => {
+        if (Email.header["references"])
+            delete Email.header["references"]
+        expect(Email.isInConversation()).equal(0)
+    })
+
+    it('Should return 1 if key "references" is present', () => {
+        Email.header["references"] = ['']
+        expect(Email.isInConversation()).equal(1)
+        delete Email.header["references"]
+    })
+})
