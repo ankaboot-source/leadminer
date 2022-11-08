49d255dfcbf3ef8e94ee77f29da0998ede6bb613
malek salem <malek.salem.14@gmail.com>
2022-11-08 17:17:20 +0100

    enable realtime for table refinedpersons

--- backend/supabase/migrations/20220922191519_migration.sql
+++ backend/supabase/migrations/20220922191519_migration.sql
@@ -125,6 +125,17 @@ CREATE TABLE IF NOT EXISTS public.refinedpersons
     FOREIGN KEY (personid) REFERENCES persons(id)
 );
 
+begin;
+
+  -- remove the supabase_realtime publication
+  drop publication if exists supabase_realtime;
+
+  -- re-create the supabase_realtime publication with no tables
+  create publication supabase_realtime;
+
+commit;
+-- add a table to the publication
+alter publication supabase_realtime add table public.refinedpersons;
 
 create or replace function get_tags_per_person(personid uuid, userid uuid)
 returns text[]
