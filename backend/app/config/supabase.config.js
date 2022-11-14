1e1fe8949af159e5bf91d8644bdf2a8f8b6919e9
Chedly Zouche <chedly.zouche@pm.me>
2022-11-14 10:08:22 +0100

    Add env vars explanation (#365)

--- backend/app/config/supabase.config.js
+++ backend/app/config/supabase.config.js
@@ -1,10 +1,11 @@
 const config = require('config');
 
 const supabaseUrl =
-  process.env.SUPABASE_ID ?? config.get('server.supabase.url');
+  process.env.SUPABASE_PROJECT_URL ?? config.get('server.supabase.url');
 
 const supabaseToken =
-  process.env.SUPABASE_TOKEN ?? config.get('server.supabase.token');
+  process.env.SUPABASE_SECRET_PROJECT_TOKEN ??
+  config.get('server.supabase.token');
 
 module.exports = {
   supabaseUrl,
