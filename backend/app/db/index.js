78255166137f5c9b9169e9ae0568e5c106bafce5
Chedly Zouche <chedly.zouche@pm.me>
2022-12-09 14:04:40 +0100

    Fix deepsource issue

--- backend/app/db/index.js
+++ backend/app/db/index.js
@@ -3,16 +3,17 @@ const { Postgres } = require('./node-postgres');
 const { SupabaseHandlers } = require('./supabase');
 const logger = require('../utils/logger')(module);
 
-let db;
-switch (connectionType) {
-  case 'native':
-    db = new Postgres(logger);
-    break;
-  case 'pgrest':
-    db = new SupabaseHandlers();
-    break;
-  default:
-    break;
+function getDb() {
+  switch (connectionType) {
+    case 'native':
+      return new Postgres(logger);
+    case 'pgrest':
+      return new SupabaseHandlers();
+    default:
+      break;
+  }
 }
+
+const db = getDb();
 
 module.exports = { db };
