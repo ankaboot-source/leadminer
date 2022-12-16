d6a514e76e0a9420fa84d25903640d997d3b2954
Malek salem <malek.salem.14@gmail.com>
2022-12-16 12:41:39 +0100

    update code

--- backend/app/services/storage.js
+++ backend/app/services/storage.js
@@ -8,18 +8,18 @@ const logger = require('../utils/logger')(module);
  */
 function PrepareData(arrayData) {
 
-    const object = { messages: [], persons: new Map(), pointOfContacts: [], tags: [] }
+  const object = { messages: [], persons: new Map(), pointOfContacts: [], tags: [] };
 
-    arrayData.forEach(data => {
-        object.messages.push(data.message)
-        for (const personObject of data.persons) {
-            object.persons.set(personObject.person.email, personObject.person)
-            object.pointOfContacts.push(personObject.pointOfContact)
-            object.tags.push(...personObject.tags)
-        }
-    });
-    object.persons = [...object.persons.values()] // No duplicates
-    return object
+  arrayData.forEach(data => {
+    object.messages.push(data.message);
+    for (const personObject of data.persons) {
+      object.persons.set(personObject.person.email, personObject.person);
+      object.pointOfContacts.push(personObject.pointOfContact);
+      object.tags.push(...personObject.tags);
+    }
+  });
+  object.persons = [...object.persons.values()]; // No duplicates
+  return object;
 
 }
 
@@ -30,122 +30,133 @@ function PrepareData(arrayData) {
  * @returns {Object}
  */
 function buildLookup(key, objects) {
-    const result = {}
+  const result = {};
 
-    for (const data of objects) {
-        result[data[key]] = data
-    }
-    return result
+  for (const data of objects) {
+    result[data[key]] = data;
+  }
+  return result;
 }
 
 class Storage {
 
-    #BULK_INSERT_BUFFER = {}   // Private temporary storage for data
+  #BULK_INSERT_BUFFER = {}; // Private temporary storage for data
 
-    /**
-     * Stores data
-     * @param {string} url - The supabase URL.
-     * @param {string} token - The supabase token.
-     */
-    constructor(batch=true, batchSize = 200, databaseClient = db) {
+  /**
+       * Storage constructor - Manages The way we store data
+       * @param {string} batch - Inserts in bulk if true else normal insert. Default: True
+       * @param {string} batchSize - The Size of the bulk data to be inserted.
+       */
+  constructor(batch = false, batchSize = 200, dbClient) {
 
-        this.db = databaseClient
-        this.batchSize = batchSize
-        this.batch = batch
+    this.db = dbClient;
+    this.batchSize = batchSize;
+    this.batch = batch;
+  }
+
+  /**
+       * exists - Checks for userID  
+       * @param {string} userID - ID of the user.
+       */
+  #exists(userID) {
+    return this.#BULK_INSERT_BUFFER[userID] !== undefined;
+  }
+
+  /**
+       * _registerUser - Creates new user in buffer.
+       * @param {string} userID - ID of the user.
+       */
+  #registerUser(userID, data) {
+
+    if (!this.#exists(userID)) {
+      this.#BULK_INSERT_BUFFER[userID] = [data]; // reserves space for new user
+    }
+  }
+
+  /**
+       * _checkAndReleaseBuffer - Checks if buffer is full
+       * @param {string} userID - ID of the user.
+       */
+  #checkAndReleaseBuffer(userID) {
+
+    const isFull = this.#exists(userID) && this.#BULK_INSERT_BUFFER[userID].length === this.batchSize; // check if max limit is reached
+    const data = isFull ? Object.assign([], this.#BULK_INSERT_BUFFER[userID]) : [];
+    if (isFull) {
+      delete this.#BULK_INSERT_BUFFER[userID];
+    }
+    return data;
+  }
+
+  #addToBuffer(userID, data) {
+
+    if (this.#exists(userID)) {
+      this.#BULK_INSERT_BUFFER[userID].push(data);
+    } else {
+      this.#registerUser(userID, data);
     }
 
-    /**
-     * exists - Checks for userID  
-     * @param {string} userID - ID of the user.
-     */
-    #exists(userID) {
-        return this.#BULK_INSERT_BUFFER[userID] !== undefined
-    }
+  }
+  /**
+       * storeBulk - Stores data in batches
+       * @param {Object[]} bulkData - Array of objects contains extrcted data [{message: {}, persons: []} ...]
+       */
+  async storeBulk(bulkObjects) {
 
-    /**
-     * _registerUser - Creates new user in buffer.
-     * @param {string} userID - ID of the user.
-     */
-    #registerUser(userID, data) {
+    const { messages, persons, pointOfContacts, tags } = PrepareData(bulkObjects);
 
-        if (!this.#exists(userID)) {
-            this.#BULK_INSERT_BUFFER[userID] = [data] // reserves space for new user
-        }
-    }
+    Promise.allSettled([this.db.upsertPerson(persons), this.db.insertMessage(messages)])
 
-    /**
-     * _checkAndReleaseBuffer - Checks if buffer is full
-     * @param {string} userID - ID of the user.
-     */
-    #checkAndReleaseBuffer(userID) {
+      .then(async ([pPromise, mPromise]) => {
 
-        const isFull = this.#exists(userID) && this.#BULK_INSERT_BUFFER[userID].length === this.batchSize // check if max limit is reached
-        const data = isFull ? Object.assign([], this.#BULK_INSERT_BUFFER[userID]) : []
-        isFull ? delete this.#BULK_INSERT_BUFFER[userID] : undefined
-        return data
-    }
+        const mids = mPromise.value;
+        const pids = pPromise.value;
 
-    #addToBuffer(userID, data) {
-        
-        if (this.#exists(userID)) this.#BULK_INSERT_BUFFER[userID].push(data)
-        else this.#registerUser(userID, data)
+        if (pids.data && mids.data) {
+          pids.data = buildLookup('email', pids.data);
+          mids.data = buildLookup('message_id', mids.data);
 
-    }
-    /**
-     * storeBulk - Stores data in batches
-     * @param {Object[]} bulkData - Array of objects
-     * @returns {Promise}
-     */
-    async storeBulk(bulkObjects) {
+          for (const tag of tags) {
+            if (tag && pids.data[tag.email]) {
+              tag.personid = pids.data[tag.email].id;
+              delete tag.email;
+            }
+          }
 
-        const { messages, persons, pointOfContacts, tags } = PrepareData(bulkObjects)
-        if (!messages.length && !persons.length) {
-            console.log(messages, persons)
+          for (const poc of pointOfContacts) {
+            if (pids.data[poc.email] && mids.data[poc.message_id]) {
+              poc.personid = pids.data[poc.email].id;
+              poc.messageid = mids.data[poc.message_id].id;
+              delete poc.message_id;
+              delete poc.email;
+            } else {
+              // TODO: add logging
+            }
+          }
+
+          this.db.insertPointOfContact(pointOfContacts).then((res) => {
+            if (res.error) {
+              console.log(res.error); //TODO: Proper Logging
+            }
+          });
+          this.db.createTags(tags).then((res) => {
+            if (res.error) {
+              console.log(res.error); //TODO: Proper Logging
+            }
+          });
+
         } else {
-            Promise.allSettled([this.db.upsertPerson(persons), this.db.insertMessage(messages)])
-
-            .then(async ([pPromise, mPromise]) => {
-
-                const mids = mPromise.value
-                const pids = pPromise.value
-
-                if (pids.data && mids.data) {
-                    pids.data = buildLookup('email', pids.data)
-                    mids.data = buildLookup('message_id', mids.data)
-
-                    for (const tag of tags) {
-                        if (tag && pids.data[tag.email])
-                            tag.personid = pids.data[tag.email].id; delete tag.email
-                    }
-
-                    for (const poc of pointOfContacts) {
-                        if (pids.data[poc.email] && mids.data[poc.message_id]) {
-                            poc.personid = pids.data[poc.email].id
-                            poc.messageid = mids.data[poc.message_id].id
-                            delete poc.message_id
-                            delete poc.email
-                        } else {
-                            // TODO: add logging
-                        }
-                    }
-
-                    this.db.insertPointOfContact(pointOfContacts).then((res) => {if (res.error) console.log(err)})
-                    this.db.createTags(tags).then((res) => { { if (res.error) console.log(error) } })
-
-                } else {
-                    // TODO: add logging
-                    console.log("Top level Message Error", mids.error)
-                    console.log("Top level Person Error", pids.error)
-                }
-            })
+          //TODO: Proper Logging
+          console.log('Top level Message Error', mids.error);
+          console.log('Top level Person Error', pids.error);
         }
-    }
+      });
+  }
 
-    /**
-     * _storeData -  Stores data to Message, Person, pointOfContact, tags tables. 
-     * @param {*} Object 
-     */
-    async store(object) {
+  /**
+       * _storeData -  Stores data to Message, Person, pointOfContact, tags tables. 
+       * @param {*} Object - An object contains extrcted data {message: {}, persons: []}
+       */
+  async store(object) {
 
     /**
              * This can be change instead of awaiting for message then acting, we can
@@ -203,31 +214,30 @@ class Storage {
         }
       });
     }
-    }
+  }
 
-    /**
-     * storeData - Stores data, redirects to differnt storing channels based on configuration.
-     * @param {*} userID - ID of user
-     * @param { Object } object - A data object
-     */
-    async storeData(userID, data) {
+  /**
+       * storeData - Stores data, redirects to differnt storing channels based on configuration.
+       * @param {*} userID - ID of user
+       * @param { Object } object - An object contains extrcted data {message: {}, persons: []}
+       */
+  async storeData(userID, data) {
 
-        if (!this.batch) {
-            this.store(data)
-        }
-        else{   // In case we're using bulk
-            const releasedData = this.#checkAndReleaseBuffer(userID)
-            if (releasedData.length) {
-                await this.storeBulk(releasedData)
-            } else {
-                this.#addToBuffer(userID, data);
-            }
-        }
+    if (!this.batch) {
+      await this.store(data);
+    } else { // In case we're using bulk
+      const releasedData = this.#checkAndReleaseBuffer(userID);
+      if (releasedData.length) {
+        await this.storeBulk(releasedData);
+      } else {
+        this.#addToBuffer(userID, data);
+      }
     }
+  }
 }
 
-const storage = new Storage()
+const storage = new Storage();
 
 module.exports = {
-    storage,
-}
+  storage
+};
