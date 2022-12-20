ea199d3853aaa76e29c49210aa7e99ca3dca3472
Malek salem <malek.salem.14@gmail.com>
2022-12-20 16:20:42 +0100

    fix deepsource

--- backend/test/unit/regexHelpers.test.js
+++ backend/test/unit/regexHelpers.test.js
@@ -6,27 +6,25 @@ const testData = require('../testData.json');
 
 describe('Regex redos checker', () => {
 
-    const regex = regExHelpers.getRegEx()
-    let messageError = 'Regex is vulnerable !'
-    
-    regex.forEach((r) => {
-      it('regex should be REDOS safe', async () => {
-        const { attack, complexity, hotspots, status } = await check(r.source, r.flags)
-        
-        if (status === 'vulnerable'){  // Constructs helpful error message
-          const attack = attack.pattern
-          const complexity = complexity.type
-          const vulParts = hotspot.map((i) => {return ` index(${i.start}, ${i.end}): ${r.source.slice(i.start, i.end)}`})
-          messageError += ` \n\t- Complixity: ${complexity} \n\t- Attack string: ${attack} \n\t- Vulnerable parts: ${vulParts}\n\t`
-        }
-        expect(status, messageError).to.eq('safe')
+  const regex = regExHelpers.getRegEx()
+  let messageError = 'Regex is vulnerable !'
+
+  regex.forEach((r) => {
+    it('regex should be REDOS safe', async () => {
+      const { attack, complexity, hotspots, status } = await check(r.source, r.flags)
+
+      if (status === 'vulnerable') {  // Constructs helpful error message
+        const vulParts = hotspot.map((i) => { return ` index(${i.start}, ${i.end}): ${r.source.slice(i.start, i.end)}` })
+        messageError += ` \n\t- Complixity: ${complexity.type} \n\t- Attack string: ${attack.pattern} \n\t- Vulnerable parts: ${vulParts}\n\t`
+      }
+      expect(status, messageError).to.eq('safe')
     })
   })
 
 })
 
 describe('regExHelpers.extractEmailsFromBody(text)', () => {
-  
+
   it('Should return a valid array of emails', () => {
     const output = regExHelpers.extractNameAndEmailFromBody(testData.emailBody);
     expect(output).to.eql(testData.expectedForBodyExtraction);
@@ -48,11 +46,11 @@ describe('regExHelpers.extractNameAndEmail(data)', () => {
   it('Should return array with one valid object', () => {
     // Test with all information
     let fullInformationOutput = regExHelpers.extractNameAndEmail("this is myyyyyyyyyyyyyyyy name <tester@testing.com>");
-    expect(fullInformationOutput).to.eql([{name:'this is myyyyyyyyyyyyyyyy name', identifier: 'tester', address:'tester@testing.com'}]);
+    expect(fullInformationOutput).to.eql([{ name: 'this is myyyyyyyyyyyyyyyy name', identifier: 'tester', address: 'tester@testing.com' }]);
 
     // test only with email
     emailOnlyOutput = regExHelpers.extractNameAndEmail("<tester@testing.com>");
-    expect(emailOnlyOutput).to.eql([{name:'', identifier: 'tester', address:'tester@testing.com'}]);
+    expect(emailOnlyOutput).to.eql([{ name: '', identifier: 'tester', address: 'tester@testing.com' }]);
 
   })
 });
