099ede387761466ff58c3c86e2cbb4cfc5728cc4
Malek salem <malek.salem.14@gmail.com>
2022-12-20 15:59:30 +0100

    fix bugs

--- backend/test/unit/regexHelpers.test.js
+++ backend/test/unit/regexHelpers.test.js
@@ -11,15 +11,15 @@ describe('Regex redos checker', () => {
     
     regex.forEach((r) => {
       it('regex should be REDOS safe', async () => {
-        const { attack, complexity, hotspots } = await check(r.source, r.flags)
+        const { attack, complexity, hotspots, status } = await check(r.source, r.flags)
         
-        if (result.status === 'vulnerable'){  // Constructs helpful error message
+        if (status === 'vulnerable'){  // Constructs helpful error message
           const attack = attack.pattern
           const complexity = complexity.type
           const vulParts = hotspot.map((i) => {return ` index(${i.start}, ${i.end}): ${r.source.slice(i.start, i.end)}`})
           messageError += ` \n\t- Complixity: ${complexity} \n\t- Attack string: ${attack} \n\t- Vulnerable parts: ${vulParts}\n\t`
         }
-        expect(result, messageError).to.have.property('status', 'safe')
+        expect(status, messageError).to.eq('safe')
     })
   })
 
