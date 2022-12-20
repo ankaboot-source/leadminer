d7bb8c0b97f86f3012614a60af1e31a119e435e5
Malek salem <malek.salem.14@gmail.com>
2022-12-20 15:57:29 +0100

    rename vars

--- backend/test/unit/regexHelpers.test.js
+++ backend/test/unit/regexHelpers.test.js
@@ -11,15 +11,15 @@ describe('Regex redos checker', () => {
     
     regex.forEach((r) => {
       it('regex should be REDOS safe', async () => {
-        const result = await check(r.source, r.flags)
+        { attack, complexity, hotspots } = await check(r.source, r.flags)
         
         if (result.status === 'vulnerable'){  // Constructs helpful error message
-          const attack = result.attack.pattern
-          const complexity = result.complexity.type
-          const vulParts = result.hotspot.map((i) => {return ` index(${i.start}, ${i.end}): ${r.source.slice(i.start, i.end)}`})
+          const attack = attack.pattern
+          const complexity = complexity.type
+          const vulParts = hotspot.map((i) => {return ` index(${i.start}, ${i.end}): ${r.source.slice(i.start, i.end)}`})
           messageError += ` \n\t- Complixity: ${complexity} \n\t- Attack string: ${attack} \n\t- Vulnerable parts: ${vulParts}\n\t`
         }
-        expect(result, messageError).to.deep.have.property('status', 'safe')
+        expect(result, messageError).to.have.property('status', 'safe')
     })
   })
 
@@ -41,18 +41,18 @@ describe('regExHelpers.extractEmailsFromBody(text)', () => {
 describe('regExHelpers.extractNameAndEmail(data)', () => {
   it('Should return objects with valid emails', () => {
     const output = regExHelpers.extractNameAndEmail(testData.EmailNameTest[0]);
-    expect(output).to.have.deep.members(testData.expectedEmailNameAddress);
+    expect(output).to.eql(testData.expectedEmailNameAddress);
 
   });
 
   it('Should return array with one valid object', () => {
     // Test with all information
-    let output = regExHelpers.extractNameAndEmail("this is myyyyyyyyyyyyyyyy name <tester@testing.com>");
-    expect(output).to.have.deep.members([{name:'this is myyyyyyyyyyyyyyyy name', identifier: 'tester', address:'tester@testing.com'}]);
+    let fullInformationOutput = regExHelpers.extractNameAndEmail("this is myyyyyyyyyyyyyyyy name <tester@testing.com>");
+    expect(fullInformationOutput).to.eql([{name:'this is myyyyyyyyyyyyyyyy name', identifier: 'tester', address:'tester@testing.com'}]);
 
     // test only with email
-    output = regExHelpers.extractNameAndEmail("<tester@testing.com>");
-    expect(output).to.have.deep.members([{name:'', identifier: 'tester', address:'tester@testing.com'}]);
+    emailOnlyOutput = regExHelpers.extractNameAndEmail("<tester@testing.com>");
+    expect(emailOnlyOutput).to.eql([{name:'', identifier: 'tester', address:'tester@testing.com'}]);
 
   })
 });
