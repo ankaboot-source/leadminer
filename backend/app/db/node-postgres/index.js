f2cc0566315fcc4e1d70087152b746cf70f0b663
Chedly Zouche <chedly.zouche@pm.me>
2022-12-23 16:14:18 +0100

    Fix references  error

--- backend/app/db/node-postgres/index.js
+++ backend/app/db/node-postgres/index.js
@@ -28,7 +28,7 @@ class Postgres {
     isConversation
   ) {
     const query =
-      'INSERT INTO messages(channel, folder_path, date, userid, message_id, references, list_id, conversation) ' +
+      'INSERT INTO messages(channel, folder_path, date, userid, message_id, "references", list_id, conversation) ' +
       'VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *';
 
     try {
@@ -115,7 +115,20 @@ class Postgres {
 
       const result = await pool.query(
         query,
-        [name, emailsAddress, userID, '', '', '', [], [], '', '', '', [identifier]],
+        [
+          name,
+          emailsAddress,
+          userID,
+          '',
+          '',
+          '',
+          [],
+          [],
+          '',
+          '',
+          '',
+          [identifier]
+        ],
         this.logger
       );
       return { data: result.rows[0], error: null };
