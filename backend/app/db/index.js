bac70d2133ede18c6aa9386b6a1521eaa09b09ee
Malek salem <malek.salem.14@gmail.com>
2022-12-28 11:57:05 +0100

    add store && refactoring

--- backend/app/db/index.js
+++ backend/app/db/index.js
@@ -1,19 +1,35 @@
 const { connectionType } = require('../config/supabase.config');
 const { Postgres } = require('./node-postgres');
+const { Store } = require('./store');
 const { SupabaseHandlers } = require('./supabase');
 const logger = require('../utils/logger')(module);
 
-function getDb() {
-  switch (connectionType) {
-    case 'native':
-      return new Postgres(logger);
-    case 'pgrest':
-      return new SupabaseHandlers();
-    default:
-      return new Postgres(logger);
+
+function initDatabase() {
+
+  const db = connectionType == 'pgrest' ? new SupabaseHandlers() : new Postgres(logger)
+  const store = new Store(db) 
+
+  return {  
+    /**
+     * Stores contacts to Message, Person, pointOfContact, tags tables. 
+     * @param {{message: object, persons: object[]}} contacts - The extracted (messages, persons, pocs, tags)
+     * @param {string} userID - The id of the user
+     */
+    add: function (contacts, userID) {
+      return store.store(contacts, userID)
+    },
+
+    /**
+     * returns the current db client either 'pgrest' or 'native postgres'
+     * @returns {db}
+     */
+    getClient: function () {
+      return db
+    }
   }
 }
 
-const db = getDb();
+const db = initDatabase()
 
 module.exports = { db };
