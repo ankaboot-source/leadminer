9c8e2e3c23a2b7cfcf34e137a5fc9be400c80026
Malek salem <malek.salem.14@gmail.com>
2023-01-04 20:05:34 +0100

    add views

--- supabase/migrations/20230103181806_new_rpc_functions.sql
+++ supabase/migrations/20230103181806_new_rpc_functions.sql
@@ -12,27 +12,19 @@ AS $function$
 DECLARE
   person_name text;
 BEGIN
-    select (
+    return (
         select name FROM public.pointsofcontact
         inner join public.messages as msg on public.pointsofcontact.messageid = msg.id
-        where public.pointsofcontact.personid=get_most_recent_name.personid
-            and msg.date >= current_date - interval '365' day
+        where 
+        (
+          public.pointsofcontact.personid=get_most_recent_name.personid
+          and msg.date >= current_date - interval '365' day 
+          and name != ''
+        ) or public.pointsofcontact.personid=get_most_recent_name.personid and name !=''
         group by name
         order by count(name) desc
         limit 1
-    ) into person_name;
-
-    if (person_name = '' or person_name is null) then
-      return (
-        select name FROM public.pointsofcontact
-        inner join public.messages as msg on public.pointsofcontact.messageid = msg.id
-        where public.pointsofcontact.personid=get_most_recent_name.personid
-        group by name
-        order by count(name) desc
-        limit 1
-      );
-    end if;
-    return person_name;
+    );
 END;
 $function$;
 
@@ -43,21 +35,8 @@ DECLARE
 BEGIN
   update public.refinedpersons
     set engagement=person.engagement, recency=person.recency, occurence=person.occurence, alternate_names=person.alternate_names
-    from (
-      select
-        personid,
-        refined_persons.userid as userid,
-        count(*) filter (where msg.conversation) as engagement,
-        max(msg.date) as recency,
-        count(*) filter (where cc or _to or _from or reply_to or bcc or body) as occurence,
-        public.get_most_recent_name(personid) as name,
-        array_agg(distinct(name)) as alternate_names
-      from public.pointsofcontact
-        inner join public.messages as msg on public.pointsofcontact.messageid = msg.id
-      where public.pointsofcontact.userid=refined_persons.userid
-      group by personid
-    ) as person
-    where refinedpersons.personid=person.personid;
+    from public.calculated_refined_view as person
+    where refinedpersons.personid=person.personid and person.userid = refined_persons.userid;
 END;
 $function$;
 
@@ -66,20 +45,31 @@ LANGUAGE plpgsql
 AS $function$
 BEGIN
   INSERT INTO refinedpersons(personid, userid, tags, name, email)
-    select id,
-    populate_refined.userid as userid,
-    tags,
-    name,
-    email
-    from (
-      select id, persons.name, email, array(
-        select tags.name
-        from tags
-        where tags.personid=public.persons.id group by tags.personid, tags.name
-      ) as tags
-      from public.persons
-    ) as _
-    where not tags && '{"transactional", "no-reply"}'
+    select id, populate_refined.userid, t.tags, name, email
+    from public.persons
+    inner join public.grouped_tags_by_person_view as t on t.personid = id
+    where not t.tags_reachability && '{0}'
   on conflict(personid) do nothing;
 END;
-$function$
+$function$;
+
+create or replace view public.grouped_tags_by_person_view as
+    select
+    array_agg(name) as tags,
+    array_agg(reachable) as tags_reachability,
+    personid
+    from tags
+    group  by personid;
+    
+create or replace view public.calculated_refined_view as
+  select
+      personid,
+      pointsofcontact.userid,
+      count(*) filter (where msg.conversation) as engagement,
+      max(msg.date) as recency,
+      count(*) filter (where cc or _to or _from or reply_to or bcc or body) as occurence,
+      public.get_most_recent_name(personid) as name,
+      array_agg(distinct(name)) as alternate_names
+  from public.pointsofcontact
+  inner join messages as msg on msg.id = messageid
+  group by personid, pointsofcontact.userid;
