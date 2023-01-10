edd7cce5ac3324e08c8aee0273e54cf198e931a6
Malek salem <malek.salem.14@gmail.com>
2023-01-10 15:38:15 +0100

    update unittest

--- backend/test/integration/domain.test.js
+++ backend/test/integration/domain.test.js
@@ -1,16 +1,19 @@
 const { expect } = require('chai');
+const { redis } = require('../../app/utils/redis');
 const domainHelpers = require('../../app/utils/helpers/domainHelpers');
+
+const redisClient = redis.getClient()
 
 describe('domainHelpers.checkMXStatus(domain)', () => {
   it('should return true', async () => {
     const validDomain = 'gmail.com';
-    const output = await domainHelpers.checkMXStatus(validDomain);
+    const output = await domainHelpers.checkMXStatus(redisClient, validDomain);
     expect(output[0]).to.be.true;
   });
 
   it('should return false', async () => {
     const invalidDomain = 'domain.lead122111miner.io';
-    const output = await domainHelpers.checkMXStatus(invalidDomain);
+    const output = await domainHelpers.checkMXStatus(redisClient, invalidDomain);
     expect(output[0]).to.be.false;
   });
 });
@@ -19,6 +22,7 @@ describe('domainHelpers.checkDomainStatus(emailAddress)', async () => {
   it('should return true', async () => {
     const emailAddressWithValidDomain = 'leadminer@gmail.com';
     const output = await domainHelpers.checkDomainStatus(
+      redisClient,
       emailAddressWithValidDomain
     );
     expect(output[0]).to.be.true;
@@ -28,6 +32,7 @@ describe('domainHelpers.checkDomainStatus(emailAddress)', async () => {
   it('should return false', async () => {
     const emailAddressWithInvalidDomain = 'domain.lead@122111miner.io';
     const output = await domainHelpers.checkDomainStatus(
+      redisClient,
       emailAddressWithInvalidDomain
     );
     expect(output[0]).to.be.false;
