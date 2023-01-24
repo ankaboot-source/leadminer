291b6ada6eeb504f845a3e26d183a0bdc9386610
Malek salem <malek.salem.14@gmail.com>
2023-01-24 11:36:33 +0100

    refactor and fix refresh_token bugs

--- backend/app/utils/helpers/tokenHelpers.js
+++ backend/app/utils/helpers/tokenHelpers.js
@@ -11,42 +11,59 @@ function getOAuthClient() {
 }
 
 /**
- * Uses the refreshToken to refresh an expired accessToken
- * @param {string} refreshToken - Stored token
- * @param {number} expirationDate - Expiration date of the token
- * @returns {Promise<object>} New access token and expiration
- */
-async function refreshAccessToken(refreshToken, expirationDate) {
+ * Refreshes an expired access token using a refresh token
+ * @param {string} refreshToken - The stored refresh token
+ * @returns {Promise<string>} The new access token
+*/
+async function refreshAccessToken(refreshToken) {
   const oauth2Client = getOAuthClient();
-
-  oauth2Client.setCredentials({
-    refreshToken
-  });
+  oauth2Client.setCredentials({ refresh_token: refreshToken });
 
   try {
-    const token = await oauth2Client.getAccessToken();
-    return {
-      access_token: token,
-      expiration: Math.floor(expirationDate / 1000)
-    };
+    let { token } = await oauth2Client.getAccessToken();
+
+    // Extract the access_token from the response data
+    if (token.res?.data) {
+      token = token.res.data.access_token;
+    }
+
+    oauth2Client.setCredentials({ token });
+    return token;
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error.message);
+  }
+}
+
+/**
+ * check for token validity and expiry date
+ * @param {Object} oauth2Client - an instance of the OAuth 2.0 client library
+ * @param {string} accessToken - access token
+ * @returns {boolean} - true if token is valid and not expired, false otherwise
+ */
+async function checkTokenValidity(accessToken) {
+  try {
+    const oauth2Client = getOAuthClient();
+    const tokenInfo = await oauth2Client.getTokenInfo(accessToken);
+    const now = new Date().getTime() / 1000;
+    if (tokenInfo.expires_at <= now) {
+      return false;
+    }
+    return true;
+  } catch (err) {
+    return false;
   }
 }
 
 /**
  * Generates an XOAuth2 token for the user to authenticate with the IMAP server.
- * @param {object} user - The user account data we're authenticating to.
- * @returns {Promise<object>} An object with two properties: xoauth2Token and newToken.
+ * @param {object} authData - An object containing the token, refresh token, and email of the user account.
+ * @returns {Promise<object>} An object containing the XOAuth2 token and the new token.
  */
 async function generateXOauthToken({ token, refreshToken, email }) {
   let accessToken = token;
 
-  const oauth2Client = getOAuthClient();
-  const { expiry_date } = await oauth2Client.getTokenInfo(accessToken);
-
-  if (Number(token.expiration) + 8 > Math.floor(expiry_date / 1000)) {
-    accessToken = await refreshAccessToken(refreshToken, expiry_date);
+  if (!await checkTokenValidity(token)) {
+    accessToken = await refreshAccessToken(refreshToken);
   }
 
   const xoauth2gen = xoauth2.createXOAuth2Generator({
@@ -55,6 +72,7 @@ async function generateXOauthToken({ token, refreshToken, email }) {
     clientSecret: googleClientSecret,
     accessToken
   });
+
   const authData = `user=${email}\x01auth=Bearer ${xoauth2gen.accessToken}\x01\x01`;
   const xoauth2_token = new Buffer.from(authData, 'utf-8').toString('base64');
 
