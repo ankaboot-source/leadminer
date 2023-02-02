f269358593d21c3f9c5514c25ecfd377a646647c
Malek salem <malek.salem.14@gmail.com>
2023-02-02 13:20:23 +0100

    505 fix function refined persons (#520)
    * update code and remove slow function call
    
    * restore old code
    
    * update code
    
    * fix bug
    
    * update function refined_persons
    
    * refactor function refined_persons

---
+++ supabase/migrations/20230130195016_refactor_and_add_recent_name.sql
@@ -0,0 +1,30 @@
+DROP FUNCTION IF EXISTS public.get_most_recent_name;
+
+CREATE OR REPLACE FUNCTION public.refined_persons(userid uuid) RETURNS void
+LANGUAGE plpgsql
+AS $function$
+DECLARE
+BEGIN
+  UPDATE public.refinedpersons
+  SET
+    engagement = subquery.engagement,
+    recency = subquery.recency,
+    occurence = subquery.occurence,
+    alternate_names = subquery.alternate_names,
+    name = subquery.name
+  FROM (
+    SELECT
+      poc.personid,
+      (array_agg(poc.name ORDER BY m.date desc))[1] AS name,
+      array_agg(distinct name) as alternate_names,
+      count(m.conversation) AS engagement,
+      max(m.date) AS recency,
+      count(*) AS occurence
+    FROM pointsofcontact poc
+      JOIN messages m ON poc.messageid = m.id
+    WHERE poc.userid = refined_persons.userid
+    GROUP BY poc.personid
+  ) subquery
+  WHERE refinedpersons.personid = subquery.personid;
+END;
+$function$;
