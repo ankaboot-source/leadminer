53e7ab92f557c4a0a1838f2abaa6d5113c0bf35e
Malek salem <malek.salem.14@gmail.com>
2023-02-13 16:51:12 +0100

    fix sse headers

--- backend/app/index.js
+++ backend/app/index.js
@@ -7,20 +7,13 @@ const { notFound } = require('./middleware/notFound');
 const { errorLogger } = require('./middleware/errorLogger');
 const { errorHandler } = require('./middleware/errorHandler');
 const imapRouter = require('./routes/imap.routes');
-const { sse } = require('./middleware/sse');
+const { sse, sseHeaders } = require('./middleware/sse');
 
 const app = express();
 
 initializeSentryIfNeeded(app);
 
 app.use(corsMiddleware);
-app.use((_, res, next) => {
-  res.setHeader('Connection', 'keep-alive');
-  res.setHeader('Content-Type', 'text/event-stream');
-  res.setHeader('Cache-Control', 'no-cache');
-  res.setHeader('X-Accel-Buffering', 'no');
-  next();
-});
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -34,7 +27,7 @@ app.get('/', (_, res) => {
   return res.json({ message: 'Welcome to leadminer application.' });
 });
 
-app.get('/api/stream', sse.init);
+// Get server logs
 app.get('/logs', (_, res, next) => {
   const filePath = path.resolve(__dirname, '..', 'logs/server.log');
 
@@ -48,6 +41,7 @@ app.get('/logs', (_, res, next) => {
   });
 });
 
+app.get('/api/stream', sseHeaders, sse.init);
 app.use('/api/imap', imapRouter);
 
 app.use(notFound);
