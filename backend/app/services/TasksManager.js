acd48bca4c0c107c934126b8b476464266441bd5
Malek salem <malek.salem.14@gmail.com>
2023-03-09 15:56:56 +0100

    move function to helpers

--- backend/app/services/TasksManager.js
+++ backend/app/services/TasksManager.js
@@ -2,20 +2,6 @@ const { generateUUID } = require('../utils/helpers/hashHelpers');
 const { logger } = require('../utils/logger');
 const { redis } = require('../utils/redis');
 
-/**
- * Sends a Server-Sent Event (SSE) to the specified client with the given data and event name.
- * @param {Object} sseClient - The SSE client to send the event to.
- * @param {string} sseData - The data to be sent as part of the SSE.
- * @param {string} sseEvent - The name of the event associated with the SSE.
- */
-function sendSSE(sseClient, sseData, sseEvent) {
-  try {
-    sseClient.send(sseData, sseEvent);
-  } catch (error) {
-    logger.error('Somthing happend when sending SSE', { metadata: { error } });
-  }
-}
-
 class TasksManager {
   /**
    * The Map of active mining tasks, with mining ID as the key and mining task object as the value.
