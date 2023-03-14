45aeea926436e3fa551f0cdc12ed5979050eeb3f
Chedly Zouche <chedly.zouche@pm.me>
2023-03-14 10:49:23 +0100

    Add script for fetching providers

---
+++ backend/fetchProviders.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+
+const DISPOSABLE_EMAIL_PROVIDERS_SAVE_LOCATION = path.join(
+  __dirname,
+  'app/utils/Disposable.json'
+);
+const DISPOSABLE_EMAIL_PROVIDERS_REMOTE_PATH =
+  'https://raw.githubusercontent.com/ankaboot-source/open-data/main/disposable-email-domains.json';
+
+const PUBLIC_EMAIL_PROVIDERS_REMOTE_PATH =
+  'https://raw.githubusercontent.com/ankaboot-source/open-data/main/public-email-providers.json';
+const PUBLIC_EMAIL_PROVIDERS_SAVE_LOCATION = path.join(
+  __dirname,
+  'app/utils/FreeProviders.json'
+);
+
+function fetchRemoteFile(remotePath, saveLocation) {
+  const file = fs.createWriteStream(saveLocation);
+
+  return new Promise((resolve, reject) => {
+    https.get(remotePath, (response) => {
+      const stream = response.pipe(file);
+
+      stream.on('finish', () => {
+        resolve();
+      });
+
+      stream.on('error', (error) => {
+        reject(`Failed retrieving file from remote: ${error}`);
+      });
+    });
+  });
+}
+
+(async () => {
+  await Promise.all([
+    fetchRemoteFile(
+      DISPOSABLE_EMAIL_PROVIDERS_REMOTE_PATH,
+      DISPOSABLE_EMAIL_PROVIDERS_SAVE_LOCATION
+    ),
+    fetchRemoteFile(
+      PUBLIC_EMAIL_PROVIDERS_REMOTE_PATH,
+      PUBLIC_EMAIL_PROVIDERS_SAVE_LOCATION
+    )
+  ]);
+})();
