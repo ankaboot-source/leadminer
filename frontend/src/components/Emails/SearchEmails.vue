65087766f9ca2c294ca3f37f4e7ee36ae61ef8ce
Jaafoura <mehdi.jaafourag@gmail.com>
2023-04-01 01:31:35 +0100

    Update SearchEmails.vue

--- frontend/src/components/Emails/SearchEmails.vue
+++ frontend/src/components/Emails/SearchEmails.vue
@@ -2,53 +2,63 @@
   <div class="row q-col-gutter-sm">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
       <div class="row q-pa-lg">
-        <div class="q-mr-sm col-lg-5 col-md-5 q-ma-sm">
-          <q-card flat bordered>
-            <div class="row q-pa-sm">
-              <div class="bg-grey-2 border q-pa-sm col-lg-12 col-md-6">
-                <div class="text-bold col-lg-5 col-md-5">
-                  Select mailbox folders
-                  <q-btn
-                    outline
-                    round
-                    size="sm"
-                    color="orange-5"
-                    icon="refresh"
-                    @click="getBoxes"
-                  />
-                </div>
-                <TreeCard
-                  v-if="boxes.length > 0"
-                  :boxes="boxes"
-                  :scanned-boxes="scannedBoxes"
-                  :class="{ disabled: activeMiningTask }"
-                  @selected-boxes="updateSelectedBoxes"
-                />
-                <q-spinner-tail v-else color="teal" size="4em" />
-              </div>
-              <div class="column col-lg-8">
-                <div class="col-6" />
-                <div class="q-mt-md q-ml-lg col-6">
-                  <q-btn
-                    :disable="activeMiningTask"
-                    no-caps
-                    :color="activeMiningTask ? 'grey-6' : 'red'"
-                    label="Start"
-                    @click="startMining"
-                  />
-                  <q-btn
-                    :disable="!activeMiningTask"
-                    class="q-ma-md"
-                    no-caps
-                    :color="activeMiningTask ? 'red' : 'grey-6'"
-                    label="Stop"
-                    @click="stopMining"
-                  />
-                </div>
-              </div>
+        <q-card flat bordered class="q-mr-sm q-ma-sm">
+          <div class="row q-pa-sm q-pt-md q-pl-lg">
+            <div>
+              <q-btn
+                :disable="activeMiningTask"
+                :color="activeMiningTask ? 'grey-6' : 'teal'"
+                label="Get To Mining!"
+                @click="startMining"
+                no-caps
+                unelevated
+              />
+              <q-btn
+                :disable="!activeMiningTask"
+                :color="activeMiningTask ? 'red' : 'grey-6'"
+                label="Have a rest"
+                @click="stopMining"
+                no-caps
+                outline
+                class="q-ma-md"
+              />
+              <br />
+              <a
+                label="Advanced options"
+                @click="mailbox = true"
+                :disable="activeMiningTask"
+                class="cursor-pointer q-hoverable text-teal-6"
+                style="text-decoration: underline"
+              >
+                Advanced options
+              </a>
             </div>
-          </q-card>
-        </div>
+          </div>
+          <q-dialog v-model="mailbox">
+            <div class="bg-grey-2 border q-pa-sm col-lg-12 col-md-6">
+              <div class="text-bold col-lg-5 col-md-5">
+                Select mailbox folders
+                <q-btn
+                  outline
+                  round
+                  size="sm"
+                  color="orange-5"
+                  icon="refresh"
+                  @click="getBoxes"
+                />
+              </div>
+              <TreeCard
+                v-if="boxes.length > 0"
+                :boxes="boxes"
+                :scanned-boxes="scannedBoxes"
+                :class="{ disabled: activeMiningTask }"
+                @selected-boxes="updateSelectedBoxes"
+                default-expand-all
+              />
+              <q-spinner-tail v-else color="teal" size="4em" />
+            </div>
+          </q-dialog>
+        </q-card>
         <div class="bg-transparent col q-ma-sm">
           <ProgressCard
             v-if="boxes"
@@ -79,6 +89,7 @@ const selectedBoxes = ref([]);
 const $q = useQuasar();
 const $store = useStore();
 const $router = useRouter();
+const mailbox = ref(false);
 
 onMounted(async () => {
   const googleUser = LocalStorage.getItem("googleUser");
