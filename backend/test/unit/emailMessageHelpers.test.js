2ab1eef283a42f3299fb48c3a1adb4d1352d0b11
Malek salem <malek.salem.14@gmail.com>
2023-04-04 16:12:48 +0100

    move unittests

--- backend/test/unit/emailMessageHelpers.test.js
+++ backend/test/unit/emailMessageHelpers.test.js
@@ -48,3 +48,24 @@ describe('emailMessageHelpers.getSpecificHeader', () => {
     });
   });
 });
+
+describe('emailMessageHelpers.getMessageId', () => {
+  it('should return the message ID if it exists in the parsed header', () => {
+    const parsedHeader = {
+      'message-id': ['12345'],
+      'return-path': ['test@example.com'],
+      date: ['2022-01-01']
+    };
+    const messageId = emailMessageHelpers.getMessageId(parsedHeader);
+    expect(messageId).to.equal('12345');
+  });
+
+  it('should generate a pseudo message ID if the parsed header does not have one', () => {
+    const parsedHeader = {
+      'return-path': ['test@example.com'],
+      date: ['2022-01-01']
+    };
+    const messageId = emailMessageHelpers.getMessageId(parsedHeader);
+    expect(messageId).to.match(/^UNKNOWN \d+@example\.com$/);
+  });
+});
