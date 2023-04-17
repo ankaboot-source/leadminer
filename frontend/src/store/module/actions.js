144712cbeacd528e2d3c968377b9fd8769bf0c83
Malek salem <malek@ankaboot.io>
2023-04-17 11:05:46 +0100

    fix "OK" error message bug

--- frontend/src/store/module/actions.js
+++ frontend/src/store/module/actions.js
@@ -174,8 +174,9 @@ export async function signIn({ state, commit }, { data }) {
 
     return response.data;
   } catch (error) {
-    commit("SET_ERROR", error?.response.data.error.message);
-    throw new Error(error.message);
+    const err = (error?.response?.data?.error?.message || error?.message)
+    commit("SET_ERROR", err);
+    throw new Error(err.message);
   }
 }
 
