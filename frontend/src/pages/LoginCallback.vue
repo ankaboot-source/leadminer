16793bca637651cd71eb3e63c002a40b62f22a69
Malek salem <malek@ankaboot.io>
2023-04-28 11:24:13 +0100

    add callback page

---
+++ frontend/src/pages/LoginCallback.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "LoginCallback",
+  created() {
+    // Extract token and other data from query parameters
+
+    const { id, email, accessToken } = this.$route.query;
+
+    // Store token and other data in Vuex store or local storage
+    this.$store.commit("leadminer/SET_OAUTH_USER", { id, email, accessToken });
+    localStorage.setItem(
+      "oauthUser",
+      JSON.stringify(this.$store.state.leadminer.oauthUser)
+    );
+    // Redirect user to desired page
+    this.$router.push("/dashboard");
+  },
+};
+</script>
