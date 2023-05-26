5727070809e411d4589687e33c9370ff8b25aaee
Malek salem <malek@ankaboot.io>
2023-05-26 12:46:30 +0100

    fix bug

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -18,11 +18,7 @@ const routes: RouteRecordRaw[] = [
   {
     path: "/:catchAll(.*)*",
     component: () => import("pages/ErrorNotFound.vue"),
-  },
-  {
-    path: "/login/callback",
-    component: () => import("src/pages/LoginCallback.vue"),
-  },
+  }
 ];
 
 export default routes;
