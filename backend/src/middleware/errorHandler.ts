49e3f4fd16a238d35ff931dcc3ecc800e6bce964
Malek salem <malek@ankaboot.io>
2023-06-06 01:26:25 +0100

    add more typing

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -2,15 +2,21 @@ import { NextFunction, Request, Response } from 'express';
 import ENV from '../config';
 
 export default function errorHandler(
-  error: Error,
+  error: any,
   _req: Request,
   res: Response,
   next: NextFunction // eslint-disable-line @typescript-eslint/no-unused-vars
 ) {
   const code = res.statusCode !== 200 ? res.statusCode : 500;
-  const response: { error: { message: string; stack?: string } } = {
+  const response: {
+    error: { message: string; stack?: string; errors?: Record<string, string> };
+  } = {
     error: { message: error.message }
   };
+
+  if (error.errors) {
+    response.error.errors = error.errors;
+  }
 
   if (ENV.NODE_ENV === 'development') {
     if (error.stack) {
