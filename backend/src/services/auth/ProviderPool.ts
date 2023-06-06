24a923bf99b6c9e33438b222d8a854716f7e8902
Malek salem <malek@ankaboot.io>
2023-06-06 23:26:11 +0100

    refactor & add MSN conf

--- backend/src/services/auth/ProviderPool.ts
+++ backend/src/services/auth/ProviderPool.ts
@@ -1,19 +1,32 @@
 import ENV from '../../config';
 import ProviderPool, { ProviderConfig } from './Provider';
 
+const GOOGLE_OAUTH_CONFIG = {
+  issuerURL: 'https://accounts.google.com',
+  authorizationURL: 'https://accounts.google.com/o/oauth2/v2/auth',
+  userInfoURL: 'https://openidconnect.googleapis.com/v1/userinfo',
+  jwkURI: 'https://www.googleapis.com/oauth2/v3/certs',
+  tokenURL: 'https://oauth2.googleapis.com/token',
+  clientID: ENV.GOOGLE_CLIENT_ID,
+  clientSecret: ENV.GOOGLE_SECRET,
+  scopes: ['https://mail.google.com/']
+};
+const AZURE_OAUTH_CONFIG = {
+  issuerURL:
+    'https://login.microsoftonline.com/9188040d-6c67-4c5b-b112-36a304b66dad/v2.0',
+  authorizationURL:
+    'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
+  userInfoURL: 'https://graph.microsoft.com/oidc/userinfo',
+  jwkURI: 'https://login.microsoftonline.com/common/discovery/v2.0/keys',
+  tokenURL: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+  clientID: ENV.AZURE_CLIENT_ID,
+  clientSecret: ENV.AZURE_SECRET,
+  scopes: []
+};
 const providersConfigs: ProviderConfig[] = [
   {
     name: 'google',
-    oauthConfig: {
-      issuerURL: 'https://accounts.google.com',
-      authorizationURL: 'https://accounts.google.com/o/oauth2/v2/auth',
-      userInfoURL: 'https://openidconnect.googleapis.com/v1/userinfo',
-      jwkURI: 'https://www.googleapis.com/oauth2/v3/certs',
-      tokenURL: 'https://oauth2.googleapis.com/token',
-      clientID: ENV.GOOGLE_CLIENT_ID,
-      clientSecret: ENV.GOOGLE_SECRET,
-      scopes: ['https://mail.google.com/']
-    },
+    oauthConfig: GOOGLE_OAUTH_CONFIG,
     imapConfig: {
       name: 'google',
       host: 'imap.gmail.com',
@@ -23,24 +36,24 @@ const providersConfigs: ProviderConfig[] = [
   },
   {
     name: 'azure',
-    oauthConfig: {
-      issuerURL:
-        'https://login.microsoftonline.com/9188040d-6c67-4c5b-b112-36a304b66dad/v2.0',
-      authorizationURL:
-        'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
-      userInfoURL: 'https://graph.microsoft.com/oidc/userinfo',
-      jwkURI: 'https://login.microsoftonline.com/common/discovery/v2.0/keys',
-      tokenURL: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
-      clientID: ENV.AZURE_CLIENT_ID,
-      clientSecret: ENV.AZURE_SECRET,
-      scopes: []
-    },
+    oauthConfig: AZURE_OAUTH_CONFIG,
     imapConfig: {
       name: 'azure',
       host: 'outlook.office365.com',
       port: 993
     },
-    domains: ['outlook', 'hotmail', 'live', 'msn']
+    domains: ['outlook', 'hotmail', 'live', 'windowslive', 'dbmail']
+  },
+  {
+    // Microsoft MSN configuration
+    name: 'azure',
+    oauthConfig: AZURE_OAUTH_CONFIG,
+    imapConfig: {
+      name: 'azure',
+      host: 'mail.outlook.com',
+      port: 993
+    },
+    domains: ['msn']
   }
 ];
 
