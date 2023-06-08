fa052d003c9292dc34c967e661f0e748a6aee2a3
Malek salem <malek@ankaboot.io>
2023-06-08 16:45:30 +0100

    add router navigation guard

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,9 +1,18 @@
-import { RouteRecordRaw } from "vue-router";
+import { LocalStorage } from "quasar";
+import { RouteLocationNormalized, RouteRecordRaw } from "vue-router";
+
+function authGuard(to: RouteLocationNormalized) {
+  if (!LocalStorage.has("user") && !to.hash) {
+    return "/";
+  }
+  return true;
+}
 
 const routes: RouteRecordRaw[] = [
   {
     path: "/dashboard",
     component: () => import("layouts/MainLayout.vue"),
+    beforeEnter: authGuard,
     children: [
       { path: "", component: () => import("src/pages/DashboardPage.vue") },
     ],
