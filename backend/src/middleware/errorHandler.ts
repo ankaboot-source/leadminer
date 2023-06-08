3387401bd1e0c9a184bf8d98f2b96e3c3bc4f095
Malek salem <malek@ankaboot.io>
2023-06-08 10:48:15 +0100

    update errorHandler

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,32 +1,28 @@
 import { NextFunction, Request, Response } from 'express';
 import ENV from '../config';
-
-interface CustomError extends Error {
-  errors?: Record<string, string>;
-}
+import { ErrorResponse, ImapAuthError } from '../utils/errors';
 
 export default function errorHandler(
-  error: CustomError,
+  error: Error,
   _req: Request,
   res: Response,
   next: NextFunction // eslint-disable-line @typescript-eslint/no-unused-vars
 ) {
   const code = res.statusCode !== 200 ? res.statusCode : 500;
-  const response: {
-    error: { message: string; stack?: string; errors?: Record<string, string> };
-  } = {
-    error: { message: error.message }
-  };
 
-  if (error.errors) {
-    response.error.errors = error.errors;
-  }
+  const response: ErrorResponse = {
+    message: error.message
+  };
 
   if (ENV.NODE_ENV === 'development') {
     if (error.stack) {
-      response.error.stack = error.stack;
+      response.stack = error.stack;
     }
   }
 
-  return res.status(code).send(response);
+  if (error instanceof ImapAuthError && error.fieldErrors) {
+    response.details = error.fieldErrors;
+  }
+
+  return res.status(code).send({ error: response });
 }
