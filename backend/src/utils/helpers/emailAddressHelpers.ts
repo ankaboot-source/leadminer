446ff3c2dcdc45ea627d8131a7b765969e673973
Malek salem <malek@ankaboot.io>
2023-06-15 14:45:27 +0100

    remove emailTagging function

--- backend/src/utils/helpers/emailAddressHelpers.ts
+++ backend/src/utils/helpers/emailAddressHelpers.ts
@@ -1,4 +1,4 @@
-import { DomainType, Tag } from '../../services/tagging/types';
+import { DomainType } from '../../services/tagging/types';
 import {
   NEWSLETTER_EMAIL_ADDRESS_INCLUDES,
   NOREPLY_EMAIL_ADDRESS_INCLUDES,
@@ -54,45 +54,3 @@ export function isNewsletter(emailAddress: string) {
   );
 }
 
-/**
- * Tags an email address.
- * @param email - The email to check.
- * @param email.address - The email address.
- * @param email.name - The user name.
- * @param domainType - The type of domain, it can be either "provider" or "custom"
- * @returns List of tags
- */
-export function getEmailTags(
-  { address, name }: { address: string; name: string },
-  domainType: DomainType
-): Tag[] {
-  const emailTags: Tag[] = [];
-
-  const emailType = findEmailAddressType(domainType);
-
-  if (isNoReply(address)) {
-    emailTags.push({ name: 'no-reply', reachable: 0, source: 'refined' });
-  }
-
-  if (isTransactional(address)) {
-    emailTags.push({
-      name: 'transactional',
-      reachable: 0,
-      source: 'refined'
-    });
-  }
-
-  if (isNewsletter(address) || name?.includes('newsletter')) {
-    emailTags.push({ name: 'newsletter', reachable: 2, source: 'refined' });
-  }
-
-  if (emailType !== '') {
-    emailTags.push({
-      name: emailType,
-      reachable: 1,
-      source: 'refined'
-    });
-  }
-
-  return emailTags;
-}
