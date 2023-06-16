173fb6688b1bf23165ab871ba80c90d224dbd721
Malek salem <malek@ankaboot.io>
2023-06-16 09:50:12 +0100

    add newsletter to tags

--- backend/src/services/tagging/index.ts
+++ backend/src/services/tagging/index.ts
@@ -1,11 +1,13 @@
 import groupEmailMessage from './group';
 import linkedinEmailMessage from './linkedin';
+import newsletterEmailMessage from './newsletter';
 import transactionalEmailMessage from './transactional';
 import { EmailMessageTagExtractor } from './types';
 
 const messageTaggingRules: EmailMessageTagExtractor[] = [
-  groupEmailMessage,
   linkedinEmailMessage,
+  newsletterEmailMessage,
+  groupEmailMessage,
   transactionalEmailMessage // Always keep transactional as the last rule to check
 ];
 
