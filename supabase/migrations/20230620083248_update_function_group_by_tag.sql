b996b9f03d3e95a0ae71a60fdedf3a93adebb78b
Malek salem <malek@ankaboot.io>
2023-06-20 14:48:23 +0100

    fix SQL bug filtering by userid

--- supabase/migrations/20230620083248_update_function_group_by_tag.sql
+++ supabase/migrations/20230620083248_update_function_group_by_tag.sql
@@ -19,6 +19,7 @@ BEGIN
     array_agg(name) AS tags,
     array_agg(reachable) AS tags_reachability
   FROM tags
+  WHERE user_id = _userid
   GROUP BY person_email
   HAVING
     COUNT(DISTINCT name) = 1
