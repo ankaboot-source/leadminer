e90f803b0dc7aeee45af1b8f9016de6f237c32bf
Chedly Zouche <chedly.zouche@pm.me>
2023-07-18 09:55:16 +0100

    Setup reacher api client (#1017)
    * Setup reacher api client
    
    * Remove null assertion
    
    * Fix dev dependency

---
+++ backend/src/services/email-status/reacher/mappers.ts
@@ -0,0 +1,50 @@
+import { EmailStatusResult, Status } from '../EmailStatusVerifier';
+import { EmailCheckOutput } from './client';
+
+export function reachableToEmailStatus(
+  isReachable: 'invalid' | 'unknown' | 'safe' | 'risky'
+): Status {
+  switch (isReachable) {
+    case 'invalid':
+      return Status.INVALID;
+    case 'unknown':
+      return Status.UNKNOWN;
+    case 'safe':
+      return Status.VALID;
+    case 'risky':
+      return Status.RISKY;
+    default:
+      return Status.UNKNOWN;
+  }
+}
+
+export function reacherResultToEmailStatus(
+  reacherResult: EmailCheckOutput
+): EmailStatusResult {
+  return {
+    email: reacherResult.input,
+    status: reachableToEmailStatus(reacherResult.is_reachable),
+    details: {
+      isRole:
+        'is_role_account' in reacherResult.misc
+          ? reacherResult.misc.is_role_account
+          : undefined,
+      isDisposable:
+        'is_disposable' in reacherResult.misc
+          ? reacherResult.misc.is_disposable
+          : undefined,
+      isDisabled:
+        'is_disabled' in reacherResult.smtp
+          ? reacherResult.smtp.is_disabled
+          : undefined,
+      isCatchAll:
+        'is_catch_all' in reacherResult.smtp
+          ? reacherResult.smtp.is_catch_all
+          : undefined,
+      isDeliverable:
+        'is_deliverable' in reacherResult.smtp
+          ? reacherResult.smtp.is_deliverable
+          : undefined
+    }
+  };
+}
---
+++ backend/src/utils/axios.ts
@@ -0,0 +1,19 @@
+import { AxiosError } from 'axios';
+
+// eslint-disable-next-line import/prefer-default-export
+export function handleAxiosError<T>(axiosError: AxiosError<T>): {
+  error: Error;
+} {
+  if (axiosError.response) {
+    // Request was made, but the server responded with an error status
+    return {
+      error: new Error('Error received from verification server')
+    };
+  }
+  if (axiosError.request) {
+    // Request was made, but no response was received
+    return { error: new Error('No response received from server') };
+  }
+  // Something else happened, such as a network error
+  return { error: new Error('A network error occurred') };
+}
