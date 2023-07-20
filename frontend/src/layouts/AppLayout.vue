ffe6acc851272bde4b903ed81cc2a73a0fa72afe
Chedly Zouche <chedly.zouche@pm.me>
2023-07-20 16:57:41 +0100

    Remove class prop

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -2,15 +2,11 @@
   <q-layout view="lHh Lpr lFf">
     <app-header />
     <q-page-container>
-      <q-page :class="pageClass" padding><slot></slot></q-page>
+      <q-page padding><slot></slot></q-page>
     </q-page-container>
   </q-layout>
 </template>
 
 <script setup lang="ts">
 import AppHeader from "src/components/AppHeader.vue";
-
-defineProps<{
-  pageClass?: string;
-}>();
 </script>
