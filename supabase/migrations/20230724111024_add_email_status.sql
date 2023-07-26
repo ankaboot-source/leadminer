fa2f222ba1e21be31eba9fbdab80c9c533aeb1d8
Chedly Zouche <chedly.zouche@pm.me>
2023-07-26 10:33:18 +0100

    Update migration

--- supabase/migrations/20230724111024_add_email_status.sql
+++ supabase/migrations/20230724111024_add_email_status.sql
@@ -1,9 +1,10 @@
+-- Adds a new column 'status' to 'persons' and 'refinedpersons' tables
+
 alter table "public"."persons" add column "status" text not null default 'UNKNOWN'::text;
 
 alter table "public"."refinedpersons" add column "status" text;
 
 -- UPDATE POPULATE FUNCTION
-DROP FUNCTION public.populate_refined;
 CREATE OR REPLACE FUNCTION public.populate_refined(_userid uuid) RETURNS void
 LANGUAGE plpgsql
 AS $function$
@@ -12,7 +13,7 @@ BEGIN
     SELECT populate_refined._userid, t.tags, p.name, p.email, p.status
     FROM public.persons p
     INNER JOIN public.get_grouped_tags_by_person(_userid) AS t ON t.email = p.email
-    WHERE p.user_id=populate_refined._userid AND NOT t.tags_reachability && '{0}'
+    WHERE p.user_id=populate_refined._userid
     ON conflict(email, userid) do nothing;
 END;
 $function$;
