f08e125bf36cd122acf852f0530831b198e46e92
Malek salem <malek.salem.14@gmail.com>
2023-08-17 09:37:10 +0100

    move export csv to backend (#1092)
    * add new endpoint
    
    * add get contacts + refinedpersons from db
    
    * add csv helpers
    
    * add csv package
    
    * remove csv package
    
    * remove csv parsing from frontend
    
    * update exportCsv logic
    
    * remove any
    
    * Fix deepsource JS-W1043
    
    * fix formatting
    
    * update unitest

---
+++ backend/src/controllers/contacts.controller.ts
@@ -0,0 +1,61 @@
+import { NextFunction, Request, Response } from 'express';
+import { User } from '@supabase/supabase-js';
+import { Contacts } from '../db/Contacts';
+import getCsvStr from '../utils/helpers/csv';
+
+export default function initializeContactsController(contacts: Contacts) {
+  return {
+    async exportContactsCSV(req: Request, res: Response, next: NextFunction) {
+      const user = res.locals.user as User;
+      try {
+        const minedContacts = await contacts.getContactsTable(user.id);
+
+        if (!minedContacts || minedContacts.length === 0) {
+          return res
+            .status(404)
+            .json({ message: 'No contacts available for export' });
+        }
+
+        const csvSeparator = String(req.query.delimiter ?? ',');
+
+        const csvData = minedContacts.map((contact) => ({
+          name: contact.name?.trim(),
+          email: contact.email,
+          recency: contact.recency
+            ? new Date(contact.recency).toISOString().slice(0, 10)
+            : '',
+          seniority: contact.seniority
+            ? new Date(contact.seniority).toISOString().slice(0, 10)
+            : '',
+          occurrence: contact.occurrence,
+          sender: contact.sender,
+          recipient: contact.recipient,
+          conversations: contact.conversations,
+          repliedConversations: contact.replied_conversations,
+          tags: contact.tags?.join('\n'),
+          status: contact.status
+        }));
+        const csvStr = await getCsvStr(
+          [
+            { key: 'name', header: 'Name' },
+            { key: 'email', header: 'Email' },
+            { key: 'recency', header: 'Recency' },
+            { key: 'seniority', header: 'Seniority' },
+            { key: 'occurrence', header: 'Occurrence' },
+            { key: 'sender', header: 'Sender' },
+            { key: 'recipient', header: 'Recipient' },
+            { key: 'conversations', header: 'Conversations' },
+            { key: 'repliedConversations', header: 'Replied conversations' },
+            { key: 'tags', header: 'Tags' },
+            { key: 'status', header: 'Status' }
+          ],
+          csvData,
+          csvSeparator
+        );
+        return res.header('Content-Type', 'text/csv').status(200).send(csvStr);
+      } catch (err) {
+        return next(err);
+      }
+    }
+  };
+}
