3c182ca9155fa98ba2a763667b701e8433119b12
Malek salem <malek@ankaboot.io>
2023-08-28 18:59:14 +0100

    add basic tests for createCreditHandler

---
+++ backend/test/unit/credits.test.ts
@@ -0,0 +1,43 @@
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { createCreditHandler } from '../../src/utils/billing/credits';
+import { Users } from '../../src/db/interfaces/Users';
+
+describe('createCreditHandler', () => {
+  let userResolverMock: Users;
+
+  beforeEach(() => {
+    userResolverMock = {
+      getUserProfile: jest.fn(() => Promise.resolve(undefined)),
+      updateUserProfile: jest.fn(() => Promise.resolve(undefined)),
+      deleteUser: jest.fn(() => Promise.resolve(undefined)),
+      deleteUserData: jest.fn(() => Promise.resolve(undefined))
+    };
+  });
+
+  describe('createCreditHandler setup', () => {
+    it('should return undefined when enable is false', () => {
+      const creditHandler = createCreditHandler(false, 10, userResolverMock);
+      expect(creditHandler).toBeUndefined();
+    });
+
+    it('should return undefined when creditsPerUnit is undefined', () => {
+      const creditHandler = createCreditHandler(
+        true,
+        undefined,
+        userResolverMock
+      );
+      expect(creditHandler).toBeUndefined();
+    });
+
+    it('should return validateCreditUsage and deductCredits methods when passing correct values', () => {
+      const handler = createCreditHandler(true, 10, userResolverMock);
+
+      if (handler === undefined) {
+        throw new Error('Handler is undefined');
+      }
+
+      expect(typeof handler.validateCreditUsage).toBe('function');
+      expect(typeof handler.deductCredits).toBe('function');
+    });
+  });
+});
