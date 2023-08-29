1e6cecb02a9ccc40f512f3c7d2c76eae978c22d4
Malek salem <malek@ankaboot.io>
2023-08-29 07:48:42 +0100

    add unit tests

--- backend/test/unit/credits.test.ts
+++ backend/test/unit/credits.test.ts
@@ -1,6 +1,7 @@
 import { beforeEach, describe, expect, it, jest } from '@jest/globals';
 import { createCreditHandler } from '../../src/utils/billing/credits';
 import { Users } from '../../src/db/interfaces/Users';
+import { Profile } from '../../src/db/types';
 
 describe('createCreditHandler', () => {
   let userResolverMock: Users;
@@ -41,5 +42,52 @@ describe('createCreditHandler', () => {
     });
   });
 
-  // TODO: Implement tests validateCreditUsage and deductCredits using mocks
+  describe('createCreditHandler.validateCreditUsage', () => {
+    it('should return insufficientCredits when userCredits < CREDITS_PER_UNIT', async () => {
+      userResolverMock.getUserProfile = jest.fn(() => Promise.resolve({ credits: 5 } as Profile))
+      const creditHandler = createCreditHandler(true, 10, userResolverMock);
+
+      const result = await creditHandler?.validateCreditUsage('userId', 3);
+
+      expect(result?.insufficientCredits).toBe(true);
+      expect(result?.requestedUnits).toBe(3);
+      expect(result?.availableUnits).toBe(3);
+    });
+
+    it('should return availableUnits when userCredits >= CREDITS_PER_UNIT * units', async () => {
+      userResolverMock.getUserProfile = jest.fn(() => Promise.resolve({ credits: 30 } as Profile))
+      const creditHandler = createCreditHandler(true, 10, userResolverMock);
+
+      const result = await creditHandler?.validateCreditUsage('userId', 3);
+
+      expect(result?.insufficientCredits).toBe(false);
+      expect(result?.requestedUnits).toBe(3);
+      expect(result?.availableUnits).toBe(3);
+    });
+
+
+    it('should return availableUnits when userCredits < CREDITS_PER_UNIT * units', async () => {
+      userResolverMock.getUserProfile = jest.fn(() => Promise.resolve({ credits: 20 } as Profile))
+      const creditHandler = createCreditHandler(true, 10, userResolverMock);
+
+      const result = await creditHandler?.validateCreditUsage('userId', 3);
+
+      expect(result?.insufficientCredits).toBe(false);
+      expect(result?.requestedUnits).toBe(3);
+      expect(result?.availableUnits).toBe(2);
+    });
+
+  });
+
+  describe('createCreditHandler.deductCredits', () => {
+    it('should return true if profile udpated successfully', async () => {
+      userResolverMock.getUserProfile = () => Promise.resolve({ credits: 30 } as Profile);
+      userResolverMock.updateUserProfile = () => Promise.resolve(true);
+
+      const creditHandler = createCreditHandler(true, 10, userResolverMock);
+      const result = await creditHandler?.deductCredits('userId', 2);
+
+      expect(result).toBe(true);
+    });
+  });
 });
