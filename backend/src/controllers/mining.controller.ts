98add7b44a08d19422131dabe4e57cf8a4c4f463
Malek salem <malek@ankaboot.io>
2023-09-28 14:34:40 +0100

    fix formatting

--- backend/src/controllers/mining.controller.ts
+++ backend/src/controllers/mining.controller.ts
@@ -334,7 +334,6 @@ export default function initializeMiningController(
         } else {
           miningTask = await tasksManager.createTask(imapEmailsFetcherOptions);
         }
-
       } catch (err) {
         if (imapConnection) {
           await imapConnectionProvider.releaseConnection(imapConnection);
