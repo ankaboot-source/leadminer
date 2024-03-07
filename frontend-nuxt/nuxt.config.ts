388740ad04ddcdf4cc4be140ad18c88d5825eb63
Malek salem <malek@ankaboot.io>
2024-03-07 12:34:14 +0100

    frontend-nuxt-blueprint

---
+++ frontend-nuxt/nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-primevue",
+    '@nuxtjs/supabase'
+  ],
+  supabase: {
+    redirect: false
+  },
+  primevue: {
+    cssLayerOrder: "tailwind-base, primevue, tailwind-utilities",
+    components: {
+      exclude: ["Editor", "Chart"]
+    }
+  },
+  css: [
+    "primevue/resources/themes/aura-light-amber/theme.css"
+  ]
+});
