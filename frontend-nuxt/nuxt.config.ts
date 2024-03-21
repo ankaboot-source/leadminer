048a7a2d84c3667107d1bf47db72ac20363bb8e8
Malek salem <malek@ankaboot.io>
2024-03-21 23:09:12 +0100

    add vue-tsc for cli type-checking

--- frontend-nuxt/nuxt.config.ts
+++ frontend-nuxt/nuxt.config.ts
@@ -1,12 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  srcDir: 'src',
+  imports: {
+    autoImport: true,
+  },
+  eslint: {
+    lintOnStart: false,
+  },
   $development: {
     devtools: { enabled: true },
     devServer: {
       port: 8082,
     },
+    typescript: {
+      // Enable after removing quasar
+      typeCheck: false,
+    },
   },
-  srcDir: 'src',
+  runtimeConfig: {
+    public: {
+      SERVER_ENDPOINT: process.env.SERVER_ENDPOINT,
+      // Analytics
+      POSTHOG_API_KEY: process.env.POSTHOG_API_KEY,
+      POSTHOG_INSTANCE_ADDRESS: process.env.POSTHOG_INSTANCE_ADDRESS,
+      // UI
+      AVERAGE_EXTRACTION_RATE: process.env.AVERAGE_EXTRACTION_RATE,
+      // Credits
+      ENABLE_CREDIT: process.env.ENABLE_CREDIT,
+      EXTERNAL_REFILL_CREDITS_LINK: process.env.EXTERNAL_REFILL_CREDITS_LINK,
+    },
+  },
   modules: [
     '@nuxt/test-utils/module',
     '@nuxtjs/eslint-module',
@@ -23,11 +46,13 @@ export default defineNuxtConfig({
       exclude: ['Editor', 'Chart'],
     },
   },
-  css: [
-    'primeicons/primeicons.css',
-    'primevue/resources/themes/aura-light-indigo/theme.css',
-    '~/assets/css/app.scss',
-  ],
+  quasar: {
+    plugins: ['Notify'],
+    sassVariables: '~/assets/css/quasar.variables.scss',
+    extras: {
+      fontIcons: ['material-icons', 'mdi-v5', 'fontawesome-v5'],
+    },
+  },
   i18n: {
     locales: ['en', 'fr'],
     defaultLocale: 'en',
@@ -48,13 +73,11 @@ export default defineNuxtConfig({
       },
     },
   },
-  quasar: {
-    plugins: ['Notify'],
-    sassVariables: '~/assets/css/quasar.variables.scss',
-    extras: {
-      fontIcons: ['material-icons', 'mdi-v5', 'fontawesome-v5'],
-    },
-  },
+  css: [
+    'primeicons/primeicons.css',
+    'primevue/resources/themes/aura-light-indigo/theme.css',
+    '~/assets/css/app.scss',
+  ],
   supabase: {
     url: process.env.SUPABASE_PROJECT_URL,
     key: process.env.SUPABASE_ANON_KEY,
@@ -71,21 +94,5 @@ export default defineNuxtConfig({
       login: '/auth/login',
       exclude: ['/auth/signup', '/auth/forgot-password'],
     },
-  },
-  runtimeConfig: {
-    public: {
-      SERVER_ENDPOINT: process.env.SERVER_ENDPOINT,
-      // Analytics
-      POSTHOG_API_KEY: process.env.POSTHOG_API_KEY,
-      POSTHOG_INSTANCE_ADDRESS: process.env.POSTHOG_INSTANCE_ADDRESS,
-      // UI
-      AVERAGE_EXTRACTION_RATE: process.env.AVERAGE_EXTRACTION_RATE,
-      // Credits
-      ENABLE_CREDIT: process.env.ENABLE_CREDIT,
-      EXTERNAL_REFILL_CREDITS_LINK: process.env.EXTERNAL_REFILL_CREDITS_LINK,
-    },
-  },
-  imports: {
-    autoImport: true,
   },
 });
