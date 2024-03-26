7c5076ce87077e60461b71861fb4ee4a1703a171
Malek salem <malek@ankaboot.io>
2024-03-26 23:39:31 +0100

    update switching logic & add unittests (#1220)

--- backend/test/unit/email-status/EmailStatusVerifierFactory.test.ts
+++ backend/test/unit/email-status/EmailStatusVerifierFactory.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, jest } from '@jest/globals';
+import { describe, expect, it, jest, beforeEach } from '@jest/globals';
 import EmailStatusVerifierFactory from '../../../src/services/email-status/EmailStatusVerifierFactory';
 import MailerCheckEmailStatusVerifier from '../../../src/services/email-status/mailercheck';
 import RandomEmailStatusVerifier from '../../../src/services/email-status/random';
@@ -20,87 +20,100 @@ describe('getVerifier', () => {
   };
 
   const outlookEmails = [
-    'user1@outlook.com',
-    'john.doe@hotmail.co.uk',
-    'jane_smith@live.fr',
-    'test.user@msn.us',
-    'example@outlook.in',
-    'user@hotmail.co.jp',
-    'jane.doe@live.com.au',
-    'msnuser@outlook.nl',
-    'test.email@live.es',
-    'user@msn.com.br',
-    'john.doe@hotmail.ru',
-    'test.email@live.mx'
+    'leadminer-test1@outlook.com',
+    'leadminer-test3@live.fr',
+    'leadminer-test4@msn.us',
+    'leadminer-test5@outlook.in',
+    'leadminer-test7@live.com.au',
+    'leadminer-test8@outlook.nl',
+    'leadminer-test9@live.es',
+    'leadminer-test10@msn.com.br',
+    'leadminer-test12@live.mx'
+  ];
+
+  const hotmailEmails = [
+    'leadminer-test1@hotmail.co.uk',
+    'leadminer-test2@hotmail.co.jp',
+    'leadminer-test3@hotmail.ru'
   ];
 
   const yahooEmails = [
-    'user1@yahoo.com',
-    'jane_smith@yahoo.fr',
-    'test.user@yahoo.us',
-    'example@yahoo.in'
+    'leadminer-test1@yahoo.com',
+    'leadminer-test2@yahoo.fr',
+    'leadminer-test3@yahoo.us',
+    'leadminer-test4@yahoo.in'
   ];
 
-  it.each(outlookEmails)(
-    'should use MailerCheck for outlook email %s when MailerCheck api key is provided',
-    (email) => {
-      const sut = new EmailStatusVerifierFactory(
+  const googleEmails = ['leadminer-test1@gmail.com'];
+
+  describe('EmailStatusVerifierFactory() with Reacher & Mailercheck configured', () => {
+    let factory: EmailStatusVerifierFactory;
+
+    beforeEach(() => {
+      factory = new EmailStatusVerifierFactory(
         {
           MAILERCHECK_API_KEY: 'apiKey',
+          REACHER_API_KEY: 'apiKey',
           ...reacherDefaultConfig
         },
         logger
       );
+    });
 
-      const verifier = sut.getVerifier(email);
+    it.each(googleEmails)('should use Reacher for google email %s', (email) => {
+      const verifier = factory.getVerifier(email);
+      expect(verifier).toBeInstanceOf(ReacherEmailStatusVerifier);
+    });
 
-      expect(verifier).toBeInstanceOf(MailerCheckEmailStatusVerifier);
-    }
-  );
+    it.each(hotmailEmails)(
+      'should use Reacher for hotmail.* email %s',
+      (email) => {
+        const verifier = factory.getVerifier(email);
+        expect(verifier).toBeInstanceOf(ReacherEmailStatusVerifier);
+      }
+    );
 
-  it.each(yahooEmails)(
-    'should use MailerCheck for yahoo email %s when MailerCheck api key is provided',
-    (email) => {
-      const sut = new EmailStatusVerifierFactory(
-        {
-          MAILERCHECK_API_KEY: 'apiKey',
-          ...reacherDefaultConfig
-        },
-        logger
-      );
+    it.each([...outlookEmails, ...yahooEmails])(
+      'should use MailerCheck for other providers email %s',
+      (email) => {
+        const verifier = factory.getVerifier(email);
+        expect(verifier).toBeInstanceOf(MailerCheckEmailStatusVerifier);
+      }
+    );
+  });
 
-      const verifier = sut.getVerifier(email);
-
-      expect(verifier).toBeInstanceOf(MailerCheckEmailStatusVerifier);
-    }
-  );
-
-  it.each([...yahooEmails, ...outlookEmails])(
-    'should use Reacher for yahoo or outlook email "%s" when MailerCheck api key is not provided but Reacher credentials are provided',
-    (email) => {
-      const sut = new EmailStatusVerifierFactory(
+  describe('EmailStatusVerifierFactory() with Reacher configured', () => {
+    let factory: EmailStatusVerifierFactory;
+    beforeEach(() => {
+      factory = new EmailStatusVerifierFactory(
         {
           REACHER_API_KEY: 'apiKey',
           ...reacherDefaultConfig
         },
         logger
       );
+    });
 
-      const verifier = sut.getVerifier(email);
-
+    it.each([
+      ...googleEmails,
+      ...yahooEmails,
+      ...outlookEmails,
+      ...hotmailEmails
+    ])('should use Reacher for all emails providers', (email) => {
+      const verifier = factory.getVerifier(email);
       expect(verifier).toBeInstanceOf(ReacherEmailStatusVerifier);
-    }
-  );
+    });
+  });
 
   it('should fallback to default implementation when Reacher and MailerCheck credentials are not provided', () => {
-    const sut = new EmailStatusVerifierFactory(
+    const factory = new EmailStatusVerifierFactory(
       {
         ...reacherDefaultConfig
       },
       logger
     );
 
-    const verifier = sut.getVerifier('email@domain.com');
+    const verifier = factory.getVerifier('email@domain.com');
 
     expect(verifier).toBeInstanceOf(RandomEmailStatusVerifier);
   });
