d6c032fc2374361050df2de5cbfe7ce433e65643
Malek salem <malek@ankaboot.io>
2024-04-02 23:37:06 +0100

    fix margin sidebar

--- frontend/src/components/Mining/MiningConsentSidebar.vue
+++ frontend/src/components/Mining/MiningConsentSidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <Sidebar v-model:visible="show" class="pt-16 h-auto" position="top">
+  <Sidebar v-model:visible="show" class="h-auto" position="top">
     <template #container>
       <div class="grid gap-3 p-8">
         <span class="text-xl font-bold merriweather"
