175700ab394ba3b3445b6733b9c7f4734bae118d
Jaafoura <73950268+J43fura@users.noreply.github.com>
2024-04-04 12:35:11 +0100

    refactor: export & fix: Table UI (#1238)
    resolves #1204
    * Refactor Export
    * Migrate `InsufficientCreditsDialog.vue` into PrimeVue 
    
    resolves #1223 
    * Fix Select Column width & clickable behind header bug
    * Fix Loading table UI
    * Apply filters once finished mining
    * Fix Table should occupy available space (vertically)
    * Fix: Valid contacts filter
    * Fix: Filter toggles not updated when filters are modified manually
    
    * Delete `MiningProgress.vue`

--- backend/src/controllers/contacts.controller.ts
+++ backend/src/controllers/contacts.controller.ts
@@ -3,116 +3,129 @@ import { NextFunction, Request, Response } from 'express';
 import ENV from '../config';
 import { Contacts } from '../db/interfaces/Contacts';
 import { Users } from '../db/interfaces/Users';
+import { Contact } from '../db/types';
 import CreditsHandler from '../services/credits/creditHandler';
 import {
   exportContactsToCSV,
   getLocalizedCsvSeparator
 } from '../utils/helpers/csv';
 
+async function exportToCSV(
+  contactsToExport: Contact[],
+  delimiterOption: string | undefined,
+  localeFromHeader: string | undefined
+) {
+  const csvSeparator =
+    delimiterOption ?? getLocalizedCsvSeparator(localeFromHeader ?? '');
+  const csvData = await exportContactsToCSV(contactsToExport, csvSeparator);
+  return csvData;
+}
 export default function initializeContactsController(
   contacts: Contacts,
   userResolver: Users
 ) {
   return {
-    async verifyExportContacts(_: Request, res: Response, next: NextFunction) {
-      const user = res.locals.user as User;
-      try {
-        const newContacts = await contacts.getNonExportedContacts(user.id);
-        const previousExportedContacts = await contacts.getExportedContacts(
-          user.id
-        );
-
-        if (newContacts.length + previousExportedContacts.length === 0) {
-          return res.sendStatus(204);
-        }
-
-        if (ENV.ENABLE_CREDIT && ENV.CONTACT_CREDIT) {
-          const creditHandler = new CreditsHandler(
-            userResolver,
-            ENV.CONTACT_CREDIT
-          );
-          const { insufficientCredits, requestedUnits, availableUnits } =
-            await creditHandler.validate(user.id, newContacts.length);
-
-          const response = {
-            total: newContacts.length + previousExportedContacts.length,
-            available: insufficientCredits ? 0 : availableUnits
-          };
-
-          let statusCode = 200;
-          if (insufficientCredits && availableUnits) {
-            statusCode = creditHandler.INSUFFICIENT_CREDITS_STATUS;
-          } else if (availableUnits !== requestedUnits) {
-            statusCode = 206;
-          }
-
-          return res.status(statusCode).json(response);
-        }
-
-        return res.sendStatus(200);
-      } catch (error) {
-        return next(error);
-      }
-    },
-
     async exportContactsCSV(req: Request, res: Response, next: NextFunction) {
       const user = res.locals.user as User;
+      const partialExport = req.body.partialExport ?? false;
+      const contactsToExport = JSON.parse(req.body.contactsToExport);
+      let statusCode = 200;
+
+      if (!contactsToExport.length) {
+        statusCode = 204; // 204 No Content
+        return res.sendStatus(statusCode);
+      }
+
       try {
-        const minedContacts = await contacts.getContacts(user.id);
+        if (!ENV.ENABLE_CREDIT || !ENV.CONTACT_CREDIT) {
+          // No need to Verify Credits, Export.
 
-        if (minedContacts.length === 0) {
-          return res.sendStatus(204);
-        }
-
-        const delimiterOption = req.query.delimiter;
-        const localeFromHeader = req.headers['accept-language'];
-        const csvSeparator = delimiterOption
-          ? String(delimiterOption)
-          : getLocalizedCsvSeparator(localeFromHeader ?? '');
-
-        if (ENV.ENABLE_CREDIT && ENV.CONTACT_CREDIT) {
-          const creditHandler = new CreditsHandler(
-            userResolver,
-            ENV.CONTACT_CREDIT
+          const selectedContacts = await contacts.getSelectedContacts(
+            user.id,
+            contactsToExport
           );
-
-          const newContacts = await contacts.getNonExportedContacts(user.id);
-          const previousExportedContacts = await contacts.getExportedContacts(
-            user.id
-          );
-
-          const { insufficientCredits, requestedUnits, availableUnits } =
-            await creditHandler.validate(user.id, newContacts.length);
-
-          const availableContacts = newContacts.slice(0, availableUnits);
-          const contactsToExport = [
-            ...previousExportedContacts,
-            ...availableContacts
-          ];
-
-          const csvData = await exportContactsToCSV(
-            contactsToExport,
-            csvSeparator
-          );
-
-          if (insufficientCredits === false && availableUnits) {
-            await contacts.registerExportedContacts(
-              availableContacts.map(({ email }) => email),
-              user.id
-            );
-            await creditHandler.deduct(user.id, availableUnits);
+          if (!selectedContacts.length) {
+            statusCode = 204; // 204 No Content
+            return res.sendStatus(statusCode);
           }
 
+          const csvData = await exportToCSV(
+            selectedContacts,
+            req.query.delimiter ? String(req.query.delimiter) : undefined,
+            req.headers['accept-language']
+          );
           return res
             .header('Content-Type', 'text/csv')
-            .status(availableUnits === requestedUnits ? 200 : 206)
+            .status(statusCode)
             .send(csvData);
         }
 
-        const csvData = await exportContactsToCSV(minedContacts, csvSeparator);
-        return res.header('Content-Type', 'text/csv').status(200).send(csvData);
-      } catch (err) {
-        return next(err);
+        // Verify
+        const newContacts = await contacts.getSelectedNonExportedContacts(
+          user.id,
+          contactsToExport
+        );
+
+        const previousExportedContacts =
+          await contacts.getSelectedExportedContacts(user.id, contactsToExport);
+
+        // Verify Credits
+        const creditHandler = new CreditsHandler(
+          userResolver,
+          ENV.CONTACT_CREDIT
+        );
+        const { hasDeficientCredits, hasInsufficientCredits, availableUnits } =
+          await creditHandler.validate(user.id, newContacts.length);
+
+        if (hasDeficientCredits && !previousExportedContacts.length) {
+          statusCode = creditHandler.DEFICIENT_CREDITS_STATUS; // 402 Payment Required
+          const response = {
+            total: newContacts.length + previousExportedContacts.length,
+            available: Math.floor(availableUnits),
+            availableAlready: previousExportedContacts.length
+          };
+          return res.status(statusCode).json(response);
+        }
+        if (hasInsufficientCredits) {
+          statusCode = 206; // 206 Partial Content
+          if (!partialExport) {
+            statusCode = 266; // 266 Confirm Partial Content
+            res.statusMessage = 'Confirm Partial Content';
+            const response = {
+              total: newContacts.length + previousExportedContacts.length,
+              available: Math.floor(availableUnits),
+              availableAlready: previousExportedContacts.length
+            };
+            return res.status(statusCode).json(response);
+          }
+        }
+        // Verified, Export.
+        const availableContacts = newContacts.slice(0, availableUnits);
+        const selectedContacts = [
+          ...previousExportedContacts,
+          ...availableContacts
+        ];
+
+        const csvData = await exportToCSV(
+          selectedContacts,
+          req.query.delimiter ? String(req.query.delimiter) : undefined,
+          req.headers['accept-language']
+        );
+
+        if (availableContacts.length) {
+          await contacts.registerExportedContacts(
+            availableContacts.map(({ email }) => email),
+            user.id
+          );
+          await creditHandler.deduct(user.id, availableUnits);
+        }
+
+        return res
+          .header('Content-Type', 'text/csv')
+          .status(statusCode)
+          .send(csvData);
+      } catch (error) {
+        return next(error);
       }
     }
   };
