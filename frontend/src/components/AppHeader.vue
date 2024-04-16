93a442a000132309c69d82232649a1daac6cff46
MBATheGamer-Job <mbathegamer+job@gmail.com>
2024-04-16 11:02:37 +0100

    Update navbar design

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -20,7 +20,7 @@
 
     <Button class="md:hidden" icon="pi pi-bars" @click="visible = true" />
 
-    <Sidebar :visible="visible">
+    <Sidebar v-model:visible="visible">
       <template #container>
         <div class="absolute flex flex-column h-screen px-6">
           <div class="w-full">
--- frontend/src/components/Credits/CreditsCounter.vue
+++ frontend/src/components/Credits/CreditsCounter.vue
@@ -1,41 +1,46 @@
 <template>
   <div class="grid md:flex gap-2">
     <Button
-      class="flex justify-center cursor-default"
-      outlined
-      severity="danger"
       v-tooltip.bottom="{
         value: `${CREDITS_PER_EMAIL} credit per email / ${CREDITS_PER_CONTACT} credits per contact`,
         showDelay: 0,
         hideDelay: 300,
       }"
+      class="flex justify-center md:justify-start cursor-default md:w-[155px]"
+      :class="creditsBadgeState"
+      outlined
+      severity="danger"
       disabled
     >
-      <img class="h-[1.5rem] mr-2" src="/icons/coin.svg" />
-      <span
-        :class="
-          credits < CREDITS_MIN_THRESHOLD
-            ? 'text-caption flash-animation'
-            : 'text-caption'
-        "
-      >
+      <PhosphorCoin class="h-[1.5rem] mr-2" />
+      <span :class="creditsBadgeTextAnimation">
         {{ formattedCredits }}
       </span>
     </Button>
     <Button
-      class="flex justify-center"
+      class="flex space-x-1 items-center justify-center md:justify-normal text-white"
       severity="contrast"
+      size="small"
       @click="refillCreditsOrUpgrade"
     >
-      Refill
-      <img class="ml-2 h-[1.5rem]" src="/icons/rocket.svg" />
+      <span class="font-semibold">Refill</span>
+      <span class="material-icons" style="font-size: 1.3rem"
+        >rocket_launch</span
+      >
     </Button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { CREDITS_MIN_THRESHOLD, refillCreditsOrUpgrade } from '@/utils/credits';
+import {
+  CREDITS_MIN_THRESHOLD,
+  CREDITS_PER_CONTACT,
+  CREDITS_PER_EMAIL,
+  refillCreditsOrUpgrade,
+} from '@/utils/credits';
+import PhosphorCoin from '../icons/PhosphorCoin.vue';
 
+const $toast = useToast();
 const leadminerStore = useLeadminerStore();
 
 onMounted(async () => {
@@ -49,4 +54,30 @@ const formattedCredits = computed(() =>
     ? 'Out of credit'
     : new Intl.NumberFormat().format(credits.value)
 );
+const creditsBadgeState = computed(() =>
+  credits.value >= CREDITS_MIN_THRESHOLD
+    ? 'text-black border-gray-400'
+    : 'text-red  border border-red-400'
+);
+const creditsBadgeTextAnimation = computed(() =>
+  credits.value < CREDITS_MIN_THRESHOLD ? 'text-xs flash-animation' : ''
+);
+
+watch(credits, (newVal: number) => {
+  if (newVal === 0) {
+    $toast.add({
+      severity: 'warn',
+      summary: 'Insufficient credits',
+      detail: 'Your current balance is empty. Please refill.',
+      life: 3000,
+    });
+  } else if (newVal < CREDITS_MIN_THRESHOLD) {
+    $toast.add({
+      severity: 'warn',
+      summary: 'Low credits',
+      detail: 'Running low on credits. Remember to refill.',
+      life: 3000,
+    });
+  }
+});
 </script>
