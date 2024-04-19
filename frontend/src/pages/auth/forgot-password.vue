7aaca5287e2a3fc87eee367bbe5048e449ae5948
Malek salem <malek@ankaboot.io>
2024-04-19 12:38:19 +0100

    fix issue with styling due to merge conflict

--- frontend/src/pages/auth/forgot-password.vue
+++ frontend/src/pages/auth/forgot-password.vue
@@ -37,34 +37,6 @@
           <NuxtLink to="/auth/login" class="font-bold link"> Sign in </NuxtLink>
         </p>
       </div>
-      <div class="grid gap-6 w-full">
-        <div class="text-lg">
-          Enter the email address associated with your account
-        </div>
-        <div class="grid gap-1">
-          <label class="text-left" for="email">Email</label>
-          <InputText
-            v-model="email"
-            :invalid="!!email && !isValidEmail(email)"
-            type="email"
-            required
-            aria-describedby="email-help"
-            @focusin="emailFocus = true"
-            @focusout="emailFocus = false"
-          />
-          <small
-            v-if="!!email && !isValidEmail(email)"
-            id="email-help"
-            class="text-red-400 text-left pl-4"
-            >Please enter a valid email</small
-          >
-        </div>
-      </div>
-      <Button
-        label="Send reset instructions"
-        size="large"
-        @click="resetPassword"
-      />
   </ClientOnly>
 </template>
 
