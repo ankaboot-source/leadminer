f0f90a2d9948b51929d0b621e779316ace712498
MBATheGamer-Job <152849306+MBATheGamer-Job@users.noreply.github.com>
2024-04-19 11:37:45 +0100

    Revert "Merge branch 'main' of https://github.com/ankaboot-source/leadminer into 1226-user-profile-and-navbar-migration-to-primevue"
    This reverts commit d61bdf007c0868ab1d71f4d91ca03491ffe4e679, reversing
    changes made to c7460366ab1b7534c133595904f28025ef74f381.

--- frontend/src/components/cards/TreeCard.vue
+++ frontend/src/components/cards/TreeCard.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <q-tree
     ref="tree"
@@ -51,34 +50,3 @@ import { useLeadminerStore } from '@/stores/leadminer';
 
 const leadminerStore = useLeadminerStore();
 </script>
-=======
-<template>
-  <Tree
-    :value="leadminerStore.boxes"
-    :expanded-keys="expandedKeys"
-    :selection-keys="leadminerStore.selectedBoxes"
-    selection-mode="checkbox"
-  >
-    <template #default="{ node }">
-      {{ node.label }}
-      <Badge
-        v-if="node.key === '' || node.key! in expandedKeys"
-        v-tooltip="'Total emails in this folder'"
-        severity="secondary"
-      >
-        {{ node.total ? node.total : node.cumulativeTotal }}
-      </Badge>
-      <Badge v-else>
-        {{ node.total ? node.total : node.cumulativeTotal }}
-      </Badge>
-    </template>
-  </Tree>
-</template>
-
-<script setup lang="ts">
-import { useLeadminerStore } from '@/stores/leadminer';
-
-const leadminerStore = useLeadminerStore();
-const expandedKeys = ref({});
-</script>
->>>>>>> f4d841ab9f10db11448ed46d18f0dec3a7d9d2fa
--- frontend/src/utils/boxes.ts
+++ frontend/src/utils/boxes.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // @ts-expect-error missing types definition file
 import objectScan from 'object-scan';
 
@@ -50,63 +49,3 @@ export function getDefaultSelectedFolders(boxes: BoxNode[]) {
 
   return filteredBoxes;
 }
-=======
-// @ts-expect-error missing types definition file
-import objectScan from 'object-scan';
-import type { TreeSelectionKeys } from 'primevue/tree';
-
-export interface BoxNode {
-  key: string;
-  label: string;
-  total: number;
-  children?: BoxNode[];
-}
-
-export const EMAIL_EXCLUDED_FOLDERS = [
-  'mailspring',
-  'outbox',
-  'drafts',
-  'junk',
-  'trash',
-  '\\all',
-  '\\drafts',
-  '\\junk',
-  '\\trash',
-];
-
-/**
- * Filters out default selected folders from the input boxes based on email service
- * @param boxes - The array of folder names to filter
- * @returns The filtered array of boxes
- */
-export function getDefaultSelectedFolders(boxes: BoxNode[]) {
-  const filteredBoxes: TreeSelectionKeys = [];
-  objectScan(['**.key'], {
-    joined: true,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    filterFn: ({ parent }: any) => {
-      const { key, attribs } = parent;
-      const folder = key.split('/');
-      const folderName = folder.pop();
-      const folderParent = folder.pop();
-
-      const isExcluded = [...(attribs ?? []), folderName, folderParent]
-        .filter(Boolean)
-        .map((name) => name.toLowerCase())
-        .some((name) => EMAIL_EXCLUDED_FOLDERS.includes(name));
-
-      if (!isExcluded) {
-        // Format to be like PrimeVue's TreeSelectionKeys
-        const checked = attribs && !attribs.includes('\\HasChildren');
-        const partialChecked = !checked;
-
-        filteredBoxes[key] = {
-          checked,
-          partialChecked,
-        };
-      }
-    },
-  })(boxes);
-  return filteredBoxes;
-}
->>>>>>> f4d841ab9f10db11448ed46d18f0dec3a7d9d2fa
