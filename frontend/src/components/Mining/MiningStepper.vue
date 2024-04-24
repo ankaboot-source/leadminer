f2e5d534d1340224d9a9bffdc67584cd7a9d2ac0
Malek salem <malek@ankaboot.io>
2024-04-24 10:03:49 +0100

    fix stepper ui

--- frontend/src/components/Mining/MiningStepper.vue
+++ frontend/src/components/Mining/MiningStepper.vue
@@ -1,15 +1,19 @@
 <template>
-  <Panel class="mb-4" header="Start a new mining" toggleable>
+  <Panel class="mb-4" :header="panelHeader" toggleable>
     <Stepper v-model:active-step="stepper" linear>
-      <StepperPanel header="Select source">
+      <StepperPanel header="Source">
         <template #content="{ nextCallback }">
-          <SourcePanel :next-callback="nextCallback" />
+          <SourcePanel
+            v-model:title="panelHeader"
+            :next-callback="nextCallback"
+          />
         </template>
       </StepperPanel>
 
       <StepperPanel header="Mine">
         <template #content="{ prevCallback, nextCallback }">
           <MinePanel
+            v-model:title="panelHeader"
             :mining-source="$leadminerStore.activeMiningSource!"
             :next-callback="nextCallback"
             :prev-callback="prevCallback"
@@ -18,7 +22,10 @@
       </StepperPanel>
       <StepperPanel header="Clean">
         <template #content="{ prevCallback }">
-          <CleanPanel :prev-callback="prevCallback" />
+          <CleanPanel
+            v-model:title="panelHeader"
+            :prev-callback="prevCallback"
+          />
         </template>
       </StepperPanel>
     </Stepper>
@@ -43,6 +50,7 @@ const $route = useRoute();
 const $consentSidebar = useMiningConsentSidebar();
 const $leadminerStore = useLeadminerStore();
 
+const panelHeader = ref('');
 const stepper = ref();
 
 const { error, provider, source } = $route.query;
