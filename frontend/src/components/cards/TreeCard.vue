191ee1943ef7b2f33ff2bf54783009c59e3c17b4
Jaafoura <mehdi.jaafourag@gmail.com>
2024-05-02 18:01:15 +0100

    remove tree padding
    resolves #1225

--- frontend/src/components/cards/TreeCard.vue
+++ frontend/src/components/cards/TreeCard.vue
@@ -4,6 +4,7 @@
     v-model:selection-keys="leadminerStore.selectedBoxes"
     v-model:expanded-keys="expandedKeys"
     selection-mode="checkbox"
+    class="px-0"
   >
     <template #default="{ node }">
       {{ node.label }}
