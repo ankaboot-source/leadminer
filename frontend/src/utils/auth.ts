40ee60d87ac4394d7c0049ff35b62cc21263f145
Malek salem <malek@ankaboot.io>
2024-05-17 12:10:53 +0100

    fix signout when deleting account

--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -1,7 +1,19 @@
 import { sse } from './sse';
 
+const clearAllCookies = () => {
+  const cookies = document.cookie.split(';');
+
+  for (const cookie of cookies) {
+    const eqPos = cookie.indexOf('=');
+    const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`;
+  }
+};
+
 export function logout() {
   sse.closeConnection();
   useResetStore().all();
+  clearAllCookies();
   navigateTo('/auth/login');
 }
+
