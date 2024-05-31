35e496aba00b505d3555a20e340d84734b6dcd47
Jaafoura <mehdi.jaafourag@gmail.com>
2024-05-31 14:56:00 +0100

    drop CREDITS_MIN_THRESHOLD to 100

--- frontend/src/utils/credits.ts
+++ frontend/src/utils/credits.ts
@@ -1,6 +1,5 @@
-export const CREDITS_PER_EMAIL = 1;
-export const CREDITS_PER_CONTACT = 10;
-export const CREDITS_MIN_THRESHOLD = 10000;
+export const CREDITS_PER_CONTACT = 1;
+export const CREDITS_MIN_THRESHOLD = 100;
 
 export function refillCreditsOrUpgrade() {
   window.open(useRuntimeConfig().public.EXTERNAL_REFILL_CREDITS_LINK, '_blank');
