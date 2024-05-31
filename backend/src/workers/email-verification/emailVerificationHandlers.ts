9d9a24cd18f1cee2154f47d313a6ca4d68405894
Malek salem <malek@ankaboot.io>
2024-05-31 14:24:09 +0100

    fix linting

--- backend/src/workers/email-verification/emailVerificationHandlers.ts
+++ backend/src/workers/email-verification/emailVerificationHandlers.ts
@@ -103,7 +103,7 @@ export default function initializeEmailVerificationProcessor(
 ) {
   return {
     processStreamData: async (message: EmailVerificationData[]) =>
-      await emailVerificationHandlerWithBulk(
+      emailVerificationHandlerWithBulk(
         message,
         contacts,
         emailStatusCache,
