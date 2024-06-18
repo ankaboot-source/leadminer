efb81d09b918ed9875dead54103b5efd28e7edbb
Malek salem <malek@ankaboot.io>
2024-06-18 10:20:23 +0100

    use deno serve instead of express

--- saas-supabase/supabase/functions/imap/index.ts
+++ saas-supabase/supabase/functions/imap/index.ts
@@ -1,15 +1,75 @@
-import express from "express";
-import cors from "cors";
+import corsHeaders from "../_shared/cors.ts";
+import Logger from "../_shared/logger.ts";
+import { createSupabaseClient } from "../_shared/supabase-self-hosted.ts";
+import IMAPSettingsDetector from "npm:@ankaboot.io/imap-autoconfig";
 
-import { getIMAPConfigFromEmail } from "./controllers.ts";
-import { authorizeUser } from "../_shared/middlewares.ts";
+Deno.serve(async (req: Request) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
 
-const app = express();
+  const authorization = req.headers.get("Authorization");
 
-app.use(cors());
-app.use(express.json());
-app.use(authorizeUser);
+  if (!authorization) {
+    Logger.error("No authorization header passed");
 
-app.get("/imap/config/:email", getIMAPConfigFromEmail);
+    return new Response(
+      JSON.stringify({ error: "No authorization header passed" }),
+      {
+        status: 401,
+        headers: { ...corsHeaders, "Content-Type": "application/json" },
+      }
+    );
+  }
 
-app.listen(3000);
+  const client = createSupabaseClient(authorization);
+  const { email } = await req.json();
+
+  if (!email) {
+    return new Response(null, {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400,
+    });
+  }
+
+  const {
+    data: { user },
+    error,
+  } = await client.auth.getUser();
+
+  if (error) {
+    Logger.error(error.message);
+
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+    });
+  }
+
+  if (!user) {
+    return new Response(null, {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 401,
+    });
+  }
+
+  try {
+    const config = await new IMAPSettingsDetector.default().detect(email);
+
+    if (error) {
+      throw error;
+    }
+
+    return new Response(config ? JSON.stringify(config) : null, {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (error) {
+    Logger.error(error.message);
+
+    return new Response(JSON.stringify(error), {
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 500,
+    });
+  }
+});
