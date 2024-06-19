75929b330bc1e6948d103d80961838325ff18664
MBATheGamer-Job <mbathegamer+job@gmail.com>
2024-06-19 09:43:53 +0100

    Add i18n to email templates

--- frontend/i18n.config.ts
+++ frontend/i18n.config.ts
@@ -71,51 +71,52 @@ export default defineI18nConfig(() => ({
         continue_with: 'continue with {provider}',
       },
       email_template: {
-        "change_email": {
-          "prehead": "Confirm your email change",
-          "title": "Confirm Your Email Change",
-          "body": {
-            "p1": "Welcome to ",
-            "p2": "! We're happy to assist you with keeping your account information up to date. You recently requested to change the email address associated with your ",
-            "p3": " account. Please click the button below to confirm your new email address and complete the update.",
-            "p4": "If you didn't request this change, you can safely disregard this email. Your current email address will remain unchanged."
+        change_email: {
+          prehead: 'Confirm your email change',
+          title: 'Confirm Your Email Change',
+          body: {
+            p1: 'Welcome to ',
+            p2: "! We're happy to assist you with keeping your account information up to date. You recently requested to change the email address associated with your ",
+            p3: ' account. Please click the button below to confirm your new email address and complete the update.',
+            p4: "If you didn't request this change, you can safely disregard this email. Your current email address will remain unchanged.",
           },
-          "button": "Confirm Email Address",
+          button: 'Confirm Email Address',
         },
-        "email_confirmation": {
-          "prehead": "Confirm your signup to leadminer",
-          "title": "Confirm Your Email Address",
-          "body": {
-            "p1": "Welcome to ",
-            "p2": "! We're happy that you've selected us to help you generate clean and enriched contacts from your mailbox. Click the button below to confirm your email address and activate your account.",
-            "p3": "If you didn't sign up with ",
-            "p4": ", you can safely disregard this email."
+        email_confirmation: {
+          prehead: 'Confirm your signup to leadminer',
+          title: 'Confirm Your Email Address',
+          body: {
+            p1: 'Welcome to ',
+            p2: "! We're happy that you've selected us to help you generate clean and enriched contacts from your mailbox. Click the button below to confirm your email address and activate your account.",
+            p3: "If you didn't sign up with ",
+            p4: ', you can safely disregard this email.',
           },
-          "button": "Confirm your email"
+          button: 'Confirm your email',
         },
-        "reset_password": {
-          "prehead": "Reset your leadminer password",
-          "title": "Reset Your Password",
-          "body": {
-            "p1": "Tap the button below to reset your ",
-            "p2": "account password. If you didn't request a new password, you can safely delete this email."
+        reset_password: {
+          prehead: 'Reset your leadminer password',
+          title: 'Reset Your Password',
+          body: {
+            p1: 'Tap the button below to reset your ',
+            p2: "account password. If you didn't request a new password, you can safely delete this email.",
           },
-          "button": "Reset your password"
+          button: 'Reset your password',
         },
-        "email_invite": {
-          "prehead": "Invitation to leadminer",
-          "title": "You're Invited to Join",
-          "body": {
-            "p1": "You're invited to join ",
-            "p2": "! We're excited to have you as part of our community. With our platform, you can effortlessly generate clean and enriched contacts directly from your mailbox. To get started, click the button below to accept the invitation and create your account.",
-            "p3": "If you did not expect this invitation from ",
-            "p4": ", you can safely disregard this email."
+        email_invite: {
+          prehead: 'Invitation to leadminer',
+          title: "You're Invited to Join",
+          body: {
+            p1: "You're invited to join ",
+            p2: "! We're excited to have you as part of our community. With our platform, you can effortlessly generate clean and enriched contacts directly from your mailbox. To get started, click the button below to accept the invitation and create your account.",
+            p3: 'If you did not expect this invitation from ',
+            p4: ', you can safely disregard this email.',
           },
-          "button": "Accept Invitation"
+          button: 'Accept Invitation',
         },
-        "regards": "Best regards,",
-        "footer": "You received this email because we received a request for registration for your account. If you didn't request registration you can safely delete this email."
-      }
+        regards: 'Best regards,',
+        footer:
+          "You received this email because we received a request for registration for your account. If you didn't request registration you can safely delete this email.",
+      },
     },
     fr: {
       common: {
@@ -185,51 +186,52 @@ export default defineI18nConfig(() => ({
         continue_with: 'continuer avec {provider}',
       },
       email_template: {
-        "change_email": {
-          "prehead": "Confirmez votre changement d'adresse e-mail",
-          "title": "Confirmez votre changement d'adresse e-mail",
-          "body": {
-            "p1": "Bienvenue à ",
-            "p2": "! Nous sommes heureux de vous aider à mettre à jour les informations relatives à votre compte. Vous avez récemment demandé à changer l'adresse e-mail associée à votre ",
-            "p3": " compte. Veuillez cliquer sur le bouton ci-dessous pour confirmer votre nouvelle adresse e-mail et terminer la mise à jour.",
-            "p4": "Si vous n'avez pas demandé ce changement, vous pouvez ignorer cet e-mail. Votre adresse e-mail actuelle restera inchangée."
+        change_email: {
+          prehead: "Confirmez votre changement d'adresse e-mail",
+          title: "Confirmez votre changement d'adresse e-mail",
+          body: {
+            p1: 'Bienvenue à ',
+            p2: "! Nous sommes heureux de vous aider à mettre à jour les informations relatives à votre compte. Vous avez récemment demandé à changer l'adresse e-mail associée à votre ",
+            p3: ' compte. Veuillez cliquer sur le bouton ci-dessous pour confirmer votre nouvelle adresse e-mail et terminer la mise à jour.',
+            p4: "Si vous n'avez pas demandé ce changement, vous pouvez ignorer cet e-mail. Votre adresse e-mail actuelle restera inchangée.",
           },
-          "button": "Confirmez votre adresse e-mail",
+          button: 'Confirmez votre adresse e-mail',
         },
-        "email_confirmation": {
-          "prehead": "Confirmez votre inscription à leadminer",
-          "title": "Confirmez votre adresse e-mail",
-          "body": {
-            "p1": "Bienvenue à ",
-            "p2": "! Nous sommes heureux que vous nous ayez choisis pour vous aider à générer des contacts propres et enrichis à partir de votre boîte aux lettres. Cliquez sur le bouton ci-dessous pour confirmer votre adresse e-mail et activer votre compte.",
-            "p3": "Si vous ne vous êtes pas inscrit auprès de ",
-            "p4": ", vous pouvez ignorer cet e-mail en toute sécurité."
+        email_confirmation: {
+          prehead: 'Confirmez votre inscription à leadminer',
+          title: 'Confirmez votre adresse e-mail',
+          body: {
+            p1: 'Bienvenue à ',
+            p2: '! Nous sommes heureux que vous nous ayez choisis pour vous aider à générer des contacts propres et enrichis à partir de votre boîte aux lettres. Cliquez sur le bouton ci-dessous pour confirmer votre adresse e-mail et activer votre compte.',
+            p3: 'Si vous ne vous êtes pas inscrit auprès de ',
+            p4: ', vous pouvez ignorer cet e-mail en toute sécurité.',
           },
-          "button": "Confirmez votre e-mail"
+          button: 'Confirmez votre e-mail',
         },
-        "reset_password": {
-          "prehead": "Réinitialiser votre mot de passe leadminer",
-          "title": "Réinitialiser votre mot de passe",
-          "body": {
-            "p1": "Appuyez sur le bouton ci-dessous pour réinitialiser votre ",
-            "p2": "mot de passe de votre compte. Si vous n'avez pas demandé de nouveau mot de passe, vous pouvez supprimer cet e-mail en toute sécurité."
+        reset_password: {
+          prehead: 'Réinitialiser votre mot de passe leadminer',
+          title: 'Réinitialiser votre mot de passe',
+          body: {
+            p1: 'Appuyez sur le bouton ci-dessous pour réinitialiser votre ',
+            p2: "mot de passe de votre compte. Si vous n'avez pas demandé de nouveau mot de passe, vous pouvez supprimer cet e-mail en toute sécurité.",
           },
-          "button": "Réinitialiser votre mot de passe"
+          button: 'Réinitialiser votre mot de passe',
         },
-        "email_invite": {
-          "prehead": "Invitation à leadminer",
-          "title": "Vous êtes invité à rejoindre",
-          "body": {
-            "p1": "Vous êtes invités à rejoindre ",
-            "p2": "! Nous sommes ravis que vous fassiez partie de notre communauté. Grâce à notre plateforme, vous pouvez générer sans effort des contacts propres et enrichis directement à partir de votre boîte aux lettres. Pour commencer, cliquez sur le bouton ci-dessous pour accepter l'invitation et créer votre compte.",
-            "p3": "Si vous n'attendiez pas cette invitation de la part de ",
-            "p4": ", vous pouvez ignorer cet e-mail en toute sécurité."
+        email_invite: {
+          prehead: 'Invitation à leadminer',
+          title: 'Vous êtes invité à rejoindre',
+          body: {
+            p1: 'Vous êtes invités à rejoindre ',
+            p2: "! Nous sommes ravis que vous fassiez partie de notre communauté. Grâce à notre plateforme, vous pouvez générer sans effort des contacts propres et enrichis directement à partir de votre boîte aux lettres. Pour commencer, cliquez sur le bouton ci-dessous pour accepter l'invitation et créer votre compte.",
+            p3: "Si vous n'attendiez pas cette invitation de la part de ",
+            p4: ', vous pouvez ignorer cet e-mail en toute sécurité.',
           },
-          "button": "Accepter l'invitation"
+          button: "Accepter l'invitation",
         },
-        "regards": "Cordialement,",
-        "footer": "Vous avez reçu cet e-mail parce que nous avons reçu une demande d'enregistrement pour votre compte. Si vous n'avez pas demandé d'enregistrement, vous pouvez supprimer cet e-mail en toute sécurité."
-      }
+        regards: 'Cordialement,',
+        footer:
+          "Vous avez reçu cet e-mail parce que nous avons reçu une demande d'enregistrement pour votre compte. Si vous n'avez pas demandé d'enregistrement, vous pouvez supprimer cet e-mail en toute sécurité.",
+      },
     },
   },
 }));
