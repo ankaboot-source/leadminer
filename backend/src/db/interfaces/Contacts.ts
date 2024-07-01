38afa30176ae0b72d09b06c16f33f4cd4d8319c0
Jaafoura <73950268+J43fura@users.noreply.github.com>
2024-07-01 18:22:14 +0100

    fix: if already mined, unverified aren't cleaned while they should be checked (#1458)
    * Update i18n.config.ts
    
    * bug fix
    
    * recheck unverified contacts
    
    - resolves #1434
    
    * Revert "bug fix"
    
    This reverts commit a6b8fcbe790d2698c6345a5babf6b7f35f2f53a9.
    
    * Check status from db to avoid verifying already verified ones
    
    * Update emailMessageHandlers.ts

--- backend/src/db/interfaces/Contacts.ts
+++ backend/src/db/interfaces/Contacts.ts
@@ -18,6 +18,7 @@ export interface Contacts {
   ): Promise<boolean>;
   getUnverifiedEmails(userId: string): Promise<string[]>;
   getContacts(userId: string, emails?: string[]): Promise<Contact[]>;
+  getUnverifiedContacts(userId: string, emails: string[]): Promise<Contact[]>;
   getExportedContacts(userId: string, emails?: string[]): Promise<Contact[]>;
   getNonExportedContacts(userId: string, emails?: string[]): Promise<Contact[]>;
   registerExportedContacts(contactIds: string[], userId: string): Promise<void>;
