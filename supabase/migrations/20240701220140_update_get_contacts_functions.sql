8cb769523bb74b9253d6d93995b758f2755308f4
MBATheGamer-Job <mbathegamer+job@gmail.com>
2024-07-09 14:12:01 +0100

    Fix: column reference is ambiguous

--- supabase/migrations/20240701220140_update_get_contacts_functions.sql
+++ supabase/migrations/20240701220140_update_get_contacts_functions.sql
@@ -15,18 +15,18 @@ CREATE OR REPLACE FUNCTION get_contacts_table(userid uuid) RETURNS TABLE (
 BEGIN
   RETURN QUERY WITH ExportedContacts AS (
     SELECT
-      pr.email as source,
-      p.email,
-      p.name,
-      p.status,
-      rp.recency,
-      rp.seniority,
-      rp.occurrence,
-      rp.sender,
-      rp.recipient,
-      rp.conversations,
-      rp.replied_conversations,
-      rp.tags,
+      pr.email as source_col,
+      p.email as email_col,
+      p.name as name_col,
+      p.status as status_col,
+      rp.recency as recency_col,
+      rp.seniority as seniority_col,
+      rp.occurrence as occurrence_col,
+      rp.sender as sender_col,
+      rp.recipient as recipient_col,
+      rp.conversations as conversations_col,
+      rp.replied_conversations as replied_conversations_col,
+      rp.tags as tags_col,
       ROW_NUMBER() OVER (
         PARTITION BY p.email
         ORDER BY rp.recency DESC
@@ -41,7 +41,18 @@ BEGIN
       p.user_id = get_contacts_table.userid
   )
   SELECT
-    *
+    source_col AS source,
+    email_col AS email,
+    name_col AS name,
+    status_col AS status,
+    recency_col AS recency,
+    seniority_col AS seniority,
+    occurrence_col AS occurrence,
+    sender_col AS sender,
+    recipient_col AS recipient,
+    conversations_col AS conversations,
+    replied_conversations_col AS replied_conversations,
+    tags_col AS tags
   FROM
     ExportedContacts
   WHERE
@@ -65,18 +76,19 @@ CREATE OR REPLACE FUNCTION get_contacts_table_by_emails(userid uuid, emails TEXT
 ) AS $$
 BEGIN
   RETURN QUERY WITH ExportedContacts AS (
-    SELECT pr.email as source,
-      p.email,
-      p.name,
-      p.status,
-      rp.recency,
-      rp.seniority,
-      rp.occurrence,
-      rp.sender,
-      rp.recipient,
-      rp.conversations,
-      rp.replied_conversations,
-      rp.tags,
+    SELECT
+      pr.email as source_col,
+      p.email as email_col,
+      p.name as name_col,
+      p.status as status_col,
+      rp.recency as recency_col,
+      rp.seniority as seniority_col,
+      rp.occurrence as occurrence_col,
+      rp.sender as sender_col,
+      rp.recipient as recipient_col,
+      rp.conversations as conversations_col,
+      rp.replied_conversations as replied_conversations_col,
+      rp.tags as tags_col,
       ROW_NUMBER() OVER (
         PARTITION BY p.email
         ORDER BY rp.recency DESC
@@ -93,7 +105,18 @@ BEGIN
       p.email = ANY(get_contacts_table_by_emails.emails)
   )
   SELECT
-    *
+    source_col AS source,
+    email_col AS email,
+    name_col AS name,
+    status_col AS status,
+    recency_col AS recency,
+    seniority_col AS seniority,
+    occurrence_col AS occurrence,
+    sender_col AS sender,
+    recipient_col AS recipient,
+    conversations_col AS conversations,
+    replied_conversations_col AS replied_conversations,
+    tags_col AS tags
   FROM
     ExportedContacts
   WHERE
