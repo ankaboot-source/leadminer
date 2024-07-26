e68ae3b788b4a90431e7c5e778e7ece1e414eb89
Jaafoura <mehdi.jaafourag@gmail.com>
2024-07-26 16:14:41 +0100

    Update EnrichButton.vue

--- frontend/src/components/Mining/Buttons/EnrichButton.vue
+++ frontend/src/components/Mining/Buttons/EnrichButton.vue
@@ -44,7 +44,7 @@
             @click="
               () => {
                 closeDialog();
-                startEnrichment(true);
+                startEnrichment(false);
               }
             "
           />
