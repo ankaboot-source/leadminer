d5b386405894562280980aca0037db5e4d57d588
Malek salem <malek@ankaboot.io>
2024-07-29 20:36:25 +0100

    fix step index "start mining"

--- frontend/src/components/Mining/StepperPanels/EnrichPanel.vue
+++ frontend/src/components/Mining/StepperPanels/EnrichPanel.vue
@@ -126,7 +126,7 @@ watch(contactsToEnrichLength, () => {
 });
 function startNewMining() {
   $leadminerStore.$resetMining();
-  $stepper.go(0);
+  $stepper.go(1);
 }
 
 function showNotification(
@@ -272,7 +272,7 @@ onUnmounted(() => {
     "notification": {
       "summary": "Enrich Contact",
       "enrichment_started": "Enrichment is running for {total} contacts.",
-      "enrichment_completed": "{total}/{enriched} of your contacts has been successfully enriched.",
+      "enrichment_completed": "{enriched}/{total} of your contacts has been successfully enriched.",
       "enrichment_canceled": "Your contact enrichment has been canceled.",
       "already_enriched": "Contacts you selected are already enriched.",
       "no_additional_info": "Enrichment completed, but no additional information was found for the selected contacts."
