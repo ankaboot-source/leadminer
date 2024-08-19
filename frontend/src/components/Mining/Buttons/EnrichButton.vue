8ea2c28112c3553f90f0762eba8b0c4aa5482db5
Jaafoura <mehdi.jaafourag@gmail.com>
2024-08-19 14:53:38 +0100

    disable all enrich (individual and bulk) while already enriching

--- frontend/src/components/Mining/Buttons/EnrichButton.vue
+++ frontend/src/components/Mining/Buttons/EnrichButton.vue
@@ -63,12 +63,12 @@
     icon-pos="right"
     :label="t('button.start_enrichment')"
     pt:label:class="hidden md:block"
-    :disabled="enrichmentStatus"
+    :disabled="$leadminerStore.activeEnrichment"
     @click="openDialog"
   >
     <template #icon>
       <span class="p-button-icon p-button-icon-right">
-        <span v-if="!enrichmentStatus">💎</span>
+        <span v-if="!$leadminerStore.activeEnrichment">💎</span>
         <i v-else class="pi pi-spin pi-spinner mr-1.5" />
       </span>
     </template>
@@ -175,6 +175,7 @@ function setupEnrichmentRealtime() {
                 }),
                 'achievement',
               );
+              $leadminerStore.activeEnrichment = false;
               enrichmentStatus.value = false;
             } else {
               showNotification(
