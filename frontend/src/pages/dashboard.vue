f619c79bbfab57939af4f02a7e4da68cdc42c75e
Malek salem <malek@ankaboot.io>
2024-08-23 13:17:16 +0100

    fix table rendering and loading (#1644)
    * fix table rendering and loading
    
    * refactor code in user settings page
    
    * fix linting
    
    * fix 0 contacts bug & rendering & reactivity
    
    * refactor stepper state
    
    * fix typo
    
    * remove redundant code

--- frontend/src/pages/dashboard.vue
+++ frontend/src/pages/dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col grow">
-    <mining-stepper v-model:collapsed="collapsedStepper" />
+    <mining-stepper v-model:collapsed="$stepper.collapsed" />
     <MiningTable />
   </div>
 </template>
@@ -9,15 +9,6 @@ const MiningTable = defineAsyncComponent(
   () => import('../components/Mining/Table/MiningTable.vue'),
 );
 
-const collapsedStepper = ref(false);
-
-onNuxtReady(async () => {
-  const $user = useSupabaseUser();
-  if ($user.value) {
-    const contacts = await getContacts($user.value.id);
-    useContactsStore().setContacts(contacts);
-    collapsedStepper.value =
-      contacts.length > 0 && useMiningStepper().index === 1;
-  }
-});
+const $stepper = useMiningStepper();
+$stepper.open();
 </script>
