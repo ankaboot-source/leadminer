1ca53d0f9400156e1830fb2f852599065a6c7bf8
Malek Salem <malek.salem.14@gmail.com>
2024-08-27 20:18:49 +0100

    fix linting

--- frontend/src/components/Mining/ContactInformationSidebar.vue
+++ frontend/src/components/Mining/ContactInformationSidebar.vue
@@ -202,7 +202,7 @@ import type {
 import SocialLink from '@/components/icons/SocialLink.vue';
 import EnrichButton from '@/components/Mining/Buttons/EnrichButton.vue';
 import type { Contact, ContactEdit } from '@/types/contact';
-import { type EnrichmentTask } from '@/types/enrichment';
+import type { EnrichmentTask } from '@/types/enrichment';
 import {
   getOrganization,
   getStatusColor,
