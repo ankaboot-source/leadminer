87f4c7a6258ea6ccbadf007d00f67da4bfc752ff
Malek Salem <malek.salem.14@gmail.com>
2024-08-29 20:31:48 +0100

    refactor user profile realtime

---
+++ frontend/src/plugins/supabase-profile-listener.ts
@@ -0,0 +1,43 @@
+import { defineNuxtPlugin, useSupabaseUser } from '#imports';
+import { useSupabaseUserProfile } from '~/composables/useSupabaseUserProfile';
+import type { RealtimePostgresChangesPayload } from '@supabase/supabase-js';
+import type { Profile } from '~/types/profile';
+
+export default defineNuxtPlugin({
+  name: 'supabase-profile-listener',
+  async setup() {
+    const $supabaseClient = useSupabaseClient();
+
+    $supabaseClient.auth.onAuthStateChange(async (_, session) => {
+      if (!session) {
+        return;
+      }
+
+      const $supabaseUser = useSupabaseUser();
+      const $currentProfile = useSupabaseUserProfile();
+
+      const { data } = await $supabaseClient
+        .from('profiles')
+        .select('*')
+        .single<Profile>();
+      $currentProfile.value = data;
+
+      $supabaseClient
+        .channel('user-profile-realtime')
+        .on(
+          'postgres_changes',
+          {
+            event: '*',
+            schema: 'public',
+            table: 'profiles',
+            filter: `user_id=eq.${$supabaseUser?.value.id}`,
+          },
+          (payload: RealtimePostgresChangesPayload<Profile>) => {
+            $currentProfile.value = payload.new as Profile;
+          },
+        )
+        .subscribe();
+      $supabaseClient.from('profiles').select('*').single();
+    });
+  },
+});
