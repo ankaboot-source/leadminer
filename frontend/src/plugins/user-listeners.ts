e9f418c556f01135fbd1a8307a86c1837db31860
Malek salem <malek@ankaboot.io>
2024-09-05 14:26:31 +0100

    fix realtime related bug & email template i18n (#1695)
    * fix email templates
    
    * remove duplicate call
    
    * remove async
    
    * fix profile realtime & refactor email template
    
    * fix enabling pgcrypto & typo in migration
    
    * fix supabase functions host for local dev
    
    * fix credits checking
    
    * fix org error
    
    * resolve deepsource issues
    
    * fix return type
    
    * minor fixes
    
    * fix typo

---
+++ frontend/src/plugins/user-listeners.ts
@@ -0,0 +1,157 @@
+import type {
+  RealtimeChannel,
+  RealtimePostgresChangesPayload,
+  SupabaseClient,
+} from '@supabase/supabase-js';
+import { useSupabaseUserProfile } from '~/composables/useSupabaseUserProfile';
+import type { Profile } from '~/types/profile';
+import type { $Fetch } from 'nitropack';
+
+/**
+ * Retrieves the current user's profile from the Supabase database.
+ *
+ * @param client - The Supabase client instance used to query the database.
+ * @returns A promise that resolves to the user's profile data.
+ * @throws An error if fetching the profile fails.
+ */
+async function getCurrentUserProfile(client: SupabaseClient): Promise<Profile> {
+  const { data, error } = await client
+    .from('profiles')
+    .select('*')
+    .single<Profile>();
+
+  if (error) throw error;
+  return data;
+}
+
+/**
+ * Adds a mining source using a provider token.
+ *
+ * @param api - The API client instance to interact with the edge function.
+ * @param provider - The name of the provider (e.g., GitHub, Google).
+ * @param providerToken - The token provided by the authentication provider.
+ * @returns A promise that resolves once the mining source has been added.
+ */
+async function addMiningSourceFromProviderToken(
+  api: $Fetch,
+  provider: string,
+  providerToken: string,
+): Promise<void> {
+  try {
+    await api('add-mining-source', {
+      method: 'POST',
+      body: { provider, provider_token: providerToken },
+    });
+  } catch (err) {
+    console.error('Failed to add mining source:', err);
+  }
+}
+
+/**
+ * Updates the user's email templates localization based on their language preference.
+ *
+ * @param api - The API client instance used for updating email templates.
+ * @param client - The Supabase client instance for updating user data.
+ * @param language - The language code (e.g., 'en', 'fr') for the email templates.
+ * @returns A promise that resolves once the email templates are updated.
+ */
+async function updateUserEmailTemplatesI18n(
+  api: $Fetch,
+  client: SupabaseClient,
+  language: string,
+): Promise<void> {
+  try {
+    await api('email-templates', {
+      method: 'POST',
+      body: { language },
+    });
+    /**
+     * Trigger a session sync to ensure templates don't get updated again on refresh.
+     */
+    await client.auth.updateUser({});
+  } catch (err) {
+    console.error('Failed to update email templates:', err);
+  }
+}
+
+export default defineNuxtPlugin({
+  name: 'user-listeners',
+  async setup() {
+    const $user = useSupabaseUser();
+    const $session = useSupabaseSession();
+    const $supabaseClient = useSupabaseClient();
+    const $currentProfile = useSupabaseUserProfile();
+    const $saasEdgeFunctions = useNuxtApp().$saasEdgeFunctions as $Fetch;
+
+    let channel: RealtimeChannel | null;
+    const authenticated = ref(false);
+
+    /**
+     * Starts watching for authentication changes.
+     */
+    const startAuthenticationWatcher = () => {
+      watch(authenticated, async (auth) => {
+        if (!auth) {
+          if (channel) await $supabaseClient.removeChannel(channel);
+          return;
+        }
+
+        const { provider, providerToken, hasEmailTemplate } = {
+          provider: $user.value.app_metadata.provider,
+          providerToken: $session.value.provider_token,
+          hasEmailTemplate: $user.value.user_metadata.EmailTemplate,
+        };
+
+        if (provider && providerToken) {
+          await addMiningSourceFromProviderToken(
+            $saasEdgeFunctions,
+            provider,
+            providerToken,
+          );
+        }
+
+        if (!hasEmailTemplate) {
+          await updateUserEmailTemplatesI18n(
+            $saasEdgeFunctions,
+            $supabaseClient,
+            navigator.language.split('-')[0],
+          );
+        }
+
+        channel = $supabaseClient.channel('user-profile-realtime').on(
+          'postgres_changes',
+          {
+            event: '*',
+            schema: 'public',
+            table: 'profiles',
+            filter: `user_id=eq.${$user.value.id}`,
+          },
+          (payload: RealtimePostgresChangesPayload<Profile>) => {
+            $currentProfile.value = payload.new as Profile;
+          },
+        );
+        channel.subscribe();
+      });
+    };
+
+    /**
+     * Fetches and assigns the user profile on the server
+     * to avoid empty fields in "settings" during hard refresh.
+     */
+    if ($session.value) {
+      $currentProfile.value = await getCurrentUserProfile($supabaseClient);
+    }
+
+    if (import.meta.client) {
+      startAuthenticationWatcher();
+    }
+
+    $supabaseClient.auth.onAuthStateChange((_, session) => {
+      if (session) {
+        authenticated.value = true;
+      } else {
+        authenticated.value = false;
+      }
+    });
+  },
+});
---
+++ supabase/functions/email-templates/templates.ts
@@ -0,0 +1,136 @@
+type EmailTemplate = {
+    Prehead: string;
+    Title: string;
+    Body1: string;
+    Body2: string;
+    Body3?: string;
+    Body4?: string;
+    Button: string;
+    Regards: string;
+    Footer: string;
+};
+
+type EmailTemplatesI18n = {
+    Invite: EmailTemplate;
+    Confirmation: EmailTemplate;
+    Recovery: EmailTemplate;
+    EmailChange: EmailTemplate;
+};
+
+type LanguageEmailTemplates = Map<string, EmailTemplatesI18n>;
+
+const supabaseEmailsI18n: LanguageEmailTemplates = new Map([
+    [
+        'en',
+        {
+            Invite: {
+                Prehead: 'Invitation to leadminer',
+                Title: "You're Invited to Join",
+                Body1: "You're invited to join ",
+                Body2:
+                    "! We're excited to have you as part of our community. With our platform, you can effortlessly generate clean and enriched contacts directly from your mailbox. To get started, click the button below to accept the invitation and create your account.",
+                Button: 'Accept Invitation',
+                Regards: 'Best regards,',
+                Footer:
+                    "You received this email because we received a request for your account. If you didn't request it, you can safely delete this email.",
+            },
+            Confirmation: {
+                Prehead: 'Confirm your signup to leadminer',
+                Title: 'Confirm Your Email Address',
+                Body1: 'Welcome to ',
+                Body2:
+                    "! We're happy that you've selected us to help you generate clean and enriched contacts from your mailbox. Click the button below to confirm your email address and activate your account.",
+                Body3: "If you didn't sign up with ",
+                Body4: ', you can safely disregard this email.',
+                Button: 'Confirm your email',
+                Regards: 'Best regards,',
+                Footer:
+                    "You received this email because we received a request for your account. If you didn't request it, you can safely delete this email.",
+            },
+            Recovery: {
+                Prehead: ' Reset Your Password',
+                Title: ' Reset Your Password',
+                Body1: 'Tap the button below to reset your',
+                Body2:
+                    "account password. If you didn't request a new password, you can safely delete this email.",
+                Button: 'Confirm your email',
+                Regards: 'Best regards,',
+                Footer:
+                    "You received this email because we received a request for your account. If you didn't request it, you can safely delete this email.",
+            },
+            EmailChange: {
+                Prehead: 'Confirm your email change',
+                Title: 'Confirm your email change',
+                Body1: 'Welcome to ',
+                Body2:
+                    "! We're happy to assist you with keeping your account information up to date. You recently requested to change the email address associated with your ",
+                Body3:
+                    ' account. Please click the button below to confirm your new email address and complete the update.',
+                Body4:
+                    "If you didn't request this change, you can safely disregard this email. Your current email address will remain unchanged.",
+                Button: 'Confirm Email Address',
+                Regards: 'Best regards,',
+                Footer:
+                    "You received this email because we received a request for your account. If you didn't request it, you can safely delete this email.",
+            },
+        },
+    ],
+    [
+        'fr',
+        {
+            Invite: {
+                Prehead: 'Invitation à leadminer',
+                Title: 'Vous êtes invité à rejoindre',
+                Body1: 'Vous êtes invité à rejoindre ',
+                Body2:
+                    "! Nous sommes ravis de vous accueillir dans notre communauté. Avec notre plateforme, vous pouvez générer facilement des contacts propres et enrichis directement depuis votre boîte mail. Pour commencer, cliquez sur le bouton ci-dessous pour accepter l'invitation et créer votre compte.",
+                Button: "Accepter l'invitation",
+                Regards: 'Cordialement,',
+                Footer:
+                    "Vous avez reçu cet e-mail car nous avons reçu une demande pour votre compte. Si vous n'avez pas fait cette demande, vous pouvez supprimer cet e-mail en toute sécurité.",
+            },
+            Confirmation: {
+                Prehead: 'Confirmez votre inscription à leadminer',
+                Title: 'Confirmez votre adresse e-mail',
+                Body1: 'Bienvenue à ',
+                Body2:
+                    '! Nous sommes heureux que vous nous ayez choisis pour vous aider à générer des contacts propres et enrichis à partir de votre boîte aux lettres. Cliquez sur le bouton ci-dessous pour confirmer votre adresse e-mail et activer votre compte.',
+                Body3: 'Si vous ne vous êtes pas inscrit auprès de ',
+                Body4: ', vous pouvez ignorer cet e-mail en toute sécurité.',
+                Button: 'Confirmez votre e-mail',
+                Regards: 'Cordialement,',
+                Footer:
+                    "Vous avez reçu cet e-mail parce que nous avons reçu une demande concernant votre compte. Si vous ne l'avez pas demandé, vous pouvez supprimer cet e-mail en toute sécurité.",
+            },
+            Recovery: {
+                Prehead: 'Réinitialisez votre mot de passe',
+                Title: 'Réinitialisez votre mot de passe',
+                Body1:
+                    'Appuyez sur le bouton ci-dessous pour réinitialiser le mot de passe de votre compte',
+                Body2:
+                    "Si vous n'avez pas demandé un nouveau mot de passe, vous pouvez supprimer cet e-mail en toute sécurité.",
+                Button: 'Confirmez votre e-mail',
+                Regards: 'Cordialement,',
+                Footer:
+                    "Vous avez reçu cet e-mail car nous avons reçu une demande pour votre compte. Si vous n'avez pas fait cette demande, vous pouvez supprimer cet e-mail en toute sécurité.",
+            },
+            EmailChange: {
+                Prehead: "Confirmez votre changement d'adresse e-mail",
+                Title: "Confirmez votre changement d'adresse e-mail",
+                Body1: 'Bienvenue à ',
+                Body2:
+                    "! Nous sommes heureux de vous aider à mettre à jour les informations relatives à votre compte. Vous avez récemment demandé à changer l'adresse e-mail associée à votre compte ",
+                Body3:
+                    '. Veuillez cliquer sur le bouton ci-dessous pour confirmer votre nouvelle adresse e-mail et terminer la mise à jour.',
+                Body4:
+                    "Si vous n'avez pas demandé ce changement, vous pouvez ignorer cet e-mail. Votre adresse e-mail actuelle restera inchangée.",
+                Button: 'Confirmez votre adresse e-mail',
+                Regards: 'Cordialement,',
+                Footer:
+                    "Vous avez reçu cet e-mail parce que nous avons reçu une demande concernant votre compte. Si vous ne l'avez pas demandé, vous pouvez supprimer cet e-mail en toute sécurité.",
+            },
+        },
+    ],
+]);
+
+export default supabaseEmailsI18n
