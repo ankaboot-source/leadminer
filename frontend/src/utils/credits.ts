0b776bc912f9a8eede415474ddbb0805fe77e531
Jaafoura <mehdi.jaafourag@gmail.com>
2024-09-20 12:58:29 +0100

    Remove commercial related code

--- frontend/src/utils/credits.ts
+++ frontend/src/utils/credits.ts
@@ -1,51 +1,10 @@
-export const CREDITS_PER_CONTACT = 1;
-export const CREDITS_MIN_THRESHOLD = 100;
-const $profile = useSupabaseUserProfile();
+export const CreditsCounter = null;
+export const CreditsDialog = null;
+export const CreditsDialogRef = ref();
 
-export function refillCredits() {
-  window.open(useRuntimeConfig().public.EXTERNAL_REFILL_CREDITS_LINK, '_blank');
-}
-
-const showCreditsBadge = useRuntimeConfig().public.ENABLE_CREDIT;
-
-export const CreditsCounter = showCreditsBadge
-  ? defineAsyncComponent(
-      () => import('~/components/Credits/CreditsCounter.vue'),
-    )
-  : null;
-
-export const CreditsDialog = showCreditsBadge
-  ? defineAsyncComponent(() => import('~/components/Credits/CreditsDialog.vue'))
-  : null;
-
-export const CreditsDialogRef = ref<InstanceType<
-  NonNullable<typeof CreditsDialog>
-> | null>();
-export const openCreditsDialog = (
-  hasDeficientCredits: boolean,
-  totalUnits: number,
-  availableUnits: number,
-  availableAlreadyUnits: number,
-) => {
-  CreditsDialogRef.value?.openModal(
-    hasDeficientCredits,
-    totalUnits,
-    availableUnits,
-    availableAlreadyUnits,
-  );
+export const openCreditsDialog = (..._args: (number | boolean)[]) => {
+  return;
 };
-
-export function useCreditsDialog(contactsToEnrich: string[] | undefined) {
-  if (!showCreditsBadge) return false;
-
-  const noCredits = $profile.value?.credits === 0;
-  const moreContactsThanCredits =
-    contactsToEnrich &&
-    contactsToEnrich?.length > ($profile.value?.credits ?? 0);
-
-  if (noCredits || moreContactsThanCredits) {
-    openCreditsDialog(true, 0, contactsToEnrich?.length ?? 0, 0);
-    return true;
-  }
+export function useCreditsDialog(_arg: string[] | undefined) {
   return false;
 }
