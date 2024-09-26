c45e6dcaf1b4c6b6b78f55da31e6d62172af623e
Jaafoura <mehdi.jaafourag@gmail.com>
2024-09-26 10:56:55 +0100

    SupabaseUsersResolver: remove unused functions

--- backend/src/utils/credits-plugin/database/SupabaseUsersResolver.ts
+++ backend/src/utils/credits-plugin/database/SupabaseUsersResolver.ts
@@ -45,33 +45,6 @@ export default class SupabaseUsers implements Users {
     return user;
   }
 
-  async getProfileByColumn(
-    column: string,
-    value: string
-  ): Promise<Profile | null> {
-    const { data: user, error } = await this.supabaseClient
-      .from('profiles')
-      .select('*')
-      .eq(column, value);
-
-    if (error) {
-      throw new Error(error.message);
-    }
-
-    return user[0] ?? null;
-  }
-
-  async getByEmail(email: string): Promise<Profile | null> {
-    return await this.getProfileByColumn('email', email);
-  }
-
-  async getBySubscriptionId(subscriptionId: string): Promise<Profile | null> {
-    return await this.getProfileByColumn(
-      'stripe_subscription_id',
-      subscriptionId
-    );
-  }
-
   async inviteUserByEmail(userEmail: string): Promise<true | Error> {
     const { error: inviteUserError } =
       await this.supabaseClient.auth.admin.inviteUserByEmail(userEmail, {
