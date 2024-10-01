8438ed972b85835b7646e7f18abcbfea3fe391e5
Jaafoura <mehdi.jaafourag@gmail.com>
2024-10-01 23:21:39 +0100

    Update password.ts

--- frontend/src/utils/password.ts
+++ frontend/src/utils/password.ts
@@ -1,5 +1,5 @@
 export const PASSWORD_PATTERN =
-  /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/g;
+  /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/g; // PrimeVue's password mediumRegex
 
 export const isInvalidPassword = (password: string) =>
   Boolean(password) && !PASSWORD_PATTERN.test(password);
