4083b943be075ce1ecf8cb2b7f08f831f055e19e
Jaafoura <mehdi.jaafourag@gmail.com>
2024-10-02 10:20:03 +0100

    password verification in settings

--- frontend/src/utils/password.ts
+++ frontend/src/utils/password.ts
@@ -3,3 +3,12 @@ export const PASSWORD_PATTERN =
 
 export const isInvalidPassword = (password: string) =>
   Boolean(password) && !PASSWORD_PATTERN.test(password);
+
+export const hasLowerCase = (password: string) =>
+  Boolean(password) && /.*[a-z]+.*/g.test(password);
+
+export const hasUpperCase = (password: string) =>
+  Boolean(password) && /.*[A-Z]+.*/g.test(password);
+
+export const hasNumber = (password: string) =>
+  Boolean(password) && /.*[0-9]+.*/g.test(password);
