ca155e565c7aa0c8217b2826e90415229c8624cc
Malek Salem <malek.salem.14@gmail.com>
2024-10-04 03:26:49 +0100

    fix styling

--- backend/src/routes/enrichment.routes.ts
+++ backend/src/routes/enrichment.routes.ts
@@ -3,9 +3,7 @@ import initializeAuthMiddleware from '../middleware/auth';
 import AuthResolver from '../services/auth/AuthResolver';
 import initializeEnrichmentController from '../controllers/enrichment.controller';
 
-export default function initializeEnrichmentRoutes(
-  authResolver: AuthResolver
-) {
+export default function initializeEnrichmentRoutes(authResolver: AuthResolver) {
   const router = Router();
 
   const { enrich, webhook } = initializeEnrichmentController();
