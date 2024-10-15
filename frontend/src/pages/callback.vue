6c2547008c48ee6aac4ef152e3adc84adf9b293e
Malek salem <malek@ankaboot.io>
2024-10-15 12:48:02 +0100

    fix: use nextTick for proper hydration

--- frontend/src/pages/callback.vue
+++ frontend/src/pages/callback.vue
@@ -19,8 +19,9 @@ const { error_description: errorDescription, navigate_to: navigateToPage } = {
 
 watch(
   user,
-  () => {
+  async () => {
     if (user.value) {
+      await nextTick()
       navigateTo(`${navigateToPage ?? '/dashboard'}`);
     }
   },
