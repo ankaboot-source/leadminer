db79686000f74336ee2fd076b123d3a0f532a2b0
Malek Salem <malek.salem.14@gmail.com>
2024-11-13 12:06:35 +0100

    update parsing url logic

--- frontend/src/utils/images.ts
+++ frontend/src/utils/images.ts
@@ -1,7 +1,10 @@
-export function getImageViaProxy(url: string, proxy?: string) {
+export function getImageViaProxy(url: string) {
+  const proxy = useRuntimeConfig().public.IMAGE_REVERSE_PROXY?.toString();
+
   if (!proxy) {
     return url;
   }
-  const result = url.replace(/^https?:\/\/(.+)/, `${proxy}/$1`);
+  const parsedUrl = new URL(url);
+  const result = `${proxy}/${parsedUrl.host}${parsedUrl.pathname}${parsedUrl.search}`;
   return result;
 }
