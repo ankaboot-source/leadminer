06da1de240ac09e96bc1c4b546c66f639106dc41
Malek Salem <malek.salem.14@gmail.com>
2024-12-05 10:44:46 +0100

    fix typos

--- supabase/migrations/20241202215826_delete_expired_cache_routine.sql
+++ supabase/migrations/20241202215826_delete_expired_cache_routine.sql
@@ -6,8 +6,8 @@ grant all privileges on all tables in schema cron to postgres;
 
 CREATE FUNCTION private.delete_expired_clean_cache (delete_interval INTERVAL) RETURNS void AS $$
 BEGIN
-    DELETE FROM public.public.email_status
-    WHERE verified_on < now() - delete_interval;
+    DELETE FROM public.email_status
+    WHERE verified_on <= NOW() - delete_interval;
 END;
 $$ LANGUAGE plpgsql
 SET search_path = '';
@@ -15,18 +15,17 @@ SET search_path = '';
 SELECT cron.schedule(
     'delete-expired-clean-cache',
     '0 0 1 */6 *', --  Every 6 months
-    $$SELECT private.delete_expired_cache_enrich_function('6 months');$$
+    $$SELECT private.delete_expired_clean_cache(INTERVAL '6 months');$$
 );
 
 CREATE FUNCTION private.delete_expired_enrich_cache(delete_interval INTERVAL)
 RETURNS void AS $$
 BEGIN
     DELETE FROM public.tasks
-        WHERE
-        AND t.status = 'done'
-        AND t.category = 'enriching'
-        AND t.type = 'enrich'
-        AND t.started_at < NOW() - delete_interval;
+    WHERE status = 'done'
+      AND category = 'enriching'
+      AND type = 'enrich'
+      AND started_at <= NOW() - delete_interval;
 END;
 $$ LANGUAGE plpgsql
 SET search_path = '';
@@ -34,5 +33,5 @@ SET search_path = '';
 SELECT cron.schedule(
     'delete-expired-enrich-cache',
     '0 0 */100 * *', -- Every 100 days at midnight
-    $$SELECT delete_expired_enrich_cache('100 days');$$
+    $$SELECT delete_expired_enrich_cache(INTERVAL '100 days');$$
 );
