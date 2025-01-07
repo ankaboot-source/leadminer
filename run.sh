b2ae72b74fe8d1a0def06d2ac5e2f67aab33e37b
Malek Salem <malek.salem.14@gmail.com>
2025-01-07 15:25:42 +0100

    fix formatting

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ npm run dev:backend-api &
 npm run dev:backend-worker &
 npm run dev:backend-email-worker &
 npm run dev:backend-mock-external-services&
-npm run dev:frontend&
+npm run dev:frontend &
 wait
