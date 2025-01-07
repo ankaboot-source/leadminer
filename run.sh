5c7d9e0364b625a45e289f68fb6e923e1c6308b7
Malek Salem <malek.salem.14@gmail.com>
2025-01-07 16:10:12 +0100

    update script

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
 npm run dev:backend-api &
 npm run dev:backend-worker &
 npm run dev:backend-email-worker &
-npm run dev:backend-mock-external-services&
+npm run dev:backend-mock-external-services &
 npm run dev:frontend &
 wait
