6d2b112c93bb3983c8d64e00987e13b4dab177e8
Malek Salem <malek.salem.14@gmail.com>
2025-01-08 11:44:38 +0100

    add supabase edge-functions

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ npm run dev:backend-api &
 npm run dev:backend-worker &
 npm run dev:backend-email-worker &
 npm run dev:backend-mock-external-services &
+npm run dev:supabase-functions &
 npm run dev:frontend &
 wait
