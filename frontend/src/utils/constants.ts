0daa014969b79de40063f4c12226f9dcf1c7229f
Jaafoura <mehdi.jaafourag@gmail.com>
2025-01-13 18:57:58 +0100

    fix email regex: should be single email address

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const maxFileSize = 3000000; // 3MB
 export const maxSizeInMB = maxFileSize / 1000000;
-export const REGEX_EMAIL = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i;
+export const REGEX_EMAIL = /^\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b$/i;
