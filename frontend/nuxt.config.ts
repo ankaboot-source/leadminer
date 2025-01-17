c8d20a51758fa77eb45943bef71ab6a1d1c778a8
Malek Salem <malek.salem.14@gmail.com>
2025-01-17 23:46:31 +0100

    fix pwa config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -171,10 +171,7 @@ export default defineNuxtConfig({
       ],
     },
     workbox: {
-      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
-    },
-    injectManifest: {
-      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      navigateFallback: "/",
     },
     devOptions: {
       enabled: true,
