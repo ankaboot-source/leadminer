ff499682a1ad639407f04b3a4823477ad488522c
Malek Salem <malek.salem.14@gmail.com>
2025-01-27 18:38:54 +0100

    show table only when there is contacts

--- frontend/src/pages/dashboard.vue
+++ frontend/src/pages/dashboard.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="flex flex-col grow">
     <mining-stepper v-model:collapsed="$stepper.collapsed" />
-    <MiningTable />
+    <div v-show="showTable">
+      <MiningTable />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-const MiningTable = defineAsyncComponent(
-  () => import('../components/Mining/Table/MiningTable.vue'),
-);
 
 const $stepper = useMiningStepper();
+const $contactsStore = useContactsStore();
+const showTable = computed(() => Boolean($contactsStore.contactsList))
 $stepper.open();
 </script>
