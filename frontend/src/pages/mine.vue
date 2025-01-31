05b2aec0c72012fb97e3a00fdcf56f79821746c1
Jaafoura <mehdi.jaafourag@gmail.com>
2025-01-31 18:00:01 +0100

    go to `/contacts` when finished mining

--- frontend/src/pages/mine.vue
+++ frontend/src/pages/mine.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="flex flex-col grow">
     <MiningStepper
-      v-if="!$leadminerStore.miningStartedAndFinished"
       v-model:collapsed="$stepper.collapsed"
       :is-toggleable="showTable"
     />
@@ -10,13 +9,5 @@
 </template>
 <script setup lang="ts">
 const $stepper = useMiningStepper();
-const $leadminerStore = useLeadminerStore();
 const showTable = computed(() => $stepper.index !== 1);
-
-onMounted(() => {
-  if ($leadminerStore.miningStartedAndFinished) {
-    $stepper.$reset();
-    $leadminerStore.$resetMining();
-  }
-});
 </script>
