d29dcd93aa9b2bae3c441dff6aa986cae76b5629
mohamedazizhammami <aziz@ankaboot.io>
2025-02-05 17:01:38 +0100

    Fix: remove any type usage

--- frontend/src/components/Mining/StepperPanels/MinePanel.vue
+++ frontend/src/components/Mining/StepperPanels/MinePanel.vue
@@ -174,8 +174,11 @@ onMounted(async () => {
 
   try {
     await $leadminerStore.fetchInbox();
-  } catch (error: any) {
-    if (error?.statusCode === 502 || error?.statusCode === 503) {
+  } catch (error: unknown) {
+    if (
+      error instanceof FetchError &&
+      (error.statusCode === 502 || error.statusCode === 503)
+    ) {
       $stepper.prev();
       throw error;
     } else {
