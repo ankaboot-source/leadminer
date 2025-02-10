4d5fc8c2fe0c720506a87d45ff92f6910e114e3b
Malek Salem <malek.salem.14@gmail.com>
2025-02-10 16:03:08 +0100

    add sentry frontend

---
+++ frontend/sentry.client.config.ts
@@ -0,0 +1,24 @@
+import { useRuntimeConfig } from '#imports';
+import * as Sentry from '@sentry/nuxt';
+
+Sentry.init({
+  dsn: useRuntimeConfig().public.SENTRY_DSN,
+  environment: useRuntimeConfig().public.SENTRY_ENVIRONMENT,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for tracing.
+  // We recommend adjusting this value in production
+  // Learn more at
+  // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
+  tracesSampleRate: 1.0,
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  // Learn more at
+  // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
