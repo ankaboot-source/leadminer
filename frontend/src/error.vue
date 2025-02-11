a622109b060381c54ff54d5dafd40e5a77f00d27
Malek Salem <malek.salem.14@gmail.com>
2025-02-11 21:12:28 +0100

    fix deepsource related issue

--- frontend/src/error.vue
+++ frontend/src/error.vue
@@ -41,9 +41,9 @@
 <script setup lang="ts">
 import type { NuxtError } from '#app';
 
-const { error } = defineProps({
-  error: Object as () => NuxtError,
-});
+const { error } = defineProps<{
+  error: NuxtError;
+}>();
 
 const { t } = useI18n({
   useScope: 'local',
