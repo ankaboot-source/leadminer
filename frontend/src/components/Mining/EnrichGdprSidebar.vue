bb4e33376df4c87c74c52bfc2e92167150159757
Jaafoura <73950268+J43fura@users.noreply.github.com>
2025-02-13 15:56:48 +0100

    Simplify Enrich GDPR sidebar (#2079)

--- frontend/src/components/Mining/EnrichGdprSidebar.vue
+++ frontend/src/components/Mining/EnrichGdprSidebar.vue
@@ -16,21 +16,12 @@
     </div>
     <ul class="my-3">
       <li class="mb-3">
-        <div class="text-lg font-semibold">
-          {{ t('purposeOfEnrichment.title') }}
-        </div>
         <div>
-          {{ t('purposeOfEnrichment.content') }}
+          {{ t('purposeOfEnrichment') }}
+          {{ t('thirdPartyTools') }}
         </div>
       </li>
-      <li class="mb-3">
-        <div class="text-lg font-semibold">
-          {{ t('thirdPartyTools.title') }}
-        </div>
-        <div>
-          {{ t('thirdPartyTools.content') }}
-        </div>
-      </li>
+
       <li>
         <div class="text-lg font-semibold">
           {{ t('yourResponsibilities.title') }}
@@ -51,9 +42,6 @@
         </ul>
       </li>
     </ul>
-    <div>
-      {{ t('proceedConfirmation') }}
-    </div>
     <template #footer>
       <Button
         severity="secondary"
@@ -120,42 +108,28 @@ defineExpose({ openModal });
   "en": {
     "beforeYouProceed": "Before You Proceed: Enrich My Contacts",
     "acknowledge": "By clicking \"Enrich My Contacts\", you acknowledge and agree to the following:",
-    "purposeOfEnrichment": {
-      "title": "Purpose of Enrichment",
-      "content": "This service is intended to enrich information about your existing contacts. You must have a legitimate interest or have gathered their explicit consent to perform this enrichment for a specific and lawful purpose."
-    },
-    "thirdPartyTools": {
-      "title": "Third-Party Tools",
-      "content": "The enrichment process uses TheDig, Proxycurl, and VoilaNorbert. Results are cached securely for 90 days to improve efficiency and ensure data integrity."
-    },
+    "purposeOfEnrichment": "This service is intended to enrich information about your existing contacts.",
+    "thirdPartyTools": "The enrichment process uses TheDig, Proxycurl, and VoilaNorbert. Results are cached securely for 90 days to improve efficiency and ensure data integrity.",
     "yourResponsibilities": {
       "title": "Your Responsibilities",
-      "content_1": "It is your responsibility to ensure compliance with applicable laws, including data protection regulations.",
+      "content_1": "You must have a legitimate interest or have obtained explicit consent from your contacts to perform this enrichment for specific and legal purposes.",
       "content_2": "You must inform your contacts about the use of this service and any data you enrich on their behalf.",
       "content_3": "Your contacts hold their full rights including the rights to access and delete any information about them."
     },
-    "proceedConfirmation": "By proceeding, you confirm that you have a legitimate interest or proper consent to enrich data on the selected contacts and accept your responsibility for compliance.",
     "privacyPolicyButton": "Read the data privacy policy",
     "enrichButton": "Enrich My Contacts"
   },
   "fr": {
     "beforeYouProceed": "Avant de continuer : Enrichir mes contacts",
     "acknowledge": "En cliquant sur \"Enrichir mes contacts\", vous reconnaissez et acceptez les éléments suivants :",
-    "purposeOfEnrichment": {
-      "title": "Objectif de l'enrichissement",
-      "content": "Ce service est destiné à enrichir les informations sur vos contacts existants. Vous devez avoir un intérêt légitime ou avoir obtenu leur consentement explicite pour effectuer cet enrichissement à des fins spécifiques et légales."
-    },
-    "thirdPartyTools": {
-      "title": "Outils tiers",
-      "content": "Le processus d'enrichissement utilise TheDig, Proxycurl et VoilaNorbert. Les résultats sont mis en cache en toute sécurité pendant 90 jours pour améliorer l'efficacité et garantir l'intégrité des données."
-    },
+    "purposeOfEnrichment": "Ce service est destiné à enrichir les informations sur vos contacts existants.",
+    "thirdPartyTools": "Le processus d'enrichissement utilise TheDig, Proxycurl et VoilaNorbert. Les résultats sont mis en cache en toute sécurité pendant 90 jours pour améliorer l'efficacité et garantir l'intégrité des données.",
     "yourResponsibilities": {
       "title": "Vos responsabilités",
-      "content_1": "Il est de votre responsabilité de vous conformer aux lois applicables, y compris les réglementations sur la protection des données.",
+      "content_1": "Vous devez avoir un intérêt légitime ou avoir obtenu le consentement explicite de vos contacts pour effectuer cet enrichissement à des fins spécifiques et légales.",
       "content_2": "Vous devez informer vos contacts de l'utilisation de ce service et de toute donnée que vous enrichissez en leur nom.",
       "content_3": "Vos contacts conservent tous leurs droits, y compris les droits d'accès et de suppression de toute information les concernant."
     },
-    "proceedConfirmation": "En continuant, vous confirmez que vous avez un intérêt légitime ou un consentement approprié pour enrichir les données des contacts sélectionnés et acceptez votre responsabilité en matière de conformité.",
     "privacyPolicyButton": "Lire la politique de confidentialité des données",
     "enrichButton": "Enrichir mes contacts"
   }
