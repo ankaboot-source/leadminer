6a6e5d80b2dd8d41e27e0165f06274279f1c12de
mohamedazizhammami <aziz@ankaboot.io>
2025-02-24 19:34:43 +0100

    Fix incorrect variable names

--- frontend/src/pages/account/settings.vue
+++ frontend/src/pages/account/settings.vue
@@ -119,7 +119,7 @@
     <!-- Legal Account Section -->
     <Panel :header="$t('auth.legal_information')">
       <LegalInformation />
-      <component :is="NewsletterCheckbox" type="checkbox" />
+      <component :is="AcceptNewsLetter" type="checkbox" />
     </Panel>
 
     <!-- Warning modal Section -->
