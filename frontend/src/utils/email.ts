27c860883aa661f9789b595923b79a70cca5d6c5
Jaafoura <mehdi.jaafourag@gmail.com>
2025-03-17 14:37:02 +0100

    fix email regex
    add the rest of printable characters to email's regex's local-part

--- frontend/src/utils/email.ts
+++ frontend/src/utils/email.ts
@@ -1,5 +1,5 @@
 export const REGEX_EMAIL =
-  /^\b[A-Z0-9._%+-]{1,64}@[A-Z0-9.-]{0,66}\.[A-Z]{2,18}\b$/i;
+  /^\b[A-Z0-9!#$%&'*+-/=?^_`{|}~.]{1,64}@[A-Z0-9.-]{0,66}\.[A-Z]{2,18}\b$/i;
 
 export const isInvalidEmail = (email: string) =>
   Boolean(email) && !REGEX_EMAIL.test(email);
