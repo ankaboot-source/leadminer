eaab20c1e904c1e4ba5af3659ac6d5d89ef81750
Malek salem <malek@ankaboot.io>
2025-03-26 10:09:04 +0100

    fix `mining_source` not added bug & improve maintainability (#2209)

--- frontend/src/plugins/sb-user-listeners.ts
+++ frontend/src/plugins/sb-user-listeners.ts
@@ -5,7 +5,6 @@ import type {
 } from '@supabase/supabase-js';
 import { useSupabaseUserProfile } from '~/composables/useSupabaseUserProfile';
 import type { Profile } from '~/types/profile';
-import type { $Fetch } from 'nitropack';
 
 /**
  * Retrieves the current user's profile from the Supabase database.
@@ -34,18 +33,13 @@ async function getCurrentUserProfile(client: SupabaseClient) {
  * @returns A promise that resolves once the mining source has been added.
  */
 async function addMiningSourceFromProviderToken(
-  api: $Fetch,
   provider: string,
   providerToken: string,
 ): Promise<void> {
-  try {
-    await api('add-mining-source', {
-      method: 'POST',
-      body: { provider, provider_token: providerToken },
-    });
-  } catch (err) {
-    console.error('Failed to add mining source:', err);
-  }
+  await useNuxtApp().$saasEdgeFunctions('add-mining-source', {
+    method: 'POST',
+    body: { provider, provider_token: providerToken },
+  });
 }
 
 /**
@@ -56,100 +50,100 @@ async function addMiningSourceFromProviderToken(
  * @param language - The language code (e.g., 'en', 'fr') for the email templates.
  * @returns A promise that resolves once the email templates are updated.
  */
-async function updateUserEmailTemplatesI18n(
-  api: $Fetch,
-  client: SupabaseClient,
-  language: string,
-): Promise<void> {
-  try {
-    await api('email-templates', {
-      method: 'POST',
-      body: { language },
-    });
-    /**
-     * Trigger a session sync to ensure templates don't get updated again on refresh.
-     */
-    await client.auth.updateUser({});
-  } catch (err) {
-    console.error('Failed to update email templates:', err);
+async function updateUserEmailTemplatesI18n(language: string): Promise<void> {
+  await useNuxtApp().$saasEdgeFunctions('email-templates', {
+    method: 'POST',
+    body: { language },
+  });
+  /**
+   * Trigger a session sync to ensure templates don't get updated again on refresh.
+   */
+  const { error } = await useSupabaseClient().auth.updateUser({});
+
+  if (error) throw error;
+}
+
+/**
+ * Updates the user's metadata to mark first-time sign-in as true.
+ *
+ * @param supabase - The Supabase client instance.
+ * @returns Resolves when the update is complete.
+ */
+async function updateFirstTimeSignIn() {
+  const { error } = await useSupabaseClient().auth.updateUser({
+    data: { first_time_signin: true },
+  });
+
+  if (error) throw error;
+}
+
+/**
+ * Handles first-time sign-in logic.
+ */
+async function handleFirstTimeSignIn() {
+  const $user = useSupabaseUser();
+  const $session = useSupabaseSession();
+
+  const provider = $user.value?.app_metadata.provider;
+  const providerToken = $session.value?.provider_token;
+  const firstTimeSignin = $user.value?.user_metadata.first_time_signin;
+  const emailTemplate = $user.value?.user_metadata.EmailTemplate;
+  const language = navigator.language.split('-')[0];
+
+  if (!firstTimeSignin && provider && providerToken) {
+    await addMiningSourceFromProviderToken(provider, providerToken);
+    await updateFirstTimeSignIn();
   }
+
+  if (!emailTemplate || emailTemplate.language !== language) {
+    await updateUserEmailTemplatesI18n(language);
+  }
+}
+
+function createUserProfileRealtimeChannel(userId: string) {
+  const $currentProfile = useSupabaseUserProfile();
+
+  return useSupabaseClient()
+    .channel('user-profile-realtime')
+    .on(
+      'postgres_changes',
+      {
+        event: '*',
+        schema: 'private',
+        table: 'profiles',
+        filter: `user_id=eq.${userId}`,
+      },
+      (payload: RealtimePostgresChangesPayload<Profile>) => {
+        $currentProfile.value = payload.new as Profile;
+      },
+    );
 }
 
 export default defineNuxtPlugin({
   name: 'user-listeners',
   setup() {
+    const $user = useSupabaseUser();
     const $supabaseClient = useSupabaseClient();
     const $currentProfile = useSupabaseUserProfile();
 
-    let channel: RealtimeChannel | null;
     const authenticated = ref(false);
-
-    /**
-     * Starts watching for authentication changes.
-     */
-    const startAuthenticationWatcher = () => {
-      const $user = useSupabaseUser();
-      const $session = useSupabaseSession();
-      const $saasEdgeFunctions = useNuxtApp().$saasEdgeFunctions as $Fetch;
-
-      watch(authenticated, async (auth) => {
-        if (!auth) {
-          if (channel) await $supabaseClient.removeChannel(channel);
-          return;
-        }
-        $currentProfile.value = await getCurrentUserProfile($supabaseClient);
-
-        const { provider, providerToken, emailTemplate, language } = {
-          provider: $user.value?.app_metadata.provider,
-          providerToken: $session.value?.provider_token,
-          emailTemplate: $user.value?.user_metadata.EmailTemplate,
-          language: navigator.language.split('-')[0],
-        };
-
-        // Use providerToken as a mining-source on first-time sign-in
-        // An empty emailTemplate signifies a first-time sign-in
-        if (!emailTemplate && provider && providerToken) {
-          await addMiningSourceFromProviderToken(
-            $saasEdgeFunctions,
-            provider,
-            providerToken,
-          );
-        }
-
-        if (!emailTemplate || emailTemplate.language !== language) {
-          await updateUserEmailTemplatesI18n(
-            $saasEdgeFunctions,
-            $supabaseClient,
-            language,
-          );
-        }
-
-        channel = $supabaseClient.channel('user-profile-realtime').on(
-          'postgres_changes',
-          {
-            event: '*',
-            schema: 'private',
-            table: 'profiles',
-            filter: `user_id=eq.${$user.value?.id}`,
-          },
-          (payload: RealtimePostgresChangesPayload<Profile>) => {
-            $currentProfile.value = payload.new as Profile;
-          },
-        );
-        channel.subscribe();
-      });
-    };
+    let channel: RealtimeChannel | null;
 
     if (import.meta.client) {
-      startAuthenticationWatcher();
+      watch(authenticated, async (auth) => {
+        if (!auth && channel) await $supabaseClient.removeChannel(channel);
+
+        if (!auth) return;
+
+        $currentProfile.value = await getCurrentUserProfile($supabaseClient);
+        await handleFirstTimeSignIn();
+        channel = createUserProfileRealtimeChannel($user.value?.id as string);
+        channel.subscribe();
+      });
     }
 
     $supabaseClient.auth.onAuthStateChange((_, session) => {
-      if (session) {
-        authenticated.value = true;
-      } else {
-        authenticated.value = false;
-      }
+      authenticated.value = session !== null;
     });
   },
 });
