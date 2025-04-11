a3f7e28c3aa22026e6aa56a44ede73ebfe1ea863
Malek Salem <malek.salem.14@gmail.com>
2025-04-11 12:18:13 +0100

    fix: typecast env vars to string

--- supabase/functions/_shared/supabase.ts
+++ supabase/functions/_shared/supabase.ts
@@ -1,8 +1,10 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.45.3";
 
-const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
-const SUPABASE_ANON_KEY = Deno.env.get("SUPABASE_ANON_KEY")!;
-const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
+const SUPABASE_URL = Deno.env.get("SUPABASE_URL") as string;
+const SUPABASE_ANON_KEY = Deno.env.get("SUPABASE_ANON_KEY") as string;
+const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get(
+  "SUPABASE_SERVICE_ROLE_KEY",
+) as string;
 
 export function createSupabaseClient(
   authorization = "",
