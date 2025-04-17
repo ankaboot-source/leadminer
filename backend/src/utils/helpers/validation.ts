25ead48048b9e0979458456e7193ce0c0aa83f10
Jaafoura <mehdi.jaafourag@gmail.com>
2025-04-17 16:33:20 +0100

    Update validation.ts

--- backend/src/utils/helpers/validation.ts
+++ backend/src/utils/helpers/validation.ts
@@ -1,22 +1,23 @@
+// skipcq: JS-0323
 function validateNumber(key: string, value: any) {
   return Number.isNaN(value) || value <= 0
     ? `${key} must be a valid positive number.`
     : null;
 }
-
+// skipcq: JS-0323
 function validateBoolean(key: string, value: any) {
   return !['true', 'false', true, false].includes(value)
     ? `${key} must be true or false.`
     : null;
 }
-
+// skipcq: JS-0323
 function validateStringArray(key: string, value: any) {
   return !Array.isArray(value) ||
     value.some((v) => typeof v !== 'string' || v.trim() === '')
     ? `${key} must be an array of non-empty strings.`
     : null;
 }
-
+// skipcq: JS-0323 - value can be of any type
 export default function validateType(key: string, value: any, type: string) {
   if (value === undefined || value === null) return `${key} is required.`;
 
