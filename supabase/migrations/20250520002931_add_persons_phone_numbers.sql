98e6b56080281ff057cec3494e907661187de0d4
Malek Salem <malek.salem.14@gmail.com>
2025-05-23 15:01:22 +0100

    rename phone_numbers to telephone

--- supabase/migrations/20250520002931_add_persons_phone_numbers.sql
+++ supabase/migrations/20250520002931_add_persons_phone_numbers.sql
@@ -1,5 +1,163 @@
 ALTER TABLE private.persons
-    ADD COLUMN phone_numbers text[] NULL;
+    ADD COLUMN telephone text[] NULL;
+
+DROP FUNCTION private.get_contacts_table;
+CREATE FUNCTION private.get_contacts_table(user_id uuid) RETURNS TABLE(source text, email text, name text, status text, image text, location text[], alternate_name text[], alternate_email text[], telephone text[], same_as text[], given_name text, family_name text, job_title text, works_for text, recency timestamp with time zone, seniority timestamp with time zone, occurrence integer, sender integer, recipient integer, conversations integer, replied_conversations integer, tags text[], updated_at timestamp without time zone, created_at timestamp without time zone)
+    LANGUAGE plpgsql
+    SET search_path = ''
+    AS $$
+BEGIN
+  RETURN QUERY WITH ExportedContacts AS (
+    SELECT
+      p.source as source_col,
+      p.email as email_col,
+      p.name as name_col,
+      p.status as status_col,
+      p.image as image_col,
+      p.location as location_col,
+      p.alternate_name as alternate_name_col,
+      p.alternate_email as alternate_email_col,
+      p.telephone as telephone_col,
+      p.same_as as same_as_col,
+      p.given_name as given_name_col,
+      p.family_name as family_name_col,
+      p.job_title as job_title_col,
+      o.name as works_for_col,
+      rp.recency as recency_col,
+      rp.seniority as seniority_col,
+      rp.occurrence as occurrence_col,
+      rp.sender as sender_col,
+      rp.recipient as recipient_col,
+      rp.conversations as conversations_col,
+      rp.replied_conversations as replied_conversations_col,
+      rp.tags as tags_col,
+      p.updated_at as updated_at_col,
+      p.created_at as created_at_col,
+      ROW_NUMBER() OVER (
+        PARTITION BY p.email
+      ) AS rn
+    FROM
+      private.persons p
+    INNER JOIN
+      private.refinedpersons rp ON rp.email = p.email
+    LEFT JOIN
+      private.organizations o ON o.id = p.works_for
+    WHERE
+      p.user_id = get_contacts_table.user_id
+    ORDER BY 
+      rp.occurrence DESC, rp.recency DESC
+	  )
+  SELECT
+    source_col AS source,
+    email_col AS email,
+    name_col AS name,
+    status_col AS status,
+    image_col as image,
+    location_col as location,
+    alternate_name_col as alternate_name,
+    alternate_email_col as alternate_email,
+    telephone_col as telephone,
+    same_as_col as same_as,
+    given_name_col as given_name,
+    family_name_col as family_name,
+    job_title_col as job_title,
+    works_for_col as works_for,
+    recency_col AS recency,
+    seniority_col AS seniority,
+    occurrence_col AS occurrence,
+    sender_col AS sender,
+    recipient_col AS recipient,
+    conversations_col AS conversations,
+    replied_conversations_col AS replied_conversations,
+    tags_col AS tags,
+	updated_at_col AS updated_at,
+	created_at_col AS created_at
+  FROM
+    ExportedContacts
+  WHERE
+    rn = 1;
+END;
+$$;
+
+DROP FUNCTION private.get_contacts_table_by_emails;
+CREATE FUNCTION private.get_contacts_table_by_emails(user_id uuid, emails text[]) RETURNS TABLE(source text, email text, name text, status text, image text, location text[], alternate_name text[], alternate_email text[], telephone text[], same_as text[], given_name text, family_name text, job_title text, works_for text, recency timestamp with time zone, seniority timestamp with time zone, occurrence integer, sender integer, recipient integer, conversations integer, replied_conversations integer, tags text[], updated_at timestamp without time zone, created_at timestamp without time zone)
+    LANGUAGE plpgsql
+    SET search_path = ''
+    AS $$
+BEGIN
+  RETURN QUERY WITH ExportedContacts AS (
+    SELECT
+      p.source as source_col,
+      p.email as email_col,
+      p.name as name_col,
+      p.status as status_col,
+      p.image as image_col,
+      p.location as location_col,
+      p.alternate_name as alternate_name_col,
+      P.alternate_email as alternate_email_col,
+      p.telephone as telephone_col,
+      p.same_as as same_as_col,
+      p.given_name as given_name_col,
+      p.family_name as family_name_col,
+      p.job_title as job_title_col,
+      o.name as works_for_col,
+      rp.recency as recency_col,
+      rp.seniority as seniority_col,
+      rp.occurrence as occurrence_col,
+      rp.sender as sender_col,
+      rp.recipient as recipient_col,
+      rp.conversations as conversations_col,
+      rp.replied_conversations as replied_conversations_col,
+      rp.tags as tags_col,
+      p.updated_at as updated_at_col,
+      p.created_at as created_at_col,
+      ROW_NUMBER() OVER (
+      PARTITION BY p.email
+      ) AS rn
+    FROM
+      private.persons p
+    INNER JOIN
+      private.refinedpersons rp ON rp.email = p.email
+    LEFT JOIN
+      private.organizations o ON o.id = p.works_for
+    WHERE
+      p.user_id = get_contacts_table_by_emails.user_id
+    AND
+      p.email = ANY(get_contacts_table_by_emails.emails)
+	ORDER BY 
+      rp.occurrence DESC, rp.recency DESC
+  )
+  SELECT
+    source_col AS source,
+    email_col AS email,
+    name_col AS name,
+    status_col AS status,
+    image_col as image,
+    location_col as location,
+    alternate_name_col as alternate_name,
+    alternate_email_col as alternate_email,
+    telephone_col as telephone,
+    same_as_col as same_as,
+    given_name_col as given_name,
+    family_name_col as family_name,
+    job_title_col as job_title,
+    works_for_col as works_for,
+    recency_col AS recency,
+    seniority_col AS seniority,
+    occurrence_col AS occurrence,
+    sender_col AS sender,
+    recipient_col AS recipient,
+    conversations_col AS conversations,
+    replied_conversations_col AS replied_conversations,
+    tags_col AS tags,
+	updated_at_col AS updated_at,
+	created_at_col AS created_at
+  FROM
+    ExportedContacts
+  WHERE
+    rn = 1;
+END;
+$$;
 
 CREATE OR REPLACE FUNCTION private.enrich_contacts(p_contacts_data jsonb[], p_update_empty_fields_only boolean DEFAULT true) RETURNS void
     LANGUAGE plpgsql
@@ -15,9 +173,9 @@ DECLARE
   old_alternate_name TEXT[];
   
   phone_number TEXT;
-  new_phone_numbers TEXT[];
-  old_phone_numbers TEXT[];
-  merged_phone_numbers TEXT[];
+  new_telephone TEXT[];
+  old_telephone TEXT[];
+  merged_telephone TEXT[];
 
 BEGIN
   -- Assert that all records have email and user_id
@@ -49,30 +207,30 @@ BEGIN
       organization_id := NULL;
     END IF;
 
-    -- Merge incoming phone numbers into phone_numbers array
-    new_phone_numbers := (
-        string_to_array(contact_record->>'phone_numbers', ',')
+    -- Merge incoming phone numbers into telephone array
+    new_telephone := (
+        string_to_array(contact_record->>'telephone', ',')
     );
 
-    IF new_phone_numbers IS NOT NULL THEN
-        SELECT p.phone_numbers
-            INTO old_phone_numbers
+    IF new_telephone IS NOT NULL THEN
+        SELECT p.telephone
+            INTO old_telephone
             FROM private.persons p
         WHERE p.email = contact_record->>'email'
             AND p.user_id = (contact_record->>'user_id')::UUID
         LIMIT 1;
 
-        IF old_phone_numbers IS NOT NULL THEN
+        IF old_telephone IS NOT NULL THEN
             -- append only the truly new ones
-            FOREACH phone_number IN ARRAY new_phone_numbers LOOP
-            IF NOT phone_number = ANY(old_phone_numbers) THEN
-                old_phone_numbers := ARRAY_APPEND(old_phone_numbers, phone_number);
+            FOREACH phone_number IN ARRAY new_telephone LOOP
+            IF NOT phone_number = ANY(old_telephone) THEN
+                old_telephone := ARRAY_APPEND(old_telephone, phone_number);
             END IF;
             END LOOP;
-            merged_phone_numbers := old_phone_numbers;
+            merged_telephone := old_telephone;
         ELSE
             -- no existing array → just take new
-            merged_phone_numbers := new_phone_numbers;
+            merged_telephone := new_telephone;
         END IF;
     END IF;
 
@@ -107,7 +265,7 @@ BEGIN
         works_for = COALESCE(pp.works_for, organization_id),
         identifiers = COALESCE(pp.identifiers, string_to_array(contact_record->>'identifiers', ',')::TEXT[]),
         status = COALESCE(pp.status, (contact_record->>'status')::TEXT),
-        phone_numbers = COALESCE(pp.phone_numbers, (merged_phone_numbers)::TEXT[])
+        telephone = COALESCE(pp.telephone, (merged_telephone)::TEXT[])
       WHERE 
         email = (contact_record->>'email')::TEXT AND
         user_id = (contact_record->>'user_id')::UUID;
@@ -126,7 +284,7 @@ BEGIN
         works_for = COALESCE(organization_id, pp.works_for),
         identifiers = COALESCE(string_to_array(contact_record->>'identifiers', ',')::TEXT[], pp.identifiers),
         status = COALESCE((contact_record->>'status')::TEXT, pp.status),
-        phone_numbers = COALESCE((merged_phone_numbers)::TEXT[], pp.phone_numbers)
+        telephone = COALESCE((merged_telephone)::TEXT[], pp.telephone)
 
       WHERE 
         email = (contact_record->>'email')::TEXT AND
