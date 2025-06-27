cb33e8d6a1e7d8f195a76e2dc26df21f08fb677e
Jaafoura <mehdi.jaafourag@gmail.com>
2025-06-27 16:41:18 +0100

    Update MiningTable.vue

--- frontend/src/components/Mining/Table/MiningTable.vue
+++ frontend/src/components/Mining/Table/MiningTable.vue
@@ -99,11 +99,11 @@
         <div class="ml-2">
           <i v-if="isLoading" class="pi pi-spin pi-spinner" />
           <template v-else>
-            <template v-if="!implicitSelectAll">
+            <template v-if="!implicitSelectAll && contactsLength">
               {{ implicitlySelectedContactsLength.toLocaleString() }}
               /
             </template>
-            {{ contactsLength?.toLocaleString() }}
+            {{ contactsLength?.toLocaleString() ?? 0 }}
           </template>
           {{ t('contacts') }}
         </div>
