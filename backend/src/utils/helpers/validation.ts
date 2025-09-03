de3586f66437afa98888d6fdd3f82685c8fe5f5a
Malek Salem <malek.salem.14@gmail.com>
2025-09-03 10:30:40 +0100

    ignore type any

--- backend/src/utils/helpers/validation.ts
+++ backend/src/utils/helpers/validation.ts
@@ -18,7 +18,7 @@ function validateStringArray(key: string, value: any) {
     : null;
 }
 
-// skipcq: JS-0296 
+// skipcq: JS-0296 JS-0323
 function validateString(key: String, value: any) {
   return typeof value !== 'string' || value.trim() === '' 
     ? `${key} must be a non-empty string.`
