7bc671ebb40ed3024406ea68304c5834306cc3aa
Malek Salem <malek.salem.14@gmail.com>
2025-09-05 13:24:43 +0100

    fix: add sse related header

--- backend/src/middleware/cors.ts
+++ backend/src/middleware/cors.ts
@@ -8,7 +8,8 @@ const corsOptions: CorsOptions = {
     'Authorization',
     'X-Requested-With',
     'Content-type',
-    'x-sb-jwt'
+    'x-sb-jwt',
+    'last-event-id'
   ],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204,
   credentials: true
