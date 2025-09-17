48d54a059d50d5c604b60cc3e998ce40c3dac28f
Malek Salem <malek.salem.14@gmail.com>
2025-09-17 10:59:37 +0100

    add event listener for pool errors

--- backend/src/db/pg/index.ts
+++ backend/src/db/pg/index.ts
@@ -1,9 +1,15 @@
 import { Pool } from 'pg';
 import ENV from '../../config';
+import logger from '../../utils/logger';
 
 const pool = new Pool({
   connectionString: ENV.PG_CONNECTION_STRING,
   max: 10
+});
+
+pool.on('error', function (error, client) {
+  logger.error('Error raised by pg-pool: ', error);
+  client.release();
 });
 
 export default pool;
