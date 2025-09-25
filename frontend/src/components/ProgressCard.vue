4983ece02e01df05b630d88428f374671357456a
Jaafoura <mehdi.jaafourag@gmail.com>
2025-09-25 17:19:43 +0100

    increase SUFFICIENT_ITEMS_FOR_ESTIMATION to 1000

--- frontend/src/components/ProgressCard.vue
+++ frontend/src/components/ProgressCard.vue
@@ -67,7 +67,7 @@ function togglePopover(event: MouseEvent) {
 const MIN_PROGRESS_FOR_ESTIMATION = 0.05; // wait 5% progress for estimation
 const MIN_ELAPSED_FOR_ESTIMATION = 5; // wait 5 seconds for estimation
 const SUFFICIENT_ELAPSED_FOR_ESTIMATION = 120; // estimate right away if elapsed time >= 120 seconds
-const SUFFICIENT_ITEMS_FOR_ESTIMATION = 100; // estimate right away if >=100 items treated
+const SUFFICIENT_ITEMS_FOR_ESTIMATION = 1000; // estimate right away if >=100 items treated
 const ESTIMATION_UPDATE_INTERVAL = 30; // update progress estimation once per 30 seconds
 
 const props = defineProps({
