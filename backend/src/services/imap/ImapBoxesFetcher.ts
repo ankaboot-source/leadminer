a906a9dd1687813b1f05999bb9eaa698bfb67bc5
Malek Salem <malek.salem.14@gmail.com>
2025-09-26 14:39:00 +0100

    add: fetch inboxes details using single connection

--- backend/src/services/imap/ImapBoxesFetcher.ts
+++ backend/src/services/imap/ImapBoxesFetcher.ts
@@ -1,12 +1,15 @@
+import { ImapFlow as Connection } from 'imapflow';
+import { Logger } from 'winston';
 import {
   buildFinalTree,
   createFlatTreeFromImap
 } from '../../utils/helpers/imapTreeHelpers';
-import ImapConnectionProvider from './ImapConnectionProvider';
+import { EXCLUDED_IMAP_FOLDERS } from '../../utils/constants';
 
 export default class ImapBoxesFetcher {
   constructor(
-    private readonly imapConnectionsProvider: ImapConnectionProvider
+    private readonly imapConnection: Connection,
+    private readonly logger: Logger
   ) {}
 
   /**
@@ -14,12 +17,42 @@ export default class ImapBoxesFetcher {
    * @returns IMAP tree.
    */
   async getTree(userEmail: string) {
-    const connection = await this.imapConnectionsProvider.acquireConnection();
-
-    const tree = await connection.list({
+    const tree = await this.imapConnection.list({
       statusQuery: { messages: true }
     });
 
     return buildFinalTree(createFlatTreeFromImap(tree), userEmail);
+  }
+
+  /**
+   * Fetches the total number of messages across the specified folders on an IMAP server.
+   */
+  async getTotalMessages(inboxes: string[]) {
+    let total = 0;
+
+    try {
+      // Create an array of Promises that resolve to the total number of messages in each folder.
+      const folders = inboxes.filter(
+        (folder) => !EXCLUDED_IMAP_FOLDERS.includes(folder)
+      );
+
+      folders.forEach(async (folder) => {
+        try {
+          const status = await this.imapConnection?.status(folder, {
+            messages: true
+          });
+          total += status?.messages ?? 0;
+        } catch (err) {
+          this.logger.warn(`Could not STATUS ${folder}`, err);
+        }
+      });
+      return total;
+    } catch (err) {
+      this.logger.error('Failed fetching total messages', {
+        folders: inboxes,
+        error: err
+      });
+      throw err;
+    }
   }
 }
