3c2a84c846d0648e9a11432706a1835c3ce275ae
Jaafoura <mehdi.jaafourag@gmail.com>
2025-10-06 13:43:27 +0100

    Update EmailAuth.vue

--- frontend/src/components/auth/EmailAuth.vue
+++ frontend/src/components/auth/EmailAuth.vue
@@ -42,9 +42,9 @@
         </template>
       </div>
       <div class="grid gap-1">
-        <label class="text-left" for="password">{{
-          $t('auth.password')
-        }}</label>
+        <label class="text-left" for="password">
+          {{ $t('auth.password') }}
+        </label>
         <Password
           v-model="password"
           :input-style="{ width: '100%' }"
@@ -182,9 +182,9 @@
             </template>
           </div>
           <div class="grid gap-1">
-            <label class="text-left" for="password">{{
-              $t('auth.password')
-            }}</label>
+            <label class="text-left" for="password">
+              {{ $t('auth.password') }}
+            </label>
             <Password
               v-model="password"
               :input-style="{ width: '100%' }"
