4a41383ac91e2df1b525801b3774ae97b4a94df6
Jaafoura <mehdi.jaafourag@gmail.com>
2025-11-14 12:34:55 +0100

    create and use component "NormalizedLocation"

---
+++ frontend/src/components/icons/NormalizedLocation.vue
@@ -0,0 +1,24 @@
+<template>
+  <i
+    v-if="location && Object.keys(location).length"
+    v-tooltip.top="location.display_name"
+    class="pi pi-map-marker text-primary-500 cursor-pointer hover:text-primary-600 transition-colors"
+    @click="goToLocation()"
+  />
+</template>
+
+<script setup lang="ts">
+import type { NormalizedLocation } from '~/types/contact';
+
+const { location } = defineProps<{
+  location: NormalizedLocation | null;
+}>();
+
+console.log('location', location);
+
+function goToLocation() {
+  if (!location || !location.lat || !location.lon) return;
+
+  window.open(getLocationUrl(location.lat, location.lon), '_blank');
+}
+</script>
