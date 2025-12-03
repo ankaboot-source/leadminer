6a9764d44fdee067df893a49964ffe4da1cfb209
Jaafoura <mehdi.jaafourag@gmail.com>
2025-12-03 16:06:50 +0100

    use OSM for OSS

--- frontend/src/utils/extras.ts
+++ frontend/src/utils/extras.ts
@@ -1,4 +1,9 @@
+import type { NormalizedLocation } from '~/types/contact';
+
 export const PrivacyPolicyButton = null;
 export const AcceptNewsLetter = null;
 // skipcq: JS-0321
 export function startMiningNotification() {}
+export function getLocationUrl(location: NormalizedLocation) {
+  return `https://www.openstreetmap.org/${location.osm_type}/${location.osm_id}`;
+}
