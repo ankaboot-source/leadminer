67d6352781c61d7108801b4c6e983eb881fbd514
Jaafoura <mehdi.jaafourag@gmail.com>
2025-12-19 14:18:03 +0100

    prettier

--- frontend/src/utils/sse.ts
+++ frontend/src/utils/sse.ts
@@ -1,8 +1,8 @@
 import {
   type EventSourceMessage,
   fetchEventSource,
-} from "@microsoft/fetch-event-source";
-import type { MiningType } from "~/types/mining";
+} from '@microsoft/fetch-event-source';
+import type { MiningType } from '~/types/mining';
 
 class SSE {
   private ctrl?: AbortController;
@@ -60,7 +60,7 @@ class SSE {
           const token = useSupabaseSession().value?.access_token;
 
           if (!token) {
-            throw new Error("[SSE] No access token available.");
+            throw new Error('[SSE] No access token available.');
           }
 
           console.debug(`[SSE] Setting up headers with x-sb-jwt: ${token}`);
@@ -69,18 +69,18 @@ class SSE {
             ...init,
             headers: {
               ...(init?.headers || {}),
-              "x-sb-jwt": token,
+              'x-sb-jwt': token,
             },
           });
         },
         onopen: async (response) => {
           if (response.status === 200) {
             console.debug(
-              "[SSE] Connection established successfully",
+              '[SSE] Connection established successfully',
               response,
             );
             console.debug(
-              "[SSE] Clearing any pending cleanup operations",
+              '[SSE] Clearing any pending cleanup operations',
               response,
             );
             this.clearPendingCleanup();
@@ -90,40 +90,40 @@ class SSE {
           this.clearPendingCleanup();
           const { id, event, data } = msg;
 
-          if (event === "close") {
-            if (id === "404-not-found") {
-              console.warn("[SSE] Task not found, closing connection.");
+          if (event === 'close') {
+            if (id === '404-not-found') {
+              console.warn('[SSE] Task not found, closing connection.');
               this.closeConnection();
               onError();
             } else {
-              console.log("[SSE] Server requested close.");
+              console.log('[SSE] Server requested close.');
               onClose();
             }
           } else if (event === `fetched-${miningId}`) {
             onFetchedUpdate(parseInt(data));
           } else if (event === `extracted-${miningId}`) {
             onExtractedUpdate(parseInt(data));
-          } else if (event === "fetching-finished") {
+          } else if (event === 'fetching-finished') {
             onFetchingDone(parseInt(data));
-          } else if (event === "extracting-finished") {
+          } else if (event === 'extracting-finished') {
             onExtractionDone(parseInt(data));
-          } else if (event === "cleaning-finished") {
+          } else if (event === 'cleaning-finished') {
             onCleaningDone(parseInt(data));
           } else if (event === `verifiedContacts-${miningId}`) {
             onVerifiedContacts(parseInt(data));
           } else if (event === `createdContacts-${miningId}`) {
             onCreatedContacts(parseInt(data));
-          } else if (event === "mining-completed") {
+          } else if (event === 'mining-completed') {
             onMiningCompleted();
           }
         },
         onerror: (err: unknown) => {
           if (!this.pendingCleanupTimeout) {
             console.warn(
-              "[SSE] Connection lost, scheduling cleanup in 1 minute.",
+              '[SSE] Connection lost, scheduling cleanup in 1 minute.',
             );
             this.pendingCleanupTimeout = setTimeout(() => {
-              console.error("[SSE] Cleanup triggered after connection loss.");
+              console.error('[SSE] Cleanup triggered after connection loss.');
               onError();
               this.closeConnection();
             }, this.cleanupDelay);
