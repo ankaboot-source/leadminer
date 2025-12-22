da5fb3cbf787d66f4f99514283958ec7cef7753c
Jaafoura <mehdi.jaafourag@gmail.com>
2025-12-22 22:32:39 +0100

    Update 20251218184211_create_pst_bucket.sql
    5GB maximum file

--- supabase/migrations/20251218184211_create_pst_bucket.sql
+++ supabase/migrations/20251218184211_create_pst_bucket.sql
@@ -4,7 +4,7 @@ VALUES (
   'pst', 
   'pst', 
   false, -- Private bucket 
-  52428800 -- 50MB MAXIMUM per individual file upload -- Premium plan allows up to ???
+  5368709120 -- 5GB MAXIMUM per individual file upload
 );
 
 CREATE POLICY "Give users authenticated access to folder 2dw1_0" ON storage.objects FOR INSERT TO public WITH CHECK (bucket_id = 'pst' AND (storage.foldername(name))[1] = 'private' AND auth.role() = 'authenticated');
