7b31d950db7303ca7971b8cc7091f7e15c3969ac
Jaafoura <mehdi.jaafourag@gmail.com>
2025-12-24 18:58:13 +0100

    create scheduled job to remove PST files

--- supabase/migrations/20251218184211_create_pst_bucket.sql
+++ supabase/migrations/20251218184211_create_pst_bucket.sql
@@ -14,3 +14,20 @@ CREATE POLICY "Give users authenticated access to folder 2dw1_1" ON storage.obje
 CREATE POLICY "Give users access to own folder 2dw1_0" ON storage.objects FOR SELECT TO public USING (bucket_id = 'pst' AND (select auth.uid()::text) = (storage.foldername(name))[1]);
 CREATE POLICY "Give users access to own folder 2dw1_1" ON storage.objects FOR INSERT TO public WITH CHECK (bucket_id = 'pst' AND (select auth.uid()::text) = (storage.foldername(name))[1]);
 
+-- Create scheduled job to remove PST files
+-- Function to delete PST files older than 24 hours
+CREATE OR REPLACE FUNCTION public.delete_old_pst_files()
+RETURNS void AS $$
+BEGIN
+  DELETE FROM storage.objects
+  WHERE bucket_id = 'pst'
+    AND created_at <= NOW() - INTERVAL '24 hours';
+END;
+$$ LANGUAGE plpgsql;
+
+-- Run once every day at midnight UTC
+SELECT cron.schedule(
+  'delete-old-pst-files',
+  '0 0 * * *', -- every 24 hours
+  $$SELECT public.delete_old_pst_files();$$
+);
