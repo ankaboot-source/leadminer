b322113ba64c426e3cd44b8da027f8bc95e746e6
Jaafoura <mehdi.jaafourag@gmail.com>
2026-02-05 10:00:04 +0100

    create sources page

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -12,6 +12,17 @@
         }"
         @click="navigateTo(contactsPath)"
       />
+      <Button
+        icon="pi pi-envelope"
+        outlined
+        class="ml-4 border-b-2 border-0 rounded-sm invisible lg:visible hover:border-primary"
+        :class="{
+          'border-primary': $router.currentRoute.value.path === sourcesPath,
+        }"
+        type="button"
+        :label="'Sources'"
+        @click="navigateTo(sourcesPath)"
+      />
       <div class="grow" />
       <div id="desktop-navbar" class="hidden lg:flex lg:items-center lg:gap-1">
         <Button
@@ -109,6 +120,8 @@ const $leadminerStore = useLeadminerStore();
 const visible = ref(false);
 const contactsPath = '/contacts';
 const minePath = '/mine';
+const sourcesPath = '/sources';
+
 const homePath = $user
   ? '/'
   : $contactsStore.contactCount
