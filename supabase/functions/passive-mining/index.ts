bbf1c771a3a0a67bda900e2739d2a9cc8ee31641
Jaafoura <mehdi.jaafourag@gmail.com>
2026-02-05 14:55:32 +0100

    Update index.ts

--- supabase/functions/passive-mining/index.ts
+++ supabase/functions/passive-mining/index.ts
@@ -1,11 +1,25 @@
 import { Context, Hono } from "npm:hono@4.7.4";
+import { createSupabaseAdmin } from "../../_shared/supabase.ts";
+const supabase = createSupabaseAdmin();
 
 const functionName = "passive-mining";
 const app = new Hono().basePath(`/${functionName}`);
 
-app.post("/", (c: Context) => {
+//!
+app.post("/", async (c: Context) => {
   try {
     return c.json({ msg: "Started passive-mining" });
+    const miningSources = await getMiningSources();
+    // 1. for every source
+    // 2. fetch boxes and keep latest emails (since last cron job for example)
+    // 3. startMining: /mine/email/:userId
+    //  const body = {
+    //         miningSource: {
+    //           email: source.email,;
+    //         };
+    //         boxes: string[];
+    //         extractSignatures: false;
+    //       };
   } catch (error) {
     console.error("Error in passive-mining:", error);
     return c.json({ error: "Failed to send email" }, 500);
@@ -22,3 +36,18 @@ app.get("/", (c: Context) => {
 });
 
 Deno.serve((req) => app.fetch(req));
+
+async function getMiningSources() {
+  const { data, error } = await supabase
+    .schema("private")
+    .from("mining_sources")
+    .select("*")
+    .match({ passive_mining: true });
+
+  if (error) {
+    console.error("Error fetching mining sources:", error.message);
+    throw error;
+  }
+
+  return data;
+}
