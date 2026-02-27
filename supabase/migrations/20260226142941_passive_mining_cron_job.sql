3775dd994722618713d442573595abf7017327d1
Jaafoura <mehdi.jaafourag@gmail.com>
2026-02-27 12:02:18 +0100

    Update 20260226142941_passive_mining_cron_job.sql

--- supabase/migrations/20260226142941_passive_mining_cron_job.sql
+++ supabase/migrations/20260226142941_passive_mining_cron_job.sql
@@ -6,21 +6,17 @@ AS $$
 DECLARE
     project_url TEXT;
     response JSONB;
-    payload JSONB;
 BEGIN
-    -- Fetch the base URL from Vault
     SELECT decrypted_secret INTO project_url
     FROM vault.decrypted_secrets
     WHERE name = 'project_url';
 
-    -- Validate secrets
     IF project_url IS NULL THEN
         RAISE EXCEPTION 'project_url is missing';
     END IF;
 
-    -- Perform HTTP POST to Edge Function
     response := net.http_post(
-        url:= project_url || '/functions/v1/' || edge_function_name,
+        url := project_url || '/functions/v1/' || edge_function_name,
         headers := jsonb_build_object(
             'Content-Type', 'application/json'
         )
@@ -31,9 +27,8 @@ END;
 $$;
 
 
-
 SELECT cron.schedule(
   'passive-cron-job',
-  '0 2 * * *',   -- At 02:00 AM
-  $$ SELECT execute_edge_function('passive-mining'); $$
+  '0 2 * * *', -- At 2:00 AM
+  $$ SELECT invoke_edge_function('passive-mining'); $$
 );
