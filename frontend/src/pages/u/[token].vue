417a9dc45521cbffb5eaa465b096e179efc823f7
Badreddine Lejmi <badreddine@ankaboot.fr>
2026-03-06 15:57:46 +0100

    fix: add /u/[token] route for unsubscribe links (#2700)
    * fix: allow PST and file mining to start without activeMiningSource
    
    * fix: add /u/[token] route for unsubscribe links

---
+++ frontend/src/pages/u/[token].vue
@@ -0,0 +1,16 @@
+<template>
+  <div />
+</template>
+
+<script setup lang="ts">
+const $route = useRoute();
+const token = $route.params.token as string;
+
+const previewModes = ['preview-unsubscribe', 'preview-open', 'preview-click'];
+
+if (previewModes.includes(token)) {
+  await navigateTo(`/unsubscribe/success?preview=true`, { external: true });
+} else {
+  await navigateTo(`/unsubscribe/success?sender=`, { external: true });
+}
+</script>
