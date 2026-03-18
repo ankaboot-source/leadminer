872f31dfbc70b6716a37b73fe62e6701fe189dc4
Badreddine Lejmi <badreddine@ankaboot.fr>
2026-03-18 10:01:26 +0100

    chore format frontend files for pre-push checks

--- frontend/src/pages/sources.vue
+++ frontend/src/pages/sources.vue
@@ -335,7 +335,7 @@ function getIcon(type: string) {
 function isActiveMiningSource(source: MiningSource): boolean {
   return Boolean(
     $leadminer.activeMiningSource?.email === source.email &&
-      $leadminer.miningTask,
+    $leadminer.miningTask,
   );
 }
 
