2a778662408ee87f05ecca41ecb001df91c9aaca
Malek Salem <malek.salem.14@gmail.com>
2026-03-18 10:44:11 +0100

    fix: i18n pluralization

--- supabase/functions/email-campaigns/middlewares-mod.ts
+++ supabase/functions/email-campaigns/middlewares-mod.ts
@@ -96,7 +96,8 @@ function checkCompliance(
           count: total,
           total,
           engagementType: t(
-            "modal.no_consented_contacts.engagement_types.contact_plural",
+            "modal.no_consented_contacts.engagement_types.contact",
+            { count: total },
           ),
         }),
         data: { total, available: 0, availableAlready: 0, reason: "consent" },
@@ -121,9 +122,9 @@ function checkCompliance(
         description: t("modal.consent_required.description", {
           available: consentedContacts.length,
           total,
-          engagementType: t(
-            "modal.consent_required.engagement_types.contact_plural",
-          ),
+          engagementType: t("modal.consent_required.engagement_types.contact", {
+            count: total,
+          }),
         }),
         data: {
           total: selectedEmails.length,
