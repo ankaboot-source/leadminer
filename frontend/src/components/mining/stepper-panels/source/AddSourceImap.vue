fe22731adbd0fc012fef83c8c593440167f99014
Jaafoura <mehdi.jaafourag@gmail.com>
2026-04-02 16:18:04 +0100

    fixes

--- frontend/src/components/mining/stepper-panels/source/AddSourceImap.vue
+++ frontend/src/components/mining/stepper-panels/source/AddSourceImap.vue
@@ -11,7 +11,7 @@
     modal
     :draggable="false"
     :header="t('sign_in_with_imap')"
-    class="w-full md:w-[35rem] max-h-full h-full md:h-auto rounded-none md:rounded-md"
+    class="w-full md:w-140 max-h-full h-full md:h-auto rounded-none md:rounded-md"
   >
     <div class="flex flex-col space-y-2">
       <div class="w-full flex flex-col gap-1">
@@ -140,13 +140,13 @@ const formErrors: Record<string, Ref> = {
 const loadingSave = ref(false);
 
 const invalidImapPassword = (password: string | undefined) =>
-  formErrors.password.value || !password?.length || password.length === 0;
+  formErrors.password?.value || !password?.length || password.length === 0;
 
 const isInvalidImapPort = (port: number) =>
-  formErrors.port.value || !(port > 0 && port <= 65536);
+  formErrors.port?.value || !(port > 0 && port <= 65536);
 
 const invalidImapHost = (host: string | undefined) =>
-  formErrors.host.value || !host?.length || host.length === 0;
+  formErrors.host?.value || !host?.length || host.length === 0;
 
 function resetFormErrors() {
   Object.values(formErrors).forEach((error) => {
@@ -182,7 +182,7 @@ function handleAuthenticationErrors(error: FetchError) {
       if (['host', 'port'].includes(field)) {
         imapAdvancedSettings.value = true;
       }
-      formErrors[field].value = true;
+      formErrors[field]!.value = true;
     });
   }
 
