e3020de6ecaf36b6b9a21c735b2c99bc5de9ea41
Malek <malek@ankaboot.io>
2026-06-16 22:46:58 +0100

    fix: account deletion error due to missing $user definition (#2846)

--- frontend/src/pages/account/settings.vue
+++ frontend/src/pages/account/settings.vue
@@ -151,6 +151,7 @@
 <script setup lang="ts">
 import type { UserAttributes } from '@supabase/supabase-js';
 import { AcceptNewsLetter } from '~/utils/extras';
+import type { FetchError } from 'ofetch';
 
 import { isInvalidEmail } from '@/utils/email';
 import {
@@ -172,6 +173,7 @@ const { t: $t } = useI18n({
 
 const $toast = useToast();
 const { $saasEdgeFunctions } = useNuxtApp();
+const $user = useSupabaseUser();
 const $profile = useSupabaseUserProfile();
 
 const {
@@ -315,6 +317,7 @@ async function updateUserDetailsButton() {
 
 async function deleteAccount() {
   isLoading.value = true;
+  showDeleteModal.value = false;
   try {
     await $saasEdgeFunctions('delete-user', {
       method: 'DELETE',
@@ -336,10 +339,19 @@ async function deleteAccount() {
       detail: t('account_deleted_success'),
       life: 3000,
     });
-    isLoading.value = false;
   } catch (err) {
+    const message =
+      (err as unknown as FetchError)?.response?._data?.error ||
+      (err as Error)?.message ||
+      'Something went wrong';
+    $toast.add({
+      severity: 'error',
+      summary: 'Oops!',
+      detail: message,
+      life: 5000,
+    });
+  } finally {
     isLoading.value = false;
-    throw err;
   }
 }
 </script>
