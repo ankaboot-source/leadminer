cafd1f6c8755354e579cd27219833acc773f8f0a
Malek Salem <malek@ankaboot.io>
2026-06-19 17:29:47 +0100

    Revert "restore original changes"
    This reverts commit 19f5b8a5236ba283c30b75d7ae9f0ccae1faf468.

--- supabase/migrations/20260214130000_add_email_campaigns.sql
+++ supabase/migrations/20260214130000_add_email_campaigns.sql
@@ -253,6 +253,7 @@ $$;
 
 CREATE OR REPLACE FUNCTION private.get_mining_source_credentials_for_user(_user_id UUID, _encryption_key TEXT)
 RETURNS TABLE(
+  id UUID,
   email TEXT,
   type TEXT,
   credentials JSONB
@@ -263,6 +264,7 @@ AS $$
 BEGIN
   RETURN QUERY
   SELECT
+    ms.id,
     ms.email,
     ms.type::TEXT,
     extensions.pgp_sym_decrypt(ms.credentials, _encryption_key)::JSONB AS credentials
